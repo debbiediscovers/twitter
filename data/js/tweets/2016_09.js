f06a3757e14e6bf36bdcb7c4a53e30ca771c5bbc
debbiediscovers <debbiediscovers@gmail.com>
2016-09-09 07:12:03 +0800

    Added 1 tweets

--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Thu Sep 08 22:16:36 +0000 2016",
+		"id": 774008562205003800,
+		"id_str": "774008562205003777",
+		"text": "#Omnichannel - Bloomingdales gamifies hidden #Snapchat geo-filters to lure shoppers instore https://t.co/8gWadJ6qeW https://t.co/5ErETP1TVp",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Omnichannel",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"text": "Snapchat",
+					"indices": [
+						45,
+						54
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8gWadJ6qeW",
+					"expanded_url": "http://buff.ly/2ct4ZpK",
+					"display_url": "buff.ly/2ct4ZpK",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 774008560510525400,
+					"id_str": "774008560510525441",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Cr3Ur1ZXYAEWfhF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Cr3Ur1ZXYAEWfhF.jpg",
+					"url": "https://t.co/5ErETP1TVp",
+					"display_url": "pic.twitter.com/5ErETP1TVp",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/774008562205003777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 774008560510525400,
+					"id_str": "774008560510525441",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Cr3Ur1ZXYAEWfhF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Cr3Ur1ZXYAEWfhF.jpg",
+					"url": "https://t.co/5ErETP1TVp",
+					"display_url": "pic.twitter.com/5ErETP1TVp",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/774008562205003777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 465,
+							"h": 262,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10380,
+			"friends_count": 7349,
+			"listed_count": 864,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1757,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5956,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Sep 08 18:32:59 +0000 2016",
 		"id": 773952289442713600,
 		"id_str": "773952289442713600",
