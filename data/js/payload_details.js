de35f96050e358df9aee8ca05f434cb717c431bf
debbiediscovers <debbiediscovers@gmail.com>
2016-09-13 06:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6516,
-	"created_at": "Mon Sep 12 17:12:03 +0000 2016",
+	"tweets": 6517,
+	"created_at": "Mon Sep 12 22:12:03 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Mon Sep 12 22:00:39 +0000 2016",
+		"id": 775454102377693200,
+		"id_str": "775454102377693184",
+		"text": "#MUSTTRY! 4 Little-Known Tools to #Curate #Content : @smexaminer https://t.co/YRHRT8V3ib  #SWEET! https://t.co/JphgB3ClEd",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MUSTTRY",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "Curate",
+					"indices": [
+						34,
+						41
+					]
+				},
+				{
+					"text": "Content",
+					"indices": [
+						42,
+						50
+					]
+				},
+				{
+					"text": "SWEET",
+					"indices": [
+						90,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SMExaminer",
+					"name": "SocialMedia Examiner",
+					"id": 53925101,
+					"id_str": "53925101",
+					"indices": [
+						53,
+						64
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/YRHRT8V3ib",
+					"expanded_url": "http://buff.ly/2ciZOXr",
+					"display_url": "buff.ly/2ciZOXr",
+					"indices": [
+						65,
+						88
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 775454100385370100,
+					"id_str": "775454100385370112",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/CsL3ZVEXEAAfVVM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CsL3ZVEXEAAfVVM.jpg",
+					"url": "https://t.co/JphgB3ClEd",
+					"display_url": "pic.twitter.com/JphgB3ClEd",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/775454102377693184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 775454100385370100,
+					"id_str": "775454100385370112",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/CsL3ZVEXEAAfVVM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CsL3ZVEXEAAfVVM.jpg",
+					"url": "https://t.co/JphgB3ClEd",
+					"display_url": "pic.twitter.com/JphgB3ClEd",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/775454102377693184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10663,
+			"friends_count": 7747,
+			"listed_count": 886,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1788,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5996,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Sep 12 16:16:47 +0000 2016",
 		"id": 775367565728161800,
 		"id_str": "775367565728161792",
