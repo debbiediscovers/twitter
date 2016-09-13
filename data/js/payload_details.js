87b7cfaf6d11bbf315bfa2af67761878a4eaeffa
debbiediscovers <debbiediscovers@gmail.com>
2016-09-14 06:12:11 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6539,
-	"created_at": "Tue Sep 13 21:12:06 +0000 2016",
+	"tweets": 6540,
+	"created_at": "Tue Sep 13 22:12:09 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Tue Sep 13 21:13:17 +0000 2016",
+		"id": 775804568475054100,
+		"id_str": "775804568475054080",
+		"text": "RT @mdbagency: Check out the top 10 things you must know about the iOS 10 📲😮 https://t.co/bXPY0Mkw9d #digital #technology",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "digital",
+					"indices": [
+						101,
+						109
+					]
+				},
+				{
+					"text": "technology",
+					"indices": [
+						110,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mdbagency",
+					"name": "Markdebrand",
+					"id": 4419170356,
+					"id_str": "4419170356",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/bXPY0Mkw9d",
+					"expanded_url": "https://www.theguardian.com/technology/2016/sep/13/ios-10-apple-new-iphone-ipad",
+					"display_url": "theguardian.com/technology/201…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10836,
+			"friends_count": 8666,
+			"listed_count": 888,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1806,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6019,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Sep 13 20:30:19 +0000 2016",
+			"id": 775793755655843800,
+			"id_str": "775793755655843840",
+			"text": "Check out the top 10 things you must know about the iOS 10 📲😮 https://t.co/bXPY0Mkw9d #digital #technology",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "digital",
+						"indices": [
+							86,
+							94
+						]
+					},
+					{
+						"text": "technology",
+						"indices": [
+							95,
+							106
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/bXPY0Mkw9d",
+						"expanded_url": "https://www.theguardian.com/technology/2016/sep/13/ios-10-apple-new-iphone-ipad",
+						"display_url": "theguardian.com/technology/201…",
+						"indices": [
+							62,
+							85
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://dashboard.twitter.com\" rel=\"nofollow\">Twitter Business Experience</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4419170356,
+				"id_str": "4419170356",
+				"name": "Markdebrand",
+				"screen_name": "mdbagency",
+				"location": "Miramar, FL",
+				"description": "Markdebrand is an advertising agency specialized in experiential marketing and digital services. Marketing, design and branding are our passions!",
+				"url": "https://t.co/JJEun95cg1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JJEun95cg1",
+								"expanded_url": "http://www.markdebrand.com",
+								"display_url": "markdebrand.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 923,
+				"friends_count": 220,
+				"listed_count": 103,
+				"created_at": "Tue Dec 08 20:05:51 +0000 2015",
+				"favourites_count": 5428,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 879,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/674327816104779776/Wf8hKo19.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/674327816104779776/Wf8hKo19.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/769180255160561664/n2PUxny0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/769180255160561664/n2PUxny0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4419170356/1472216749",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Sep 13 20:46:50 +0000 2016",
 		"id": 775797911514914800,
 		"id_str": "775797911514914816",
