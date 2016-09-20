35832bb1ec52277963a7d29c26461d68c3308338
debbiediscovers <debbiediscovers@gmail.com>
2016-09-20 21:12:05 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6625,
-	"created_at": "Tue Sep 20 11:12:07 +0000 2016",
+	"tweets": 6626,
+	"created_at": "Tue Sep 20 13:12:04 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,126 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Tue Sep 20 13:10:02 +0000 2016",
+		"id": 778219668628570100,
+		"id_str": "778219668628570113",
+		"text": "@Mike_Arnesen @quillh @UpBuildTeam Thanks for sharing! Nice round up!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Mike_Arnesen",
+					"name": "Mike Arnesen",
+					"id": 15763963,
+					"id_str": "15763963",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "quillh",
+					"name": "Will Hattman",
+					"id": 35842045,
+					"id_str": "35842045",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"screen_name": "UpBuildTeam",
+					"name": "UpBuild",
+					"id": 3233464044,
+					"id_str": "3233464044",
+					"indices": [
+						22,
+						34
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 777917402780672000,
+		"in_reply_to_status_id_str": "777917402780672000",
+		"in_reply_to_user_id": 15763963,
+		"in_reply_to_user_id_str": "15763963",
+		"in_reply_to_screen_name": "Mike_Arnesen",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10992,
+			"friends_count": 7713,
+			"listed_count": 908,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1220,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6102,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Sep 20 10:33:03 +0000 2016",
 		"id": 778180161766326300,
 		"id_str": "778180161766326273",
