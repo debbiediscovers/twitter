f7f19b541d4e36809776c11a5296f75ef6bf437e
debbiediscovers <debbiediscovers@gmail.com>
2016-09-23 21:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6720,
-	"created_at": "Fri Sep 23 10:12:06 +0000 2016",
+	"tweets": 6721,
+	"created_at": "Fri Sep 23 13:12:05 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,261 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Fri Sep 23 12:38:48 +0000 2016",
+		"id": 779298970942210000,
+		"id_str": "779298970942210048",
+		"text": "RT @HarvardHBX: How to make sure your startup doesn't become the next Friendster https://t.co/t8JbI4akup @FortuneMagazine #disruption #entr…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "disruption",
+					"indices": [
+						122,
+						133
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HarvardHBX",
+					"name": "HBX | HBS Online",
+					"id": 1416391302,
+					"id_str": "1416391302",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "FortuneMagazine",
+					"name": "Fortune",
+					"id": 25053299,
+					"id_str": "25053299",
+					"indices": [
+						105,
+						121
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/t8JbI4akup",
+					"expanded_url": "http://hbs.me/2d3i7nh",
+					"display_url": "hbs.me/2d3i7nh",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11038,
+			"friends_count": 7913,
+			"listed_count": 928,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1257,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6195,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Sep 22 18:31:04 +0000 2016",
+			"id": 779025235911868400,
+			"id_str": "779025235911868416",
+			"text": "How to make sure your startup doesn't become the next Friendster https://t.co/t8JbI4akup @FortuneMagazine #disruption #entrepreneurs",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "disruption",
+						"indices": [
+							106,
+							117
+						]
+					},
+					{
+						"text": "entrepreneurs",
+						"indices": [
+							118,
+							132
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "FortuneMagazine",
+						"name": "Fortune",
+						"id": 25053299,
+						"id_str": "25053299",
+						"indices": [
+							89,
+							105
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/t8JbI4akup",
+						"expanded_url": "http://hbs.me/2d3i7nh",
+						"display_url": "hbs.me/2d3i7nh",
+						"indices": [
+							65,
+							88
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.spredfast.com\" rel=\"nofollow\">Spredfast</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1416391302,
+				"id_str": "1416391302",
+				"name": "HBX | HBS Online",
+				"screen_name": "HarvardHBX",
+				"location": "",
+				"description": "Welcome to HBX, the online learning initiative of Harvard Business School.",
+				"url": "http://t.co/098S9bvgD5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/098S9bvgD5",
+								"expanded_url": "http://hbx.hbs.edu",
+								"display_url": "hbx.hbs.edu",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6119,
+				"friends_count": 528,
+				"listed_count": 236,
+				"created_at": "Thu May 09 20:37:30 +0000 2013",
+				"favourites_count": 1633,
+				"utc_offset": -10800,
+				"time_zone": "Atlantic Time (Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2674,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/765242592770527232/zTVsiC2K_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/765242592770527232/zTVsiC2K_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1416391302/1471283046",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Sep 23 09:17:00 +0000 2016",
 		"id": 779248188612116500,
 		"id_str": "779248188612116480",
