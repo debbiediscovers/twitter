996961e7bcf3084505898bf38da32819b719780b
debbiediscovers <debbiediscovers@gmail.com>
2016-10-07 12:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7018,
-	"created_at": "Fri Oct 07 03:12:04 +0000 2016",
+	"tweets": 7019,
+	"created_at": "Fri Oct 07 04:12:01 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,308 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Fri Oct 07 04:03:01 +0000 2016",
+		"id": 784242601650167800,
+		"id_str": "784242601650167808",
+		"text": "RT @NikkiElizDemere: Flattery, Association and the Halo Effect - why we tend to be influenced by people we like. by… https://t.co/p2867tt5HW",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NikkiElizDemere",
+					"name": "Nichole Elizabeth",
+					"id": 39099783,
+					"id_str": "39099783",
+					"indices": [
+						3,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/p2867tt5HW",
+					"expanded_url": "https://twitter.com/i/web/status/784242601650167808",
+					"display_url": "twitter.com/i/web/status/7…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11323,
+			"friends_count": 6630,
+			"listed_count": 988,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1349,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6483,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 778219431298273300,
+		"quoted_status_id_str": "778219431298273280",
+		"quoted_status": {
+			"created_at": "Tue Sep 20 13:09:05 +0000 2016",
+			"id": 778219431298273300,
+			"id_str": "778219431298273280",
+			"text": "Flattery, association, halo effect: \n\n\"Mental Model: Bias from Liking/Loving\" https://t.co/O4LEIvn4YJ @farnamstreet https://t.co/Sjuzg48FsO",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "farnamstreet",
+						"name": "Shane Parrish",
+						"id": 33104659,
+						"id_str": "33104659",
+						"indices": [
+							102,
+							115
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/O4LEIvn4YJ",
+						"expanded_url": "http://bit.ly/2cUH5qg",
+						"display_url": "bit.ly/2cUH5qg",
+						"indices": [
+							78,
+							101
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 778219429100412900,
+						"id_str": "778219429100412928",
+						"indices": [
+							116,
+							139
+						],
+						"media_url": "http://pbs.twimg.com/media/CszKcpTWYAANKF7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CszKcpTWYAANKF7.jpg",
+						"url": "https://t.co/Sjuzg48FsO",
+						"display_url": "pic.twitter.com/Sjuzg48FsO",
+						"expanded_url": "https://twitter.com/StefanW/status/778219431298273280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 778219429100412900,
+						"id_str": "778219429100412928",
+						"indices": [
+							116,
+							139
+						],
+						"media_url": "http://pbs.twimg.com/media/CszKcpTWYAANKF7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CszKcpTWYAANKF7.jpg",
+						"url": "https://t.co/Sjuzg48FsO",
+						"display_url": "pic.twitter.com/Sjuzg48FsO",
+						"expanded_url": "https://twitter.com/StefanW/status/778219431298273280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 591,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2797491,
+				"id_str": "2797491",
+				"name": "Stefan Wolpers",
+				"screen_name": "StefanW",
+				"location": "Berlin, Germany",
+				"description": "Agile coach, product & Scrum guy. (CSPO, CSM.) Likes all agile & lean, writes on Lean User Testing. Mentor at @sbcBerlin. Also @AgeOfProduct & @Snippet42.",
+				"url": "http://t.co/v0635ShWpn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/v0635ShWpn",
+								"expanded_url": "http://bit.ly/hands-on-agile",
+								"display_url": "bit.ly/hands-on-agile",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5782,
+				"friends_count": 382,
+				"listed_count": 531,
+				"created_at": "Thu Mar 29 06:17:41 +0000 2007",
+				"favourites_count": 2173,
+				"utc_offset": 7200,
+				"time_zone": "Bern",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 36607,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "6699FF",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/483664943258951682/HvzlyGnl.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/483664943258951682/HvzlyGnl.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/718479678999302144/x7Vlxug8_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/718479678999302144/x7Vlxug8_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2797491/1460877516",
+				"profile_link_color": "0099BB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Oct 07 03:02:09 +0000 2016",
 		"id": 784227283967971300,
 		"id_str": "784227283967971328",
