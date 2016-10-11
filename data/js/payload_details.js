10a51ba309e8ca604cc99ed3d4285a07b644e264
debbiediscovers <debbiediscovers@gmail.com>
2016-10-11 14:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7161,
-	"created_at": "Tue Oct 11 05:12:02 +0000 2016",
+	"tweets": 7162,
+	"created_at": "Tue Oct 11 06:12:04 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Tue Oct 11 05:42:01 +0000 2016",
+		"id": 785717065743077400,
+		"id_str": "785717065743077376",
+		"text": "@OntreeCa @untapt @gunnr @Support @susanfitzell @ajay60km  did Twitter ever respond to this?",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OntreeCa",
+					"name": "Ontree",
+					"id": 2364599162,
+					"id_str": "2364599162",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "untapt",
+					"name": "untapt",
+					"id": 2157797477,
+					"id_str": "2157797477",
+					"indices": [
+						10,
+						17
+					]
+				},
+				{
+					"screen_name": "gunnr",
+					"name": "Greg Gunn",
+					"id": 7096032,
+					"id_str": "7096032",
+					"indices": [
+						18,
+						24
+					]
+				},
+				{
+					"screen_name": "Support",
+					"name": "Twitter Support",
+					"id": 17874544,
+					"id_str": "17874544",
+					"indices": [
+						25,
+						33
+					]
+				},
+				{
+					"screen_name": "susanfitzell",
+					"name": "Susan Fitzell",
+					"id": 14243886,
+					"id_str": "14243886",
+					"indices": [
+						34,
+						47
+					]
+				},
+				{
+					"screen_name": "ajay60km",
+					"name": "AjaySaxena",
+					"id": 472215076,
+					"id_str": "472215076",
+					"indices": [
+						48,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 669145983637258200,
+		"in_reply_to_status_id_str": "669145983637258241",
+		"in_reply_to_user_id": 2364599162,
+		"in_reply_to_user_id_str": "2364599162",
+		"in_reply_to_screen_name": "OntreeCa",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11419,
+			"friends_count": 6886,
+			"listed_count": 988,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1389,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6616,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Oct 11 05:06:39 +0000 2016",
 		"id": 785708165765619700,
 		"id_str": "785708165765619713",
