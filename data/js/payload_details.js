1d4e8c9df30deb0052481fb0600e58f082c00c55
debbiediscovers <debbiediscovers@gmail.com>
2016-10-13 02:12:05 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7203,
-	"created_at": "Wed Oct 12 17:12:06 +0000 2016",
+	"tweets": 7204,
+	"created_at": "Wed Oct 12 18:12:04 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Wed Oct 12 17:30:34 +0000 2016",
+		"id": 786257767736586200,
+		"id_str": "786257767736586240",
+		"text": "Got my free ticket! #NeilPatel: #GrowthHacking Secrets for 2017 [Virtual Summit] https://t.co/XDdDbjszK6 https://t.co/rYXPFhrLLu",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NeilPatel",
+					"indices": [
+						20,
+						30
+					]
+				},
+				{
+					"text": "GrowthHacking",
+					"indices": [
+						32,
+						46
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XDdDbjszK6",
+					"expanded_url": "http://queue.social/UCeKggO",
+					"display_url": "queue.social/UCeKggO",
+					"indices": [
+						81,
+						104
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 786257764607680500,
+					"id_str": "786257764607680512",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/CulZRWlVYAAJkMz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CulZRWlVYAAJkMz.jpg",
+					"url": "https://t.co/rYXPFhrLLu",
+					"display_url": "pic.twitter.com/rYXPFhrLLu",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/786257767736586240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 786257764607680500,
+					"id_str": "786257764607680512",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/CulZRWlVYAAJkMz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CulZRWlVYAAJkMz.jpg",
+					"url": "https://t.co/rYXPFhrLLu",
+					"display_url": "pic.twitter.com/rYXPFhrLLu",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/786257767736586240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://queueat.com/\" rel=\"nofollow\">QueueTech</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11481,
+			"friends_count": 7214,
+			"listed_count": 1003,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1412,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6658,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Oct 12 16:23:01 +0000 2016",
 		"id": 786240767413149700,
 		"id_str": "786240767413149696",
