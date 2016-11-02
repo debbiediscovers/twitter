cb9116ed060a06edf037ce2b7fe8029eb103a985
debbiediscovers <debbiediscovers@gmail.com>
2016-11-02 18:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7563,
-	"created_at": "Wed Nov 02 06:12:01 +0000 2016",
+	"tweets": 7564,
+	"created_at": "Wed Nov 02 10:12:03 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_11.js
+++ data/js/tweets/2016_11.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2016_11 = 
 [
 	{
+		"created_at": "Wed Nov 02 09:39:38 +0000 2016",
+		"id": 793749400321482800,
+		"id_str": "793749400321482752",
+		"text": "\"Computational thinking: Do Computers Think?\" https://t.co/c4b3Nx37MF l #ct #computational #thinking #coding https://t.co/Nfus5e1FF3",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ct",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "computational",
+					"indices": [
+						76,
+						90
+					]
+				},
+				{
+					"text": "thinking",
+					"indices": [
+						91,
+						100
+					]
+				},
+				{
+					"text": "coding",
+					"indices": [
+						101,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c4b3Nx37MF",
+					"expanded_url": "https://goo.gl/HuQtqc",
+					"display_url": "goo.gl/HuQtqc",
+					"indices": [
+						46,
+						69
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 793749397746151400,
+					"id_str": "793749397746151424",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/CwP23y1W8AAcEdT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CwP23y1W8AAcEdT.jpg",
+					"url": "https://t.co/Nfus5e1FF3",
+					"display_url": "pic.twitter.com/Nfus5e1FF3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/793749400321482752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 180,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 170,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 180,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 793749397746151400,
+					"id_str": "793749397746151424",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/CwP23y1W8AAcEdT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CwP23y1W8AAcEdT.jpg",
+					"url": "https://t.co/Nfus5e1FF3",
+					"display_url": "pic.twitter.com/Nfus5e1FF3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/793749400321482752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 180,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 170,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 180,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://missinglettr.com\" rel=\"nofollow\">missinglettr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11632,
+			"friends_count": 7625,
+			"listed_count": 1045,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 1574,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7013,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Nov 02 05:44:02 +0000 2016",
 		"id": 793690109971304400,
 		"id_str": "793690109971304449",
