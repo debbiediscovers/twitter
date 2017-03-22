ef5b9213311b70f75c2d0536dbc0cabf8d51004b
debbiediscovers <debbiediscovers@gmail.com>
2017-03-22 09:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7939,
-	"created_at": "Wed Mar 15 01:12:00 +0000 2017",
+	"tweets": 7940,
+	"created_at": "Wed Mar 22 01:12:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,344 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Wed Mar 22 00:30:01 +0000 2017",
+		"id": 844345383740489700,
+		"id_str": "844345383740489728",
+		"text": "RT @StartupTabs: Featured #startup @recoupfitness \"Everyone is an athlete, Recoup like a pro\" https://t.co/9j8xKF72xy https://t.co/4GTh4UXA…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						26,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupTabs",
+					"name": "Startuptabs",
+					"id": 1408850774,
+					"id_str": "1408850774",
+					"indices": [
+						3,
+						15
+					]
+				},
+				{
+					"screen_name": "RecoupFitness",
+					"name": "Recoup Fitness",
+					"id": 3044426490,
+					"id_str": "3044426490",
+					"indices": [
+						35,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9j8xKF72xy",
+					"expanded_url": "https://goo.gl/FwlsH7",
+					"display_url": "goo.gl/FwlsH7",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.amplfy.co\" rel=\"nofollow\">AmplfyCo</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11493,
+			"friends_count": 7396,
+			"listed_count": 1037,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2466,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7340,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Mar 21 15:00:03 +0000 2017",
+			"id": 844201946735149000,
+			"id_str": "844201946735149058",
+			"text": "Featured #startup @recoupfitness \"Everyone is an athlete, Recoup like a pro\" https://t.co/9j8xKF72xy https://t.co/4GTh4UXAhl",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "startup",
+						"indices": [
+							9,
+							17
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "RecoupFitness",
+						"name": "Recoup Fitness",
+						"id": 3044426490,
+						"id_str": "3044426490",
+						"indices": [
+							18,
+							32
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/9j8xKF72xy",
+						"expanded_url": "https://goo.gl/FwlsH7",
+						"display_url": "goo.gl/FwlsH7",
+						"indices": [
+							77,
+							100
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 844201943916564500,
+						"id_str": "844201943916564480",
+						"indices": [
+							101,
+							124
+						],
+						"media_url": "http://pbs.twimg.com/media/C7c1MfnXUAA1lOl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7c1MfnXUAA1lOl.jpg",
+						"url": "https://t.co/4GTh4UXAhl",
+						"display_url": "pic.twitter.com/4GTh4UXAhl",
+						"expanded_url": "https://twitter.com/StartupTabs/status/844201946735149058/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 729,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 413,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 729,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 844201943916564500,
+						"id_str": "844201943916564480",
+						"indices": [
+							101,
+							124
+						],
+						"media_url": "http://pbs.twimg.com/media/C7c1MfnXUAA1lOl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7c1MfnXUAA1lOl.jpg",
+						"url": "https://t.co/4GTh4UXAhl",
+						"display_url": "pic.twitter.com/4GTh4UXAhl",
+						"expanded_url": "https://twitter.com/StartupTabs/status/844201946735149058/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 729,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 413,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 729,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.amplfy.co\" rel=\"nofollow\">AmplfyCo</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1408850774,
+				"id_str": "1408850774",
+				"name": "Startuptabs",
+				"screen_name": "StartupTabs",
+				"location": "Los Angeles, CA",
+				"description": "Discover new startups now! Every tab loads a new startup. Check out our browser extension: https://t.co/AUPAzZdicz.",
+				"url": "https://t.co/PFUXu2JqOr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PFUXu2JqOr",
+								"expanded_url": "http://www.startuptabs.com",
+								"display_url": "startuptabs.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/AUPAzZdicz",
+								"expanded_url": "http://startuptabs.com/chrome",
+								"display_url": "startuptabs.com/chrome",
+								"indices": [
+									91,
+									114
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1976,
+				"friends_count": 35,
+				"listed_count": 119,
+				"created_at": "Mon May 06 22:18:02 +0000 2013",
+				"favourites_count": 11444,
+				"utc_offset": -25200,
+				"time_zone": "Arizona",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1314,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/494506995521568768/P0AcRC5O_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/494506995521568768/P0AcRC5O_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1408850774/1436947494",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Mar 15 00:30:02 +0000 2017",
 		"id": 841808671600451600,
 		"id_str": "841808671600451585",
