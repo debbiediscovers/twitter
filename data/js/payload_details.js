90587963a252b191709dc9b3b8f0d8dd54542973
debbiediscovers <debbiediscovers@gmail.com>
2017-05-11 09:12:01 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7951,
-	"created_at": "Mon May 08 14:12:01 +0000 2017",
+	"tweets": 7952,
+	"created_at": "Thu May 11 01:12:00 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,430 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Thu May 11 00:30:02 +0000 2017",
+		"id": 862464783106310100,
+		"id_str": "862464783106310144",
+		"text": "RT @StartupTabs: Featured #startup @getboundless \"Lean apps build winning startups.\" https://t.co/VYebhMMwc9 https://t.co/eOmLHru2AP",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						26,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupTabs",
+					"name": "Startuptabs",
+					"id": 1408850774,
+					"id_str": "1408850774",
+					"indices": [
+						3,
+						15
+					]
+				},
+				{
+					"screen_name": "GetBoundless",
+					"name": "Paul Keck",
+					"id": 833367954,
+					"id_str": "833367954",
+					"indices": [
+						35,
+						48
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VYebhMMwc9",
+					"expanded_url": "https://goo.gl/9BoFdx",
+					"display_url": "goo.gl/9BoFdx",
+					"indices": [
+						85,
+						108
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 862321343106342900,
+					"id_str": "862321343106342913",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+					"url": "https://t.co/eOmLHru2AP",
+					"display_url": "pic.twitter.com/eOmLHru2AP",
+					"expanded_url": "https://twitter.com/StartupTabs/status/862321346419752960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1238,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 679,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 384,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 862321346419753000,
+					"source_status_id_str": "862321346419752960",
+					"source_user_id": 1408850774,
+					"source_user_id_str": "1408850774"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 862321343106342900,
+					"id_str": "862321343106342913",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+					"url": "https://t.co/eOmLHru2AP",
+					"display_url": "pic.twitter.com/eOmLHru2AP",
+					"expanded_url": "https://twitter.com/StartupTabs/status/862321346419752960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1238,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 679,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 384,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 862321346419753000,
+					"source_status_id_str": "862321346419752960",
+					"source_user_id": 1408850774,
+					"source_user_id_str": "1408850774"
+				}
+			]
+		},
+		"source": "<a href=\"http://www.amplfy.co\" rel=\"nofollow\">AmplfyCo</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/o0MiAYiEuU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o0MiAYiEuU",
+							"expanded_url": "http://info.debbiediscovers.com",
+							"display_url": "info.debbiediscovers.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11441,
+			"friends_count": 7479,
+			"listed_count": 1032,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2476,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7348,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed May 10 15:00:04 +0000 2017",
+			"id": 862321346419753000,
+			"id_str": "862321346419752960",
+			"text": "Featured #startup @getboundless \"Lean apps build winning startups.\" https://t.co/VYebhMMwc9 https://t.co/eOmLHru2AP",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "startup",
+						"indices": [
+							9,
+							17
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "GetBoundless",
+						"name": "Paul Keck",
+						"id": 833367954,
+						"id_str": "833367954",
+						"indices": [
+							18,
+							31
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/VYebhMMwc9",
+						"expanded_url": "https://goo.gl/9BoFdx",
+						"display_url": "goo.gl/9BoFdx",
+						"indices": [
+							68,
+							91
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 862321343106342900,
+						"id_str": "862321343106342913",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+						"url": "https://t.co/eOmLHru2AP",
+						"display_url": "pic.twitter.com/eOmLHru2AP",
+						"expanded_url": "https://twitter.com/StartupTabs/status/862321346419752960/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1238,
+								"h": 700,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 862321343106342900,
+						"id_str": "862321343106342913",
+						"indices": [
+							92,
+							115
+						],
+						"media_url": "http://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_eUsVoXYAEm2Fa.jpg",
+						"url": "https://t.co/eOmLHru2AP",
+						"display_url": "pic.twitter.com/eOmLHru2AP",
+						"expanded_url": "https://twitter.com/StartupTabs/status/862321346419752960/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1238,
+								"h": 700,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.amplfy.co\" rel=\"nofollow\">AmplfyCo</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1408850774,
+				"id_str": "1408850774",
+				"name": "Startuptabs",
+				"screen_name": "StartupTabs",
+				"location": "Los Angeles, CA",
+				"description": "Discover new startups now! Every tab loads a new startup. Check out our browser extension: https://t.co/AUPAzZdicz.",
+				"url": "https://t.co/PFUXu2JqOr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PFUXu2JqOr",
+								"expanded_url": "http://www.startuptabs.com",
+								"display_url": "startuptabs.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/AUPAzZdicz",
+								"expanded_url": "http://startuptabs.com/chrome",
+								"display_url": "startuptabs.com/chrome",
+								"indices": [
+									91,
+									114
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1950,
+				"friends_count": 39,
+				"listed_count": 117,
+				"created_at": "Mon May 06 22:18:02 +0000 2013",
+				"favourites_count": 11416,
+				"utc_offset": -25200,
+				"time_zone": "Arizona",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1325,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/494506995521568768/P0AcRC5O_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/494506995521568768/P0AcRC5O_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1408850774/1436947494",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon May 08 14:03:59 +0000 2017",
 		"id": 861582457015062500,
 		"id_str": "861582457015062528",
