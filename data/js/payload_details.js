bc44fc460c333f83046a8cd0589382774b1d6603
debbiediscovers <debbiediscovers@gmail.com>
2017-07-07 21:12:03 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7960,
-	"created_at": "Wed Jul 05 10:12:03 +0000 2017",
+	"tweets": 7962,
+	"created_at": "Fri Jul 07 13:12:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,511 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Fri Jul 07 13:06:20 +0000 2017",
+		"id": 883311219263774700,
+		"id_str": "883311219263774720",
+		"text": "RT @nw_luxury: Great Post: What We Learned From Spending $100k On Facebook Ads https://t.co/Pv1bycTkxJ @HubSpot #FacebookAds https://t.co/c…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FacebookAds",
+					"indices": [
+						112,
+						124
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nw_luxury",
+					"name": "Kathryn Lister",
+					"id": 241285883,
+					"id_str": "241285883",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "HubSpot",
+					"name": "HubSpot",
+					"id": 14458280,
+					"id_str": "14458280",
+					"indices": [
+						103,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Pv1bycTkxJ",
+					"expanded_url": "https://goo.gl/QZRicR",
+					"display_url": "goo.gl/QZRicR",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11408,
+			"friends_count": 7543,
+			"listed_count": 1017,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2483,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7347,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Jul 06 16:10:12 +0000 2017",
+			"id": 882995104142774300,
+			"id_str": "882995104142774272",
+			"text": "Great Post: What We Learned From Spending $100k On Facebook Ads https://t.co/Pv1bycTkxJ @HubSpot #FacebookAds https://t.co/cX0RpLWwsY",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "FacebookAds",
+						"indices": [
+							97,
+							109
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "HubSpot",
+						"name": "HubSpot",
+						"id": 14458280,
+						"id_str": "14458280",
+						"indices": [
+							88,
+							96
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Pv1bycTkxJ",
+						"expanded_url": "https://goo.gl/QZRicR",
+						"display_url": "goo.gl/QZRicR",
+						"indices": [
+							64,
+							87
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 882995100934189000,
+						"id_str": "882995100934189057",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/media/DEEHXbGXsAEXXnV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DEEHXbGXsAEXXnV.jpg",
+						"url": "https://t.co/cX0RpLWwsY",
+						"display_url": "pic.twitter.com/cX0RpLWwsY",
+						"expanded_url": "https://twitter.com/nw_luxury/status/882995104142774272/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 761,
+								"h": 315,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 761,
+								"h": 315,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 281,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 882995100934189000,
+						"id_str": "882995100934189057",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/media/DEEHXbGXsAEXXnV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DEEHXbGXsAEXXnV.jpg",
+						"url": "https://t.co/cX0RpLWwsY",
+						"display_url": "pic.twitter.com/cX0RpLWwsY",
+						"expanded_url": "https://twitter.com/nw_luxury/status/882995104142774272/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 761,
+								"h": 315,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 761,
+								"h": 315,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 281,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://eclincher.com\" rel=\"nofollow\">eClincher</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 241285883,
+				"id_str": "241285883",
+				"name": "Kathryn Lister",
+				"screen_name": "nw_luxury",
+				"location": "Seattle Wa",
+				"description": "Mother. Wife. Sister. Artist. Philanthropist. Entrepreneur. Realtor. Investor. Bering Sea Crabber. Honoring local ideas, people, places & global artistry.",
+				"url": "https://t.co/uylpVq3cky",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uylpVq3cky",
+								"expanded_url": "http://profile.kathrynlister.com/",
+								"display_url": "profile.kathrynlister.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1026,
+				"friends_count": 3360,
+				"listed_count": 10,
+				"created_at": "Fri Jan 21 22:19:08 +0000 2011",
+				"favourites_count": 154,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 361,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0E3B8A",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/209260730/x866ddde9e57c9292dd514acd5fb907c.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/209260730/x866ddde9e57c9292dd514acd5fb907c.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/833177122222940160/V05zR_5m_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/833177122222940160/V05zR_5m_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/241285883/1487479919",
+				"profile_link_color": "109DE8",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "262626",
+				"profile_text_color": "549A1E",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Fri Jul 07 13:02:59 +0000 2017",
+		"id": 883310376372158500,
+		"id_str": "883310376372158464",
+		"text": "Positive vibes! TGIF #fridayfeeling ❤️happy Friday everyone! https://t.co/NkuNFkVjO3",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "fridayfeeling",
+					"indices": [
+						21,
+						35
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 883310314489487400,
+					"id_str": "883310314489487360",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/883310314489487360/pu/img/AQ8OHTVqb82tPbGA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/883310314489487360/pu/img/AQ8OHTVqb82tPbGA.jpg",
+					"url": "https://t.co/NkuNFkVjO3",
+					"display_url": "pic.twitter.com/NkuNFkVjO3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/883310376372158464/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 340,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 883310314489487400,
+					"id_str": "883310314489487360",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/883310314489487360/pu/img/AQ8OHTVqb82tPbGA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/883310314489487360/pu/img/AQ8OHTVqb82tPbGA.jpg",
+					"url": "https://t.co/NkuNFkVjO3",
+					"display_url": "pic.twitter.com/NkuNFkVjO3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/883310376372158464/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 340,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 4000,
+						"variants": [
+							{
+								"bitrate": 320000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/883310314489487360/pu/vid/240x240/nbLXNfY6Vn5AHlwg.mp4"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/883310314489487360/pu/pl/IQyuEaNXSTOfEULd.m3u8"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/883310314489487360/pu/vid/480x480/EE4If8hqMSxGSgfx.mp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11408,
+			"friends_count": 7543,
+			"listed_count": 1017,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2483,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7347,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jul 05 09:13:33 +0000 2017",
 		"id": 882527859973042200,
 		"id_str": "882527859973042176",
