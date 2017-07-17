96861e053985f4e7933a91e9f4389d463f81be5a
debbiediscovers <debbiediscovers@gmail.com>
2017-07-17 21:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7992,
-	"created_at": "Mon Jul 17 09:17:51 +0000 2017",
+	"tweets": 7993,
+	"created_at": "Mon Jul 17 13:12:04 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,438 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Mon Jul 17 12:37:35 +0000 2017",
+		"id": 886927863017947100,
+		"id_str": "886927863017947137",
+		"text": "Who's watching @GameofThrones #WinterIsHere #GOTS7 😍 https://t.co/GYqv0qwiDd",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WinterIsHere",
+					"indices": [
+						30,
+						43
+					]
+				},
+				{
+					"text": "GOTS7",
+					"indices": [
+						44,
+						50
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GameOfThrones",
+					"name": "Game Of Thrones",
+					"id": 180463340,
+					"id_str": "180463340",
+					"indices": [
+						15,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GYqv0qwiDd",
+					"expanded_url": "https://twitter.com/lizroldancbs4/status/886750718690041856",
+					"display_url": "twitter.com/lizroldancbs4/…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/f7EuC7yqus",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/f7EuC7yqus",
+							"expanded_url": "http://www.digitaldiscovery.sg",
+							"display_url": "digitaldiscovery.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11961,
+			"friends_count": 7659,
+			"listed_count": 1016,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2526,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7347,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 886750718690041900,
+		"quoted_status_id_str": "886750718690041856",
+		"quoted_status": {
+			"created_at": "Mon Jul 17 00:53:41 +0000 2017",
+			"id": 886750718690041900,
+			"id_str": "886750718690041856",
+			"text": "Here. We. Go.\n#WinterIsHere @GameOfThrones #bringit \n https://t.co/UNmMXW3od1",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "WinterIsHere",
+						"indices": [
+							14,
+							27
+						]
+					},
+					{
+						"text": "bringit",
+						"indices": [
+							43,
+							51
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "GameOfThrones",
+						"name": "Game Of Thrones",
+						"id": 180463340,
+						"id_str": "180463340",
+						"indices": [
+							28,
+							42
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 885990768246235100,
+						"id_str": "885990768246235136",
+						"indices": [
+							54,
+							77
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/885990768246235136/img/czqDbLYTIVVoUyHc.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/885990768246235136/img/czqDbLYTIVVoUyHc.jpg",
+						"url": "https://t.co/UNmMXW3od1",
+						"display_url": "pic.twitter.com/UNmMXW3od1",
+						"expanded_url": "https://twitter.com/GameOfThrones/status/886617183333134336/video/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 340,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 640,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 600,
+								"h": 600,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 886617183333134300,
+						"source_status_id_str": "886617183333134336",
+						"source_user_id": 180463340,
+						"source_user_id_str": "180463340"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 885990768246235100,
+						"id_str": "885990768246235136",
+						"indices": [
+							54,
+							77
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/885990768246235136/img/czqDbLYTIVVoUyHc.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/885990768246235136/img/czqDbLYTIVVoUyHc.jpg",
+						"url": "https://t.co/UNmMXW3od1",
+						"display_url": "pic.twitter.com/UNmMXW3od1",
+						"expanded_url": "https://twitter.com/GameOfThrones/status/886617183333134336/video/1",
+						"type": "video",
+						"sizes": {
+							"small": {
+								"w": 340,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 640,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 600,
+								"h": 600,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 886617183333134300,
+						"source_status_id_str": "886617183333134336",
+						"source_user_id": 180463340,
+						"source_user_id_str": "180463340",
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 19353,
+							"variants": [
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/885990768246235136/vid/240x240/dzOo5FymJUvDfhWw.mp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/885990768246235136/vid/480x480/yc7F1gHbCgARZ8Bs.mp4"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/885990768246235136/pl/Xl-xhhoKYyJ4dtCj.m3u8"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "Tonight: Dragonstone (HBO)",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false,
+							"source_user": {
+								"id": 180463340,
+								"id_str": "180463340",
+								"name": "Game Of Thrones",
+								"screen_name": "GameOfThrones",
+								"location": "HBO",
+								"description": "Tweet what is yours. #GameofThrones Watch episodes of #GoT on today: https://t.co/QHfZbsHPd1",
+								"url": "https://t.co/9eJirryXIo",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "https://t.co/9eJirryXIo",
+												"expanded_url": "http://gameofthrones.com/",
+												"display_url": "gameofthrones.com",
+												"indices": [
+													0,
+													23
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": [
+											{
+												"url": "https://t.co/QHfZbsHPd1",
+												"expanded_url": "http://itsh.bo/PrepareForWinter",
+												"display_url": "itsh.bo/PrepareForWint…",
+												"indices": [
+													69,
+													92
+												]
+											}
+										]
+									}
+								},
+								"protected": false,
+								"followers_count": 5851601,
+								"friends_count": 40902,
+								"listed_count": 15350,
+								"created_at": "Thu Aug 19 18:04:32 +0000 2010",
+								"favourites_count": 8046,
+								"utc_offset": -25200,
+								"time_zone": "Pacific Time (US & Canada)",
+								"geo_enabled": true,
+								"verified": true,
+								"statuses_count": 58993,
+								"lang": "en",
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": true,
+								"profile_background_color": "000000",
+								"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/452793103464660993/ZpOaUveV.jpeg",
+								"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/452793103464660993/ZpOaUveV.jpeg",
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/867070777878859777/MtfeEkqy_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/867070777878859777/MtfeEkqy_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/180463340/1498496016",
+								"profile_link_color": "8C8457",
+								"profile_sidebar_border_color": "000000",
+								"profile_sidebar_fill_color": "0D0D0D",
+								"profile_text_color": "828282",
+								"profile_use_background_image": true,
+								"has_extended_profile": true,
+								"default_profile": false,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "regular"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 358756843,
+				"id_str": "358756843",
+				"name": "liz roldan",
+				"screen_name": "lizroldancbs4",
+				"location": "Miami, FL",
+				"description": "mother. journalist. Texan. University of Texas alum. diehard #Longhorn #TexasEx #HookEm #Texas. 🤘🏻news director @cbsmiami     Information is light.",
+				"url": "https://t.co/T3NlXpiEzt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/T3NlXpiEzt",
+								"expanded_url": "http://cbsmiami.com",
+								"display_url": "cbsmiami.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3961,
+				"friends_count": 1329,
+				"listed_count": 147,
+				"created_at": "Sat Aug 20 13:05:23 +0000 2011",
+				"favourites_count": 4652,
+				"utc_offset": -10800,
+				"time_zone": "Atlantic Time (Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 27219,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/328069611/IMG_0693.JPG",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/328069611/IMG_0693.JPG",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/460007623887446016/3iXaq3S3_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/460007623887446016/3iXaq3S3_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/358756843/1480196195",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 685,
+			"favorite_count": 2303,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Jul 17 09:14:35 +0000 2017",
 		"id": 886876776806228000,
 		"id_str": "886876776806227969",
