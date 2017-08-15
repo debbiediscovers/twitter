9d5914693619b3c14590667f88ba77d6fff6f57e
debbiediscovers <debbiediscovers@gmail.com>
2017-08-15 14:12:09 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8187,
-	"created_at": "Tue Aug 15 04:12:34 +0000 2017",
+	"tweets": 8188,
+	"created_at": "Tue Aug 15 06:12:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Tue Aug 15 06:08:01 +0000 2017",
+		"id": 897339073928413200,
+		"id_str": "897339073928413185",
+		"text": "@AskAaronLee @karizzle_bot @IKEAUSA My bot detection radar is *strong* 😬😋 https://t.co/2V1lMm5lGw",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AskAaronLee",
+					"name": "Aaron Lee",
+					"id": 25458378,
+					"id_str": "25458378",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "karizzle_bot",
+					"name": "Karizzle MarizzleBot",
+					"id": 2942470903,
+					"id_str": "2942470903",
+					"indices": [
+						13,
+						26
+					]
+				},
+				{
+					"screen_name": "IKEAUSA",
+					"name": "IKEA USA",
+					"id": 303827856,
+					"id_str": "303827856",
+					"indices": [
+						27,
+						35
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 897339020505514000,
+					"id_str": "897339020505513988",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DHP9FbhUAAQH0ox.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DHP9FbhUAAQH0ox.jpg",
+					"url": "https://t.co/2V1lMm5lGw",
+					"display_url": "pic.twitter.com/2V1lMm5lGw",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/897339073928413185/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 340,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 602,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 602,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 897339020505514000,
+					"id_str": "897339020505513988",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DHP9FbhUAAQH0ox.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DHP9FbhUAAQH0ox.jpg",
+					"url": "https://t.co/2V1lMm5lGw",
+					"display_url": "pic.twitter.com/2V1lMm5lGw",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/897339073928413185/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 340,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 602,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 602,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							240,
+							301
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DHP9FbhUAAQH0ox.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 897336625583104000,
+		"in_reply_to_status_id_str": "897336625583104000",
+		"in_reply_to_user_id": 25458378,
+		"in_reply_to_user_id_str": "25458378",
+		"in_reply_to_screen_name": "AskAaronLee",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. Teaching Kids to be Digital Storytellers and to Fundraise & Build Schools with Pencils of Promise",
+			"url": "https://t.co/f7EuC7yqus",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/f7EuC7yqus",
+							"expanded_url": "http://www.digitaldiscovery.sg",
+							"display_url": "digitaldiscovery.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14170,
+			"friends_count": 7568,
+			"listed_count": 1021,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 2763,
+			"utc_offset": -39600,
+			"time_zone": "International Date Line West",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7525,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695349571854036992/1g9ZjO5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Aug 15 04:07:05 +0000 2017",
 		"id": 897308640163528700,
 		"id_str": "897308640163528704",
