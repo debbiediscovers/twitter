6ddb5eee54276ae534b8655d70377d932c42aa64
debbiediscovers <debbiediscovers@gmail.com>
2017-08-29 20:12:09 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8530,
-	"created_at": "Tue Aug 29 11:12:12 +0000 2017",
+	"tweets": 8531,
+	"created_at": "Tue Aug 29 12:12:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_08.js",
 		"year": 2017,
 		"var_name": "tweets_2017_08",
-		"tweet_count": 483,
+		"tweet_count": 484,
 		"month": 8
 	},
 	{
