db74563d6e3d06abe37fc57df845bf5a5eba4bbf
debbiediscovers <debbiediscovers@gmail.com>
2017-09-05 12:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8694,
-	"created_at": "Tue Sep 05 02:12:04 +0000 2017",
+	"tweets": 8695,
+	"created_at": "Tue Sep 05 04:12:05 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_09.js
+++ data/js/tweets/2017_09.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2017_09 = 
 [
 	{
+		"created_at": "Tue Sep 05 03:22:40 +0000 2017",
+		"id": 904907607286079500,
+		"id_str": "904907607286079488",
+		"text": "#USEFUL! How $FB Ads Really Work: $FB Advertising Demystified https://t.co/Sdu5bj24tw via @ladderdigital Thanks @EdPlotts for sharing! 💖👍",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "USEFUL",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"symbols": [
+				{
+					"text": "FB",
+					"indices": [
+						13,
+						16
+					]
+				},
+				{
+					"text": "FB",
+					"indices": [
+						34,
+						37
+					]
+				}
+			],
+			"user_mentions": [
+				{
+					"screen_name": "ladderdigital",
+					"name": "Ladder.io",
+					"id": 2728977680,
+					"id_str": "2728977680",
+					"indices": [
+						90,
+						104
+					]
+				},
+				{
+					"screen_name": "EdPlotts",
+					"name": "Ed Plotts",
+					"id": 546356462,
+					"id_str": "546356462",
+					"indices": [
+						112,
+						121
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Sdu5bj24tw",
+					"expanded_url": "http://blog.ladder.io/facebook-ads/?utm_source=twitter&utm_medium=social&utm_campaign=sm_share",
+					"display_url": "blog.ladder.io/facebook-ads/?…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Central Region, Singapore",
+			"description": "Discovery Enthusiast, obsessed with Data, Digital and Design. @LittleCodersSG @GiftedSGP @0rchestras @DigitalFuturist are my passion projects!",
+			"url": "https://t.co/f7EuC7yqus",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/f7EuC7yqus",
+							"expanded_url": "http://www.digitaldiscovery.sg",
+							"display_url": "digitaldiscovery.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18102,
+			"friends_count": 9756,
+			"listed_count": 1041,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 3692,
+			"utc_offset": -39600,
+			"time_zone": "International Date Line West",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8012,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Sep 05 02:00:08 +0000 2017",
 		"id": 904886838636142600,
 		"id_str": "904886838636142593",
