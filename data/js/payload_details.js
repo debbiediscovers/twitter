800d1a8f09222a66d1d5d2b133d258bbf8eab174
debbiediscovers <debbiediscovers@gmail.com>
2017-11-04 19:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9506,
-	"created_at": "Sat Nov 04 10:12:05 +0000 2017",
+	"tweets": 9507,
+	"created_at": "Sat Nov 04 11:12:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,407 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Sat Nov 04 10:24:54 +0000 2017",
+		"id": 926757138403270700,
+		"id_str": "926757138403270656",
+		"text": "RT @Steph1lee: Who the Heck are #Digital Natives? Me, You, Your Kids? @xooloosoft_EN https://t.co/Guud1FFzGD https://t.co/2QqZDcix21",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Digital",
+					"indices": [
+						32,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Steph1lee",
+					"name": "Steph L Lee",
+					"id": 721241974880120800,
+					"id_str": "721241974880120832",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "xooloosoft_EN",
+					"name": "Xooloo",
+					"id": 912783770,
+					"id_str": "912783770",
+					"indices": [
+						70,
+						84
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Guud1FFzGD",
+					"expanded_url": "https://buff.ly/2zcd9x5",
+					"display_url": "buff.ly/2zcd9x5",
+					"indices": [
+						85,
+						108
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 926756666061623300,
+					"id_str": "926756666061623300",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+					"url": "https://t.co/2QqZDcix21",
+					"display_url": "pic.twitter.com/2QqZDcix21",
+					"expanded_url": "https://twitter.com/Steph1lee/status/926756668217745408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1025,
+							"h": 682,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1025,
+							"h": 682,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 926756668217745400,
+					"source_status_id_str": "926756668217745408",
+					"source_user_id": 721241974880120800,
+					"source_user_id_str": "721241974880120832"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 926756666061623300,
+					"id_str": "926756666061623300",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+					"url": "https://t.co/2QqZDcix21",
+					"display_url": "pic.twitter.com/2QqZDcix21",
+					"expanded_url": "https://twitter.com/Steph1lee/status/926756668217745408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1025,
+							"h": 682,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1025,
+							"h": 682,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 926756668217745400,
+					"source_status_id_str": "926756668217745408",
+					"source_user_id": 721241974880120800,
+					"source_user_id_str": "721241974880120832"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Lipstick entrepreneur. Chatbot builder. Sailing enthusiast. Insatiably curious. @LittleCodersSG @GiftedSGP @0rchestras @DigitalFuturist are my passion projects!",
+			"url": "https://t.co/iftpTVexLs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iftpTVexLs",
+							"expanded_url": "https://about.me/debbiediscovers",
+							"display_url": "about.me/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33280,
+			"friends_count": 22242,
+			"listed_count": 974,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 4836,
+			"utc_offset": -39600,
+			"time_zone": "International Date Line West",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8791,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Nov 04 10:23:02 +0000 2017",
+			"id": 926756668217745400,
+			"id_str": "926756668217745408",
+			"text": "Who the Heck are #Digital Natives? Me, You, Your Kids? @xooloosoft_EN https://t.co/Guud1FFzGD https://t.co/2QqZDcix21",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Digital",
+						"indices": [
+							17,
+							25
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "xooloosoft_EN",
+						"name": "Xooloo",
+						"id": 912783770,
+						"id_str": "912783770",
+						"indices": [
+							55,
+							69
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Guud1FFzGD",
+						"expanded_url": "https://buff.ly/2zcd9x5",
+						"display_url": "buff.ly/2zcd9x5",
+						"indices": [
+							70,
+							93
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 926756666061623300,
+						"id_str": "926756666061623300",
+						"indices": [
+							94,
+							117
+						],
+						"media_url": "http://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+						"url": "https://t.co/2QqZDcix21",
+						"display_url": "pic.twitter.com/2QqZDcix21",
+						"expanded_url": "https://twitter.com/Steph1lee/status/926756668217745408/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 452,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1025,
+								"h": 682,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1025,
+								"h": 682,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 926756666061623300,
+						"id_str": "926756666061623300",
+						"indices": [
+							94,
+							117
+						],
+						"media_url": "http://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DNyARxHUEAQtT4c.jpg",
+						"url": "https://t.co/2QqZDcix21",
+						"display_url": "pic.twitter.com/2QqZDcix21",
+						"expanded_url": "https://twitter.com/Steph1lee/status/926756668217745408/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 452,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1025,
+								"h": 682,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1025,
+								"h": 682,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 721241974880120800,
+				"id_str": "721241974880120832",
+				"name": "Steph L Lee",
+				"screen_name": "Steph1lee",
+				"location": "Singapore",
+				"description": "Social Media Enthusiast, Parent, Mom",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 690,
+				"friends_count": 926,
+				"listed_count": 19,
+				"created_at": "Sat Apr 16 07:41:01 +0000 2016",
+				"favourites_count": 48,
+				"utc_offset": 28800,
+				"time_zone": "Singapore",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 205,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/726518533476114433/7s2BXRRa_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/726518533476114433/7s2BXRRa_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/721241974880120832/1461865216",
+				"profile_link_color": "F58EA8",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Nov 04 10:02:00 +0000 2017",
 		"id": 926751375274344400,
 		"id_str": "926751375274344448",
