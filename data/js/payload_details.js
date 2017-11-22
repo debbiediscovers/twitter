e6ae7fda9fc4472b0e857a6fa9e676c845ddab6c
debbiediscovers <debbiediscovers@gmail.com>
2017-11-23 04:12:07 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9796,
-	"created_at": "Wed Nov 22 10:12:04 +0000 2017",
+	"tweets": 9797,
+	"created_at": "Wed Nov 22 20:12:05 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Wed Nov 22 19:32:49 +0000 2017",
+		"id": 933418006520070100,
+		"id_str": "933418006520070144",
+		"text": "RT @PassionateMktrs: How to access Twitter's API - A Guide for Non Developers https://t.co/WUoeoBQVF1 @debbiediscovers https://t.co/vxKv4Fu…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PassionateMktrs",
+					"name": "Passionate Marketers",
+					"id": 893062065350258700,
+					"id_str": "893062065350258688",
+					"indices": [
+						3,
+						19
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						102,
+						118
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/WUoeoBQVF1",
+					"expanded_url": "https://buff.ly/2zYTu6V",
+					"display_url": "buff.ly/2zYTu6V",
+					"indices": [
+						78,
+						101
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Lipstick entrepreneur. Chatbot builder. Sailing enthusiast. Insatiably curious. @LittleCodersSG @GiftedSGP @0rchestras @DigitalFuturist are my passion projects!",
+			"url": "https://t.co/IprDI3V3T9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IprDI3V3T9",
+							"expanded_url": "https://digitaldiscovery.sg/about-deborah-kay/",
+							"display_url": "digitaldiscovery.sg/about-deborah-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34824,
+			"friends_count": 13401,
+			"listed_count": 989,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 7371,
+			"utc_offset": -39600,
+			"time_zone": "International Date Line West",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9068,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Nov 22 19:01:46 +0000 2017",
+			"id": 933410191189700600,
+			"id_str": "933410191189700608",
+			"text": "How to access Twitter's API - A Guide for Non Developers https://t.co/WUoeoBQVF1 @debbiediscovers https://t.co/vxKv4FuVYR",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "debbiediscovers",
+						"name": "Deborah Kay",
+						"id": 3060836348,
+						"id_str": "3060836348",
+						"indices": [
+							81,
+							97
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/WUoeoBQVF1",
+						"expanded_url": "https://buff.ly/2zYTu6V",
+						"display_url": "buff.ly/2zYTu6V",
+						"indices": [
+							57,
+							80
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 933410187708219400,
+						"id_str": "933410187708219393",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DPQjnswUQAE8U-c.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPQjnswUQAE8U-c.jpg",
+						"url": "https://t.co/vxKv4FuVYR",
+						"display_url": "pic.twitter.com/vxKv4FuVYR",
+						"expanded_url": "https://twitter.com/PassionateMktrs/status/933410191189700608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 933410187708219400,
+						"id_str": "933410187708219393",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DPQjnswUQAE8U-c.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPQjnswUQAE8U-c.jpg",
+						"url": "https://t.co/vxKv4FuVYR",
+						"display_url": "pic.twitter.com/vxKv4FuVYR",
+						"expanded_url": "https://twitter.com/PassionateMktrs/status/933410191189700608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 893062065350258700,
+				"id_str": "893062065350258688",
+				"name": "Passionate Marketers",
+				"screen_name": "PassionateMktrs",
+				"location": "Philadelphia, PA",
+				"description": "A community for #marketers.  When you love what you do. #passionatemarketers",
+				"url": "https://t.co/wkfYs3tQPs",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wkfYs3tQPs",
+								"expanded_url": "http://www.passionatemarketers.com",
+								"display_url": "passionatemarketers.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1505,
+				"friends_count": 1260,
+				"listed_count": 10,
+				"created_at": "Thu Aug 03 10:52:43 +0000 2017",
+				"favourites_count": 124,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 238,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/918826190537867264/gsJ7ZycA_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/918826190537867264/gsJ7ZycA_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/893062065350258688/1508156805",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Nov 22 10:06:52 +0000 2017",
 		"id": 933275580404965400,
 		"id_str": "933275580404965382",
