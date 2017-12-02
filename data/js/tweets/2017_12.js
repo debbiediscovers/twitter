506cb6c29a2a745ebae36773ef53154a8eacaa8d
debbiediscovers <debbiediscovers@gmail.com>
2017-12-03 01:12:03 +0800

    Added 1 tweets

--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,404 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Sat Dec 02 16:16:34 +0000 2017",
+		"id": 936992495585468400,
+		"id_str": "936992495585468416",
+		"text": "RT @Stellar247: How to access Twitter's API - A Guide for Non Developers https://t.co/DPcQlfIqoQ @debbiediscovers https://t.co/TuZcLiKyN1",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Stellar247",
+					"name": "Kelly Noble Mirabella",
+					"id": 28188070,
+					"id_str": "28188070",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						97,
+						113
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DPcQlfIqoQ",
+					"expanded_url": "https://buff.ly/2ACElIz",
+					"display_url": "buff.ly/2ACElIz",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 936990094715772900,
+					"id_str": "936990094715772928",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+					"url": "https://t.co/TuZcLiKyN1",
+					"display_url": "pic.twitter.com/TuZcLiKyN1",
+					"expanded_url": "https://twitter.com/Stellar247/status/936990099732094977/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 936990099732095000,
+					"source_status_id_str": "936990099732094977",
+					"source_user_id": 28188070,
+					"source_user_id_str": "28188070"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 936990094715772900,
+					"id_str": "936990094715772928",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+					"url": "https://t.co/TuZcLiKyN1",
+					"display_url": "pic.twitter.com/TuZcLiKyN1",
+					"expanded_url": "https://twitter.com/Stellar247/status/936990099732094977/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 936990099732095000,
+					"source_status_id_str": "936990099732094977",
+					"source_user_id": 28188070,
+					"source_user_id_str": "28188070"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur #Bottish100 Power 100 #GlobalEducation advocate @SingaporeSail @DigitalFuturist @LittleCodersSG @GiftedSGP Love Twitter, allergic to stupid tweets",
+			"url": "https://t.co/IprDI3V3T9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IprDI3V3T9",
+							"expanded_url": "https://digitaldiscovery.sg/about-deborah-kay/",
+							"display_url": "digitaldiscovery.sg/about-deborah-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35618,
+			"friends_count": 13603,
+			"listed_count": 1010,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 8554,
+			"utc_offset": -39600,
+			"time_zone": "International Date Line West",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9376,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Dec 02 16:07:02 +0000 2017",
+			"id": 936990099732095000,
+			"id_str": "936990099732094977",
+			"text": "How to access Twitter's API - A Guide for Non Developers https://t.co/DPcQlfIqoQ @debbiediscovers https://t.co/TuZcLiKyN1",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "debbiediscovers",
+						"name": "Deborah Kay",
+						"id": 3060836348,
+						"id_str": "3060836348",
+						"indices": [
+							81,
+							97
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/DPcQlfIqoQ",
+						"expanded_url": "https://buff.ly/2ACElIz",
+						"display_url": "buff.ly/2ACElIz",
+						"indices": [
+							57,
+							80
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 936990094715772900,
+						"id_str": "936990094715772928",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+						"url": "https://t.co/TuZcLiKyN1",
+						"display_url": "pic.twitter.com/TuZcLiKyN1",
+						"expanded_url": "https://twitter.com/Stellar247/status/936990099732094977/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 936990094715772900,
+						"id_str": "936990094715772928",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQDbhtmX4AAx1ae.jpg",
+						"url": "https://t.co/TuZcLiKyN1",
+						"display_url": "pic.twitter.com/TuZcLiKyN1",
+						"expanded_url": "https://twitter.com/Stellar247/status/936990099732094977/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 28188070,
+				"id_str": "28188070",
+				"name": "Kelly Noble Mirabella",
+				"screen_name": "Stellar247",
+				"location": "Santa Clarita, CA",
+				"description": "Social media consultant, trainer and enthusiast. Passionate about helping people use social media for business. I also love to cook, travel and read.",
+				"url": "https://t.co/WabHA6mckg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WabHA6mckg",
+								"expanded_url": "http://stellar247.com",
+								"display_url": "stellar247.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4033,
+				"friends_count": 950,
+				"listed_count": 862,
+				"created_at": "Wed Apr 01 20:31:40 +0000 2009",
+				"favourites_count": 2956,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 26608,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/910297968586010624/zvLS8iIC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/910297968586010624/zvLS8iIC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/28188070/1506816117",
+				"profile_link_color": "18ADA9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Dec 02 16:00:30 +0000 2017",
 		"id": 936988452146606100,
 		"id_str": "936988452146606080",
