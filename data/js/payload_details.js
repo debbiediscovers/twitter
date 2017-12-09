16bbf496a280bac55184070579ff518b046192e8
debbiediscovers <debbiediscovers@gmail.com>
2017-12-10 06:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10295,
-	"created_at": "Sat Dec 09 18:12:04 +0000 2017",
+	"tweets": 10296,
+	"created_at": "Sat Dec 09 22:12:04 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,404 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Sat Dec 09 21:55:15 +0000 2017",
+		"id": 939614445436674000,
+		"id_str": "939614445436674048",
+		"text": "RT @RSMktgAlly: How to access Twitter's API - A Guide for Non Developers https://t.co/n9BgOas6aO @debbiediscovers https://t.co/bD4omLohCO",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RSMktgAlly",
+					"name": "R&S Marketing",
+					"id": 1855338084,
+					"id_str": "1855338084",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						97,
+						113
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/n9BgOas6aO",
+					"expanded_url": "https://buff.ly/2nN7ifU",
+					"display_url": "buff.ly/2nN7ifU",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 939586754939703300,
+					"id_str": "939586754939703296",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+					"url": "https://t.co/bD4omLohCO",
+					"display_url": "pic.twitter.com/bD4omLohCO",
+					"expanded_url": "https://twitter.com/RSMktgAlly/status/939586758785761280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 939586758785761300,
+					"source_status_id_str": "939586758785761280",
+					"source_user_id": 1855338084,
+					"source_user_id_str": "1855338084"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 939586754939703300,
+					"id_str": "939586754939703296",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+					"url": "https://t.co/bD4omLohCO",
+					"display_url": "pic.twitter.com/bD4omLohCO",
+					"expanded_url": "https://twitter.com/RSMktgAlly/status/939586758785761280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 939586758785761300,
+					"source_status_id_str": "939586758785761280",
+					"source_user_id": 1855338084,
+					"source_user_id_str": "1855338084"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, #Bottish100 Power 100, #GlobalGoals advocate, @SingaporeSail @DigitalFuturist @LittleCodersSG @GiftedSGP Love Twitter, allergic to stupid tweets",
+			"url": "https://t.co/IprDI3V3T9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IprDI3V3T9",
+							"expanded_url": "https://digitaldiscovery.sg/about-deborah-kay/",
+							"display_url": "digitaldiscovery.sg/about-deborah-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35196,
+			"friends_count": 12588,
+			"listed_count": 1010,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 8900,
+			"utc_offset": -39600,
+			"time_zone": "Midway Island",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9530,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Dec 09 20:05:14 +0000 2017",
+			"id": 939586758785761300,
+			"id_str": "939586758785761280",
+			"text": "How to access Twitter's API - A Guide for Non Developers https://t.co/n9BgOas6aO @debbiediscovers https://t.co/bD4omLohCO",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "debbiediscovers",
+						"name": "Deborah Kay",
+						"id": 3060836348,
+						"id_str": "3060836348",
+						"indices": [
+							81,
+							97
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/n9BgOas6aO",
+						"expanded_url": "https://buff.ly/2nN7ifU",
+						"display_url": "buff.ly/2nN7ifU",
+						"indices": [
+							57,
+							80
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 939586754939703300,
+						"id_str": "939586754939703296",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+						"url": "https://t.co/bD4omLohCO",
+						"display_url": "pic.twitter.com/bD4omLohCO",
+						"expanded_url": "https://twitter.com/RSMktgAlly/status/939586758785761280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 939586754939703300,
+						"id_str": "939586754939703296",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQoVLOvX0AAO6VV.jpg",
+						"url": "https://t.co/bD4omLohCO",
+						"display_url": "pic.twitter.com/bD4omLohCO",
+						"expanded_url": "https://twitter.com/RSMktgAlly/status/939586758785761280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1855338084,
+				"id_str": "1855338084",
+				"name": "R&S Marketing",
+				"screen_name": "RSMktgAlly",
+				"location": "Lake Forest, CA",
+				"description": "R&S Marketing specializes in helping small and medium size businesses get more clients with cutting edge online marketing strategies.",
+				"url": "http://t.co/3mumdw8dPF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/3mumdw8dPF",
+								"expanded_url": "http://www.rsmarketingally.com/",
+								"display_url": "rsmarketingally.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 554,
+				"friends_count": 412,
+				"listed_count": 157,
+				"created_at": "Wed Sep 11 20:25:10 +0000 2013",
+				"favourites_count": 36,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2793,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/857584459167653888/dGiN_vwE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/857584459167653888/dGiN_vwE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1855338084/1425791250",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Dec 09 17:20:03 +0000 2017",
 		"id": 939545187302412300,
 		"id_str": "939545187302412288",
