c24b349476bc09e2a299b53070d29f53cdb2d605
debbiediscovers <debbiediscovers@gmail.com>
2017-12-23 00:12:13 +0800

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_12.js",
 		"year": 2017,
 		"var_name": "tweets_2017_12",
-		"tweet_count": 571,
+		"tweet_count": 573,
 		"month": 12
 	},
 	{
