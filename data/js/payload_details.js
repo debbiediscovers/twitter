7799c5b262445e640c4fbe264306051f996be6ff
debbiediscovers <debbiediscovers@gmail.com>
2018-01-24 11:12:11 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11463,
-	"created_at": "Wed Jan 24 01:12:08 +0000 2018",
+	"tweets": 11464,
+	"created_at": "Wed Jan 24 03:12:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,107 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Wed Jan 24 02:30:28 +0000 2018",
+		"id": 955991158852796400,
+		"id_str": "955991158852796417",
+		"text": "@GrumpyAlien 💪💪💪",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GrumpyAlien",
+					"name": "Eat Bad",
+					"id": 1054574228,
+					"id_str": "1054574228",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 955823735394717700,
+		"in_reply_to_status_id_str": "955823735394717696",
+		"in_reply_to_user_id": 1054574228,
+		"in_reply_to_user_id_str": "1054574228",
+		"in_reply_to_screen_name": "GrumpyAlien",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are some of my bots",
+			"url": "https://t.co/IprDI3V3T9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IprDI3V3T9",
+							"expanded_url": "https://digitaldiscovery.sg/about-deborah-kay/",
+							"display_url": "digitaldiscovery.sg/about-deborah-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37472,
+			"friends_count": 14429,
+			"listed_count": 1055,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 10865,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10580,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1454619192",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Wed Jan 24 00:22:09 +0000 2018",
 		"id": 955958868873568300,
 		"id_str": "955958868873568256",
@@ -138,7 +239,7 @@ Grailbird.data.tweets_2018_01 =
 				"name": "Eat Bad",
 				"screen_name": "GrumpyAlien",
 				"location": "Palmela, Portugal",
-				"description": "A journey of learning and discovery, nutrition🍳 and fitness🏋️‍♀️. Understand your body's mechanisms and change your life forever! 🤩🤩",
+				"description": "A journey of learning and discovery, nutrition🍳 and fitness🏋️♀️. Understand your body's mechanisms and change your life forever! 🤩🤩",
 				"url": "https://t.co/ELrDMWzvJt",
 				"entities": {
 					"url": {
