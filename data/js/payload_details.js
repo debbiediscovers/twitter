35cda393eb49872efdd5ad156148b5b3be0208c7
debbiediscovers <debbiediscovers@gmail.com>
2018-01-31 07:12:12 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11626,
-	"created_at": "Tue Jan 30 17:12:21 +0000 2018",
+	"tweets": 11627,
+	"created_at": "Tue Jan 30 23:12:11 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,107 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Tue Jan 30 22:53:34 +0000 2018",
+		"id": 958473288451108900,
+		"id_str": "958473288451108864",
+		"text": "@MitsukuChatbot Oh WOW!!! Fingers crossed 🤞",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MitsukuChatbot",
+					"name": "Mitsuku 🏅🏅🏅",
+					"id": 578224522,
+					"id_str": "578224522",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 958455844676472800,
+		"in_reply_to_status_id_str": "958455844676472833",
+		"in_reply_to_user_id": 578224522,
+		"in_reply_to_user_id_str": "578224522",
+		"in_reply_to_screen_name": "MitsukuChatbot",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are some of my bots",
+			"url": "https://t.co/1ZPsVnusbG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnusbG",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37255,
+			"friends_count": 16366,
+			"listed_count": 1057,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11004,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10738,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jan 30 16:37:38 +0000 2018",
 		"id": 958378681654689800,
 		"id_str": "958378681654689792",
@@ -12,7 +113,7 @@ Grailbird.data.tweets_2018_01 =
 			"user_mentions": [
 				{
 					"screen_name": "nebskii",
-					"name": "Obezyanka🐒 #FBPE 🇪🇺🏳️‍🌈",
+					"name": "Obezyanka🐒 #FBPE 🇪🇺🏳️🌈",
 					"id": 21617980,
 					"id_str": "21617980",
 					"indices": [
@@ -136,7 +237,7 @@ Grailbird.data.tweets_2018_01 =
 			"user": {
 				"id": 21617980,
 				"id_str": "21617980",
-				"name": "Obezyanka🐒 #FBPE 🇪🇺🏳️‍🌈",
+				"name": "Obezyanka🐒 #FBPE 🇪🇺🏳️🌈",
 				"screen_name": "nebskii",
 				"location": "Manchester, England",
 				"description": "Intl. adventurer, multilinguist & tech geek. 👉Photography, web design, graphic design, online marketing.\n\n#liberal #proEU #DumpTrump #gay\n\n✍️🇬🇧🇫🇷🇧🇪🇷🇺",
