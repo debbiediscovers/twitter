077273f3f23a1e585be367d78d92c6c6e05f5eda
debbiediscovers <debbiediscovers@gmail.com>
2018-02-01 12:12:03 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11655,
-	"created_at": "Thu Feb 01 03:13:15 +0000 2018",
+	"tweets": 11657,
+	"created_at": "Thu Feb 01 04:12:01 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,663 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Thu Feb 01 03:44:20 +0000 2018",
+		"id": 958908850949599200,
+		"id_str": "958908850949599233",
+		"text": "RT @BuzzFeedNews: The Statue of Liberty backdropped by the #SuperBlueBloodMoon (📸: AP) https://t.co/2zCInK3oKC",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SuperBlueBloodMoon",
+					"indices": [
+						59,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BuzzFeedNews",
+					"name": "BuzzFeed News",
+					"id": 1020058453,
+					"id_str": "1020058453",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 958691470927695900,
+					"id_str": "958691470927695874",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+					"url": "https://t.co/2zCInK3oKC",
+					"display_url": "pic.twitter.com/2zCInK3oKC",
+					"expanded_url": "https://twitter.com/BuzzFeedNews/status/958692760248356864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 958692760248356900,
+					"source_status_id_str": "958692760248356864",
+					"source_user_id": 1020058453,
+					"source_user_id_str": "1020058453"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 958691470927695900,
+					"id_str": "958691470927695874",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+					"url": "https://t.co/2zCInK3oKC",
+					"display_url": "pic.twitter.com/2zCInK3oKC",
+					"expanded_url": "https://twitter.com/BuzzFeedNews/status/958692760248356864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 958692760248356900,
+					"source_status_id_str": "958692760248356864",
+					"source_user_id": 1020058453,
+					"source_user_id_str": "1020058453"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are some of my bots",
+			"url": "https://t.co/1ZPsVnusbG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnusbG",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37254,
+			"friends_count": 16695,
+			"listed_count": 1053,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11029,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10768,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jan 31 13:25:40 +0000 2018",
+			"id": 958692760248356900,
+			"id_str": "958692760248356864",
+			"text": "The Statue of Liberty backdropped by the #SuperBlueBloodMoon (📸: AP) https://t.co/2zCInK3oKC",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SuperBlueBloodMoon",
+						"indices": [
+							41,
+							60
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 958691470927695900,
+						"id_str": "958691470927695874",
+						"indices": [
+							69,
+							92
+						],
+						"media_url": "http://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+						"url": "https://t.co/2zCInK3oKC",
+						"display_url": "pic.twitter.com/2zCInK3oKC",
+						"expanded_url": "https://twitter.com/BuzzFeedNews/status/958692760248356864/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 958691470927695900,
+						"id_str": "958691470927695874",
+						"indices": [
+							69,
+							92
+						],
+						"media_url": "http://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DU300D3XkAI0BiP.jpg",
+						"url": "https://t.co/2zCInK3oKC",
+						"display_url": "pic.twitter.com/2zCInK3oKC",
+						"expanded_url": "https://twitter.com/BuzzFeedNews/status/958692760248356864/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1020058453,
+				"id_str": "1020058453",
+				"name": "BuzzFeed News",
+				"screen_name": "BuzzFeedNews",
+				"location": "",
+				"description": "The most trusted @ username in news. Download the BuzzFeed News App:\n\niOS: https://t.co/00gxs1sUXe\n\nAndroid: https://t.co/tufohzgWtp",
+				"url": "https://t.co/I6ADKX77mj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/I6ADKX77mj",
+								"expanded_url": "http://www.buzzfeed.com/news",
+								"display_url": "buzzfeed.com/news",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/00gxs1sUXe",
+								"expanded_url": "http://bzfd.it/2mAOorw",
+								"display_url": "bzfd.it/2mAOorw",
+								"indices": [
+									75,
+									98
+								]
+							},
+							{
+								"url": "https://t.co/tufohzgWtp",
+								"expanded_url": "http://bzfd.it/2n0Pf03",
+								"display_url": "bzfd.it/2n0Pf03",
+								"indices": [
+									109,
+									132
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1157174,
+				"friends_count": 4780,
+				"listed_count": 10946,
+				"created_at": "Tue Dec 18 15:46:37 +0000 2012",
+				"favourites_count": 1561,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 124344,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/796353157576138752/H2xr-NkC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/796353157576138752/H2xr-NkC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1020058453/1493406455",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 973,
+			"favorite_count": 4178,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 973,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Feb 01 03:42:24 +0000 2018",
+		"id": 958908363626000400,
+		"id_str": "958908363626000384",
+		"text": "RT @epocalibera: Time lapse: A super blue blood moon rises behind Parthenon, #Acropolis of #Athens #Greece on 31 January 2018   #SuperBlueB…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Acropolis",
+					"indices": [
+						77,
+						87
+					]
+				},
+				{
+					"text": "Athens",
+					"indices": [
+						91,
+						98
+					]
+				},
+				{
+					"text": "Greece",
+					"indices": [
+						99,
+						106
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "epocalibera",
+					"name": "epoca libera",
+					"id": 248199288,
+					"id_str": "248199288",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are some of my bots",
+			"url": "https://t.co/1ZPsVnusbG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnusbG",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37254,
+			"friends_count": 16695,
+			"listed_count": 1053,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11029,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10768,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jan 31 20:04:40 +0000 2018",
+			"id": 958793174218084400,
+			"id_str": "958793174218084352",
+			"text": "Time lapse: A super blue blood moon rises behind Parthenon, #Acropolis of #Athens #Greece on 31 January 2018… https://t.co/0xMWHTSXr7",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Acropolis",
+						"indices": [
+							60,
+							70
+						]
+					},
+					{
+						"text": "Athens",
+						"indices": [
+							74,
+							81
+						]
+					},
+					{
+						"text": "Greece",
+						"indices": [
+							82,
+							89
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/0xMWHTSXr7",
+						"expanded_url": "https://twitter.com/i/web/status/958793174218084352",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							110,
+							133
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 248199288,
+				"id_str": "248199288",
+				"name": "epoca libera",
+				"screen_name": "epocalibera",
+				"location": "Athens - Greece",
+				"description": "Απεικόνιση - #Filmmaker\n“Retweets are not endorsements”\nhttps://t.co/jrAA7itAEh https://t.co/OTPdQ4T0Kp",
+				"url": "http://t.co/Z1hSmZLXhw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/Z1hSmZLXhw",
+								"expanded_url": "http://www.epocalibera.com",
+								"display_url": "epocalibera.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/jrAA7itAEh",
+								"expanded_url": "http://www.facebook.com/epocalibera",
+								"display_url": "facebook.com/epocalibera",
+								"indices": [
+									56,
+									79
+								]
+							},
+							{
+								"url": "https://t.co/OTPdQ4T0Kp",
+								"expanded_url": "https://www.youtube.com/epocalibera",
+								"display_url": "youtube.com/epocalibera",
+								"indices": [
+									80,
+									103
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 981,
+				"friends_count": 712,
+				"listed_count": 33,
+				"created_at": "Sun Feb 06 13:36:21 +0000 2011",
+				"favourites_count": 4640,
+				"utc_offset": 7200,
+				"time_zone": "Athens",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1169,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "2F3030",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2212580188/epoca-libera_avatar_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2212580188/epoca-libera_avatar_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/248199288/1422284265",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3993,
+			"favorite_count": 5825,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3993,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Feb 01 02:00:29 +0000 2018",
 		"id": 958882715981500400,
 		"id_str": "958882715981500416",
