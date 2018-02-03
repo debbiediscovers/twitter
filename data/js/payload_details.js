ed4d8a8ad95f3195db04d16c6eb08fcab8095c7e
debbiediscovers <debbiediscovers@gmail.com>
2018-02-03 15:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11690,
-	"created_at": "Sat Feb 03 02:12:01 +0000 2018",
+	"tweets": 11691,
+	"created_at": "Sat Feb 03 07:12:01 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,391 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Sat Feb 03 06:17:55 +0000 2018",
+		"id": 959672275879342100,
+		"id_str": "959672275879342080",
+		"text": "RT @ceoericla: What Analytics do you get from Twitter's API? https://t.co/OffuNuSYT2 via @debbiediscovers https://t.co/4CtToUtHNx",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ceoericla",
+					"name": "Eric N",
+					"id": 3181077847,
+					"id_str": "3181077847",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						89,
+						105
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OffuNuSYT2",
+					"expanded_url": "https://buff.ly/2CKA2Ja",
+					"display_url": "buff.ly/2CKA2Ja",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 959661735643934700,
+					"id_str": "959661735643934721",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+					"url": "https://t.co/4CtToUtHNx",
+					"display_url": "pic.twitter.com/4CtToUtHNx",
+					"expanded_url": "https://twitter.com/ceoericla/status/959661738047234049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 959661738047234000,
+					"source_status_id_str": "959661738047234049",
+					"source_user_id": 3181077847,
+					"source_user_id_str": "3181077847"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 959661735643934700,
+					"id_str": "959661735643934721",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+					"url": "https://t.co/4CtToUtHNx",
+					"display_url": "pic.twitter.com/4CtToUtHNx",
+					"expanded_url": "https://twitter.com/ceoericla/status/959661738047234049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 959661738047234000,
+					"source_status_id_str": "959661738047234049",
+					"source_user_id": 3181077847,
+					"source_user_id_str": "3181077847"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are some of my bots",
+			"url": "https://t.co/1ZPsVnusbG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnusbG",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37343,
+			"friends_count": 17701,
+			"listed_count": 1057,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11058,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10802,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Feb 03 05:36:02 +0000 2018",
+			"id": 959661738047234000,
+			"id_str": "959661738047234049",
+			"text": "What Analytics do you get from Twitter's API? https://t.co/OffuNuSYT2 via @debbiediscovers https://t.co/4CtToUtHNx",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "debbiediscovers",
+						"name": "Deborah Kay",
+						"id": 3060836348,
+						"id_str": "3060836348",
+						"indices": [
+							74,
+							90
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/OffuNuSYT2",
+						"expanded_url": "https://buff.ly/2CKA2Ja",
+						"display_url": "buff.ly/2CKA2Ja",
+						"indices": [
+							46,
+							69
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 959661735643934700,
+						"id_str": "959661735643934721",
+						"indices": [
+							91,
+							114
+						],
+						"media_url": "http://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+						"url": "https://t.co/4CtToUtHNx",
+						"display_url": "pic.twitter.com/4CtToUtHNx",
+						"expanded_url": "https://twitter.com/ceoericla/status/959661738047234049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 959661735643934700,
+						"id_str": "959661735643934721",
+						"indices": [
+							91,
+							114
+						],
+						"media_url": "http://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVFnQ5mXcAE056M.jpg",
+						"url": "https://t.co/4CtToUtHNx",
+						"display_url": "pic.twitter.com/4CtToUtHNx",
+						"expanded_url": "https://twitter.com/ceoericla/status/959661738047234049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3181077847,
+				"id_str": "3181077847",
+				"name": "Eric N",
+				"screen_name": "ceoericla",
+				"location": "Los Angeles, CA",
+				"description": "CEO of a Los Angeles-based Creative #Media Agency | Join me as I talk, share, & ramble about all things Digital, #Business, Customer Service & more.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 841,
+				"friends_count": 2366,
+				"listed_count": 16,
+				"created_at": "Thu Apr 30 22:30:52 +0000 2015",
+				"favourites_count": 459,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 693,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/907451324786106369/vUrT10Cr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/907451324786106369/vUrT10Cr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3181077847/1505652874",
+				"profile_link_color": "264866",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Feb 03 02:08:12 +0000 2018",
 		"id": 959609434417909800,
 		"id_str": "959609434417909760",
