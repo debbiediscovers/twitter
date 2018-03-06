9dc775263bd9e6f3cb1117b0ac19929c35c97cc3
debbiediscovers <debbiediscovers@gmail.com>
2018-03-07 05:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12400,
-	"created_at": "Tue Mar 06 18:12:03 +0000 2018",
+	"tweets": 12401,
+	"created_at": "Tue Mar 06 21:12:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,249 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Tue Mar 06 20:34:43 +0000 2018",
+		"id": 971121923169058800,
+		"id_str": "971121923169058816",
+		"text": "RT @Travellers_MY: An Instagram Star's Insider Tips for Traveling the World on a Budget #Travel #BudgetTravel https://t.co/X2wCcTjKlF",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Travel",
+					"indices": [
+						88,
+						95
+					]
+				},
+				{
+					"text": "BudgetTravel",
+					"indices": [
+						96,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Travellers_MY",
+					"name": "Travellers of Malaysia",
+					"id": 966254500640993300,
+					"id_str": "966254500640993280",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/X2wCcTjKlF",
+					"expanded_url": "https://buff.ly/2GwdoFC",
+					"display_url": "buff.ly/2GwdoFC",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 39264,
+			"friends_count": 24573,
+			"listed_count": 1076,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11548,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11474,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Mar 06 07:58:00 +0000 2018",
+			"id": 970931487611768800,
+			"id_str": "970931487611768832",
+			"text": "An Instagram Star's Insider Tips for Traveling the World on a Budget #Travel #BudgetTravel https://t.co/X2wCcTjKlF",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Travel",
+						"indices": [
+							69,
+							76
+						]
+					},
+					{
+						"text": "BudgetTravel",
+						"indices": [
+							77,
+							90
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/X2wCcTjKlF",
+						"expanded_url": "https://buff.ly/2GwdoFC",
+						"display_url": "buff.ly/2GwdoFC",
+						"indices": [
+							91,
+							114
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 966254500640993300,
+				"id_str": "966254500640993280",
+				"name": "Travellers of Malaysia",
+				"screen_name": "Travellers_MY",
+				"location": "Malaysia",
+				"description": "Book now, pay later. EarlyBooker is the first travel by instalment provider for Savvy Millennials #BudgetTravelSmarter",
+				"url": "https://t.co/5T82RX3A7F",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5T82RX3A7F",
+								"expanded_url": "https://earlybooker.co/",
+								"display_url": "earlybooker.co",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 210,
+				"friends_count": 1783,
+				"listed_count": 1,
+				"created_at": "Wed Feb 21 10:13:19 +0000 2018",
+				"favourites_count": 422,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 56,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/966257558238343168/t1UB9aDx_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/966257558238343168/t1UB9aDx_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/966254500640993280/1519208673",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Mar 06 17:54:14 +0000 2018",
 		"id": 971081535163072500,
 		"id_str": "971081535163072514",
