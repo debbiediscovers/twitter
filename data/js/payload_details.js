f4d403e82fae9a5699b289acf44abd9025ce86e0
debbiediscovers <debbiediscovers@gmail.com>
2018-03-23 04:12:12 +0800

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12805,
-	"created_at": "Thu Mar 22 19:12:14 +0000 2018",
+	"tweets": 12808,
+	"created_at": "Thu Mar 22 20:12:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,514 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Thu Mar 22 20:00:19 +0000 2018",
+		"id": 976911469496303600,
+		"id_str": "976911469496303616",
+		"text": "@AndileKuzwayo Nope! Try again",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AndileKuzwayo",
+					"name": "Andile Kuzwayo",
+					"id": 3465414273,
+					"id_str": "3465414273",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 976813552651653100,
+		"in_reply_to_status_id_str": "976813552651653121",
+		"in_reply_to_user_id": 3465414273,
+		"in_reply_to_user_id_str": "3465414273",
+		"in_reply_to_screen_name": "AndileKuzwayo",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40569,
+			"friends_count": 25301,
+			"listed_count": 1088,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11535,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11867,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Mar 22 19:59:58 +0000 2018",
+		"id": 976911383248842800,
+		"id_str": "976911383248842752",
+		"text": "@CrisisSocMedia Thx for sharing! 😘",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CrisisSocMedia",
+					"name": "Crisis Social Media",
+					"id": 35216864,
+					"id_str": "35216864",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 976789366411493400,
+		"in_reply_to_status_id_str": "976789366411493377",
+		"in_reply_to_user_id": 35216864,
+		"in_reply_to_user_id_str": "35216864",
+		"in_reply_to_screen_name": "CrisisSocMedia",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40569,
+			"friends_count": 25301,
+			"listed_count": 1088,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11535,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11867,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Mar 22 19:59:43 +0000 2018",
+		"id": 976911321269596200,
+		"id_str": "976911321269596160",
+		"text": "RT @CrisisSocMedia: The Value of a Tweet - $174 per donor https://t.co/KLlWWghu30 via @debbiediscovers #Nonprofits #fundraising #socialmedi…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Nonprofits",
+					"indices": [
+						103,
+						114
+					]
+				},
+				{
+					"text": "fundraising",
+					"indices": [
+						115,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CrisisSocMedia",
+					"name": "Crisis Social Media",
+					"id": 35216864,
+					"id_str": "35216864",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						86,
+						102
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/KLlWWghu30",
+					"expanded_url": "https://digitaldiscovery.sg/blog/value-of-tweet/",
+					"display_url": "digitaldiscovery.sg/blog/value-of-…",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40569,
+			"friends_count": 25301,
+			"listed_count": 1088,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11535,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11867,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Mar 22 11:55:07 +0000 2018",
+			"id": 976789366411493400,
+			"id_str": "976789366411493377",
+			"text": "The Value of a Tweet - $174 per donor https://t.co/KLlWWghu30 via @debbiediscovers #Nonprofits #fundraising #socialmediamarketing #smem",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Nonprofits",
+						"indices": [
+							83,
+							94
+						]
+					},
+					{
+						"text": "fundraising",
+						"indices": [
+							95,
+							107
+						]
+					},
+					{
+						"text": "socialmediamarketing",
+						"indices": [
+							108,
+							129
+						]
+					},
+					{
+						"text": "smem",
+						"indices": [
+							130,
+							135
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "debbiediscovers",
+						"name": "Deborah Kay",
+						"id": 3060836348,
+						"id_str": "3060836348",
+						"indices": [
+							66,
+							82
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/KLlWWghu30",
+						"expanded_url": "https://digitaldiscovery.sg/blog/value-of-tweet/",
+						"display_url": "digitaldiscovery.sg/blog/value-of-…",
+						"indices": [
+							38,
+							61
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 35216864,
+				"id_str": "35216864",
+				"name": "Crisis Social Media",
+				"screen_name": "CrisisSocMedia",
+				"location": "GlenGilmore@GlenGilmore.com",
+				"description": "Crisis Social Media #CrisisComms #SMEM Mayor Emeritus @GlenGilmore https://t.co/bXzukXS2hz Times national hero https://t.co/YaYEsQIGZ0 l Forbes Top20 Social Media 📕 https://t.co/7zxcTFDBrI",
+				"url": "https://t.co/qZsh3BhiwE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qZsh3BhiwE",
+								"expanded_url": "http://GlenGilmore.com",
+								"display_url": "GlenGilmore.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/bXzukXS2hz",
+								"expanded_url": "http://bit.ly/2GICcLC",
+								"display_url": "bit.ly/2GICcLC",
+								"indices": [
+									67,
+									90
+								]
+							},
+							{
+								"url": "https://t.co/YaYEsQIGZ0",
+								"expanded_url": "http://nyti.ms/15i3U1",
+								"display_url": "nyti.ms/15i3U1",
+								"indices": [
+									111,
+									134
+								]
+							},
+							{
+								"url": "https://t.co/7zxcTFDBrI",
+								"expanded_url": "http://goo.gl/YgTBF",
+								"display_url": "goo.gl/YgTBF",
+								"indices": [
+									165,
+									188
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 15495,
+				"friends_count": 965,
+				"listed_count": 813,
+				"created_at": "Sat Apr 25 14:02:55 +0000 2009",
+				"favourites_count": 64,
+				"utc_offset": -18000,
+				"time_zone": "Quito",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3805,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/172326577/crisisdatalogo2.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/172326577/crisisdatalogo2.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/3315607996/61741e35727eb1c2c7a3279c9d825871_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/3315607996/61741e35727eb1c2c7a3279c9d825871_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/35216864/1348098402",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 1,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 22 17:20:01 +0000 2018",
 		"id": 976871129712398300,
 		"id_str": "976871129712398337",
