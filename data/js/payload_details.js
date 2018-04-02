8e3a04c48b349d20a9869d3b5bbaffc2f601eb5d
debbiediscovers <debbiediscovers@gmail.com>
2018-04-02 09:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12993,
-	"created_at": "Sun Apr 01 23:12:01 +0000 2018",
+	"tweets": 12994,
+	"created_at": "Mon Apr 02 01:12:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,137 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Mon Apr 02 00:12:20 +0000 2018",
+		"id": 980598770852245500,
+		"id_str": "980598770852245504",
+		"text": "@SaraSoueidan @stubbornella @LeaVerou Try @Agorapulse! I don't miss anything in my inbox with Agorapulse!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SaraSoueidan",
+					"name": "Sara Soueidan",
+					"id": 717654410,
+					"id_str": "717654410",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "stubbornella",
+					"name": "Nicole Sullivan 💎",
+					"id": 15629200,
+					"id_str": "15629200",
+					"indices": [
+						14,
+						27
+					]
+				},
+				{
+					"screen_name": "LeaVerou",
+					"name": "Lea Verou",
+					"id": 22199970,
+					"id_str": "22199970",
+					"indices": [
+						28,
+						37
+					]
+				},
+				{
+					"screen_name": "AgoraPulse",
+					"name": "AgoraPulse",
+					"id": 281546383,
+					"id_str": "281546383",
+					"indices": [
+						42,
+						53
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 979722248218202100,
+		"in_reply_to_status_id_str": "979722248218202112",
+		"in_reply_to_user_id": 717654410,
+		"in_reply_to_user_id_str": "717654410",
+		"in_reply_to_screen_name": "SaraSoueidan",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 41392,
+			"friends_count": 28648,
+			"listed_count": 1108,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11650,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12047,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Apr 01 23:01:04 +0000 2018",
 		"id": 980580835282882600,
 		"id_str": "980580835282882560",
