30c6f0dd9c75bdd7b807be826884c9fec91bf053
debbiediscovers <debbiediscovers@gmail.com>
2018-04-14 05:12:05 +0800

    Added 1 tweets

--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Fri Apr 13 20:21:53 +0000 2018",
+		"id": 984889429889183700,
+		"id_str": "984889429889183744",
+		"text": "@marthamartin @Quotes_and_Star @pqc_works @BillGarlandSpkr @IUPUIglobal @greg_aaron @MoscatoMom @danielsangi… https://t.co/VKfwlOi8IP",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marthamartin",
+					"name": "Martha Martin",
+					"id": 15172138,
+					"id_str": "15172138",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "Quotes_and_Star",
+					"name": "smithalana",
+					"id": 978863581465862100,
+					"id_str": "978863581465862148",
+					"indices": [
+						14,
+						30
+					]
+				},
+				{
+					"screen_name": "pqc_works",
+					"name": "PQC",
+					"id": 2935290774,
+					"id_str": "2935290774",
+					"indices": [
+						31,
+						41
+					]
+				},
+				{
+					"screen_name": "BillGarlandSpkr",
+					"name": "Bill Garland",
+					"id": 3006851958,
+					"id_str": "3006851958",
+					"indices": [
+						42,
+						58
+					]
+				},
+				{
+					"screen_name": "IUPUIglobal",
+					"name": "IUPUI Global",
+					"id": 90643593,
+					"id_str": "90643593",
+					"indices": [
+						59,
+						71
+					]
+				},
+				{
+					"screen_name": "greg_aaron",
+					"name": "Greg Aaron",
+					"id": 2933777411,
+					"id_str": "2933777411",
+					"indices": [
+						72,
+						83
+					]
+				},
+				{
+					"screen_name": "MoscatoMom",
+					"name": "MoscatoMom",
+					"id": 213724170,
+					"id_str": "213724170",
+					"indices": [
+						84,
+						95
+					]
+				},
+				{
+					"screen_name": "danielsangi",
+					"name": "Daniel Im",
+					"id": 81093355,
+					"id_str": "81093355",
+					"indices": [
+						96,
+						108
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VKfwlOi8IP",
+					"expanded_url": "https://twitter.com/i/web/status/984889429889183744",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 984849817355550700,
+		"in_reply_to_status_id_str": "984849817355550723",
+		"in_reply_to_user_id": 15172138,
+		"in_reply_to_user_id_str": "15172138",
+		"in_reply_to_screen_name": "marthamartin",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42844,
+			"friends_count": 28145,
+			"listed_count": 1116,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11756,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12224,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Apr 13 17:54:00 +0000 2018",
 		"id": 984852216132591600,
 		"id_str": "984852216132591620",
