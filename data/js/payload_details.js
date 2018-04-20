70053b44bf5873476d2f9eead46245be891ce27c
debbiediscovers <debbiediscovers@gmail.com>
2018-04-21 03:12:08 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13311,
-	"created_at": "Fri Apr 20 18:12:06 +0000 2018",
+	"tweets": 13312,
+	"created_at": "Fri Apr 20 19:12:07 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,125 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Fri Apr 20 18:59:35 +0000 2018",
+		"id": 987405435123544000,
+		"id_str": "987405435123544064",
+		"text": "@darussol @PencilsOfPromis That's AMAZING Daniele! 👍😀 Yes! to #GlobalEducation!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GlobalEducation",
+					"indices": [
+						62,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "darussol",
+					"name": "Daniele Russolillo",
+					"id": 8079862,
+					"id_str": "8079862",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "PencilsOfPromis",
+					"name": "Pencils of Promise",
+					"id": 18369852,
+					"id_str": "18369852",
+					"indices": [
+						10,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 987399253239746600,
+		"in_reply_to_status_id_str": "987399253239746562",
+		"in_reply_to_user_id": 8079862,
+		"in_reply_to_user_id_str": "8079862",
+		"in_reply_to_screen_name": "darussol",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43760,
+			"friends_count": 21329,
+			"listed_count": 1126,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11826,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12345,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Apr 20 17:46:37 +0000 2018",
 		"id": 987387072485797900,
 		"id_str": "987387072485797889",
@@ -651,7 +770,7 @@ Grailbird.data.tweets_2018_04 =
 		"created_at": "Fri Apr 20 17:31:58 +0000 2018",
 		"id": 987383384061050900,
 		"id_str": "987383384061050880",
-		"text": "Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷‍♀️🤷‍♂️ https://t.co/54LbRuZNtm",
+		"text": "Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷♀️🤷♂️ https://t.co/54LbRuZNtm",
 		"truncated": false,
 		"entities": {
 			"hashtags": [],
