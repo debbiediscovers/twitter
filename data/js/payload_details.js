77bd1f767ffe0011df5bab5418cb203af5d68e7e
debbiediscovers <debbiediscovers@gmail.com>
2018-04-21 14:12:09 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13322,
-	"created_at": "Sat Apr 21 05:12:11 +0000 2018",
+	"tweets": 13323,
+	"created_at": "Sat Apr 21 06:12:06 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,107 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sat Apr 21 05:57:41 +0000 2018",
+		"id": 987571050970939400,
+		"id_str": "987571050970939393",
+		"text": "“11 More Best UX Practices for Building Chatbots” by Eunji “Jinny” Seo https://t.co/4yhBMxiwgi",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4yhBMxiwgi",
+					"expanded_url": "https://chatbotsmagazine.com/11-more-best-ux-practices-for-building-chatbots-67362d1104d9",
+					"display_url": "chatbotsmagazine.com/11-more-best-u…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43814,
+			"friends_count": 21599,
+			"listed_count": 1121,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11832,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12356,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Apr 21 05:01:55 +0000 2018",
 		"id": 987557017186422800,
 		"id_str": "987557017186422790",
@@ -950,7 +1051,7 @@ Grailbird.data.tweets_2018_04 =
 		"created_at": "Sat Apr 21 05:01:22 +0000 2018",
 		"id": 987556877423857700,
 		"id_str": "987556877423857664",
-		"text": "RT @debbiediscovers: Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷‍♀️🤷‍♂️ https://t.co/54LbRuZNtm",
+		"text": "RT @debbiediscovers: Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷♀️🤷♂️ https://t.co/54LbRuZNtm",
 		"truncated": false,
 		"entities": {
 			"hashtags": [],
@@ -1064,7 +1165,7 @@ Grailbird.data.tweets_2018_04 =
 			"created_at": "Fri Apr 20 17:31:58 +0000 2018",
 			"id": 987383384061050900,
 			"id_str": "987383384061050880",
-			"text": "Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷‍♀️🤷‍♂️ https://t.co/54LbRuZNtm",
+			"text": "Yup. Twitter doesn’t care. Neither does @Twittersupport 🤷♀️🤷♂️ https://t.co/54LbRuZNtm",
 			"truncated": false,
 			"entities": {
 				"hashtags": [],
