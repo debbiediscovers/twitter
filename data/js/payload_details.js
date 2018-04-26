8224c418c590a02420947875b28a63a04825780e
debbiediscovers <debbiediscovers@gmail.com>
2018-04-26 12:12:11 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13451,
-	"created_at": "Thu Apr 26 01:12:09 +0000 2018",
+	"tweets": 13452,
+	"created_at": "Thu Apr 26 04:12:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Thu Apr 26 04:00:06 +0000 2018",
+		"id": 989353400654426100,
+		"id_str": "989353400654426112",
+		"text": "RT @CartierAwards: As #socent is gaining momentum in #Singapore, we are celebrating women social entrepreneurs in this beautiful country. J…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "socent",
+					"indices": [
+						22,
+						29
+					]
+				},
+				{
+					"text": "Singapore",
+					"indices": [
+						53,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CartierAwards",
+					"name": "Cartier Awards",
+					"id": 161049811,
+					"id_str": "161049811",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44190,
+			"friends_count": 18952,
+			"listed_count": 1122,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11897,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12482,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Apr 25 05:32:00 +0000 2018",
+			"id": 989014138666893300,
+			"id_str": "989014138666893313",
+			"text": "As #socent is gaining momentum in #Singapore, we are celebrating women social entrepreneurs in this beautiful count… https://t.co/YLW9eAMX6U",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "socent",
+						"indices": [
+							3,
+							10
+						]
+					},
+					{
+						"text": "Singapore",
+						"indices": [
+							34,
+							44
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/YLW9eAMX6U",
+						"expanded_url": "https://twitter.com/i/web/status/989014138666893313",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 161049811,
+				"id_str": "161049811",
+				"name": "Cartier Awards",
+				"screen_name": "CartierAwards",
+				"location": "Paris",
+				"description": "An international competition for women entrepreneurs organized by Cartier, the Women's Forum, McKinsey and INSEAD.",
+				"url": "http://t.co/5mfpVFTj2J",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/5mfpVFTj2J",
+								"expanded_url": "http://www.cartierwomensinitiative.com",
+								"display_url": "cartierwomensinitiative.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 14295,
+				"friends_count": 1218,
+				"listed_count": 136,
+				"created_at": "Tue Jun 29 21:21:00 +0000 2010",
+				"favourites_count": 1500,
+				"utc_offset": 7200,
+				"time_zone": "Europe/Paris",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4343,
+				"lang": "fr",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FAFAFA",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/120693693/Backgroung-Twitter-Valide.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/120693693/Backgroung-Twitter-Valide.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/798820877018152960/RmcGYkBr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/798820877018152960/RmcGYkBr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/161049811/1519232693",
+				"profile_link_color": "696969",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "9C9C9C",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 6,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Apr 26 00:45:29 +0000 2018",
 		"id": 989304421610745900,
 		"id_str": "989304421610745857",
