2e54401b2718b41206103e88bd1eb2f85b669d69
debbiediscovers <debbiediscovers@gmail.com>
2018-04-28 11:12:09 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13494,
-	"created_at": "Sat Apr 28 00:12:12 +0000 2018",
+	"tweets": 13496,
+	"created_at": "Sat Apr 28 03:12:08 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,425 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sat Apr 28 02:44:17 +0000 2018",
+		"id": 990059095448174600,
+		"id_str": "990059095448174593",
+		"text": "@Startup_Nerd @IntEngineering @MikeQuindazzi Pretty cool!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Startup_Nerd",
+					"name": "The Startup Nerd",
+					"id": 2730759696,
+					"id_str": "2730759696",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "IntEngineering",
+					"name": "Interesting Engineering",
+					"id": 564053183,
+					"id_str": "564053183",
+					"indices": [
+						14,
+						29
+					]
+				},
+				{
+					"screen_name": "MikeQuindazzi",
+					"name": "Mike Quindazzi ✨",
+					"id": 2344530218,
+					"id_str": "2344530218",
+					"indices": [
+						30,
+						44
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 989663014147252200,
+		"in_reply_to_status_id_str": "989663014147252224",
+		"in_reply_to_user_id": 2730759696,
+		"in_reply_to_user_id_str": "2730759696",
+		"in_reply_to_screen_name": "Startup_Nerd",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44327,
+			"friends_count": 19419,
+			"listed_count": 1127,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11918,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12526,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sat Apr 28 02:44:08 +0000 2018",
+		"id": 990059059586912300,
+		"id_str": "990059059586912257",
+		"text": "RT @Startup_Nerd: Transform your entire body with your own #digital body #videogame controller bodysuit. #VR #AR #Gaming #IoT Via @IntEngin…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "digital",
+					"indices": [
+						59,
+						67
+					]
+				},
+				{
+					"text": "videogame",
+					"indices": [
+						73,
+						83
+					]
+				},
+				{
+					"text": "VR",
+					"indices": [
+						105,
+						108
+					]
+				},
+				{
+					"text": "AR",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "Gaming",
+					"indices": [
+						113,
+						120
+					]
+				},
+				{
+					"text": "IoT",
+					"indices": [
+						121,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Startup_Nerd",
+					"name": "The Startup Nerd",
+					"id": 2730759696,
+					"id_str": "2730759696",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44327,
+			"friends_count": 19419,
+			"listed_count": 1127,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 11918,
+			"utc_offset": 28800,
+			"time_zone": "Singapore",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12526,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643531621665230852/wKPpg2Ub.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Apr 27 00:30:24 +0000 2018",
+			"id": 989663014147252200,
+			"id_str": "989663014147252224",
+			"text": "Transform your entire body with your own #digital body #videogame controller bodysuit. #VR #AR #Gaming #IoT Via… https://t.co/k29uNmruXf",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "digital",
+						"indices": [
+							41,
+							49
+						]
+					},
+					{
+						"text": "videogame",
+						"indices": [
+							55,
+							65
+						]
+					},
+					{
+						"text": "VR",
+						"indices": [
+							87,
+							90
+						]
+					},
+					{
+						"text": "AR",
+						"indices": [
+							91,
+							94
+						]
+					},
+					{
+						"text": "Gaming",
+						"indices": [
+							95,
+							102
+						]
+					},
+					{
+						"text": "IoT",
+						"indices": [
+							103,
+							107
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/k29uNmruXf",
+						"expanded_url": "https://twitter.com/i/web/status/989663014147252224",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							113,
+							136
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2730759696,
+				"id_str": "2730759696",
+				"name": "The Startup Nerd",
+				"screen_name": "Startup_Nerd",
+				"location": "Palo Alto, CA",
+				"description": "Growth tips for startups, small businesses & entrepreneurs at https://t.co/1HNaUkIVCJ. Growth hacking, inbound marketing, A/B testing, data, fundraising...",
+				"url": "https://t.co/1HNaUkIVCJ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/1HNaUkIVCJ",
+								"expanded_url": "http://buff.ly/2hDImUg",
+								"display_url": "buff.ly/2hDImUg",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/1HNaUkIVCJ",
+								"expanded_url": "http://buff.ly/2hDImUg",
+								"display_url": "buff.ly/2hDImUg",
+								"indices": [
+									62,
+									85
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 123251,
+				"friends_count": 100830,
+				"listed_count": 1368,
+				"created_at": "Thu Aug 14 02:48:32 +0000 2014",
+				"favourites_count": 116,
+				"utc_offset": 28800,
+				"time_zone": "Taipei",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 91110,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "709397",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/811535882448605189/pzwNlpdY_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/811535882448605189/pzwNlpdY_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2730759696/1482320921",
+				"profile_link_color": "FF3300",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "A0C5C7",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 20,
+			"favorite_count": 29,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Apr 27 23:23:58 +0000 2018",
 		"id": 990008684842401800,
 		"id_str": "990008684842401792",
