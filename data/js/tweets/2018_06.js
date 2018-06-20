036e236d53de5efdfb640565eb29939f87e1e356
debbiediscovers <debbiediscovers@gmail.com>
2018-06-20 19:12:07 +0800

    Added 1 tweets

--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,10 +1,119 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Wed Jun 20 10:37:45 +0000 2018",
+		"id": 1009384805384032300,
+		"id_str": "1009384805384032256",
+		"text": "Today is the last day of my #30shadesofkind! Hope y’all enjoyed my little reminders to be kind. The colours follow… https://t.co/RKAFp4XUHa",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "30shadesofkind",
+					"indices": [
+						28,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RKAFp4XUHa",
+					"expanded_url": "https://twitter.com/i/web/status/1009384805384032256",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore",
+			"description": "💄Entrepreneur, Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI news bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50083,
+			"friends_count": 30224,
+			"listed_count": 1152,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 12995,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13724,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jun 20 10:03:55 +0000 2018",
 		"id": 1009376291756228600,
 		"id_str": "1009376291756228609",
-		"text": "RT @xavierlur: When you photoshopped yourself into a stock image just for a sponsored post and for the gram 🤦‍♂️. (And this isn’t just it.…",
+		"text": "RT @xavierlur: When you photoshopped yourself into a stock image just for a sponsored post and for the gram 🤦♂️. (And this isn’t just it.…",
 		"truncated": false,
 		"entities": {
 			"hashtags": [],
@@ -98,7 +207,7 @@ Grailbird.data.tweets_2018_06 =
 			"created_at": "Wed Jun 20 01:56:24 +0000 2018",
 			"id": 1009253601833672700,
 			"id_str": "1009253601833672704",
-			"text": "When you photoshopped yourself into a stock image just for a sponsored post and for the gram 🤦‍♂️. (And this isn’t… https://t.co/5GkMw8tSDR",
+			"text": "When you photoshopped yourself into a stock image just for a sponsored post and for the gram 🤦♂️. (And this isn’t… https://t.co/5GkMw8tSDR",
 			"truncated": true,
 			"entities": {
 				"hashtags": [],
