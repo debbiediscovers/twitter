07e7107023ae1ba932a02351a1bb2bf5ad657bda
debbiediscovers <debbiediscovers@gmail.com>
2018-07-20 02:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15834,
-	"created_at": "Thu Jul 19 16:12:15 +0000 2018",
+	"tweets": 15835,
+	"created_at": "Thu Jul 19 18:12:13 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_07.js",
 		"year": 2018,
 		"var_name": "tweets_2018_07",
-		"tweet_count": 925,
+		"tweet_count": 926,
 		"month": 7
 	},
 	{
