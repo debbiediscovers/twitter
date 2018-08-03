51b2a0ee80920dd86d50ba16a89ff3649e82c2ad
debbiediscovers <debbiediscovers@gmail.com>
2018-08-03 09:12:06 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16192,
-	"created_at": "Thu Aug 02 21:12:03 +0000 2018",
+	"tweets": 16194,
+	"created_at": "Fri Aug 03 01:12:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,416 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Fri Aug 03 00:27:06 +0000 2018",
+		"id": 1025176193723654100,
+		"id_str": "1025176193723654144",
+		"text": "My favourite Twitter keyboard shortcut is ? 😋\n\n@madalynsklar which is yours? #Twittersmarter https://t.co/qSx21t2j2Y",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Twittersmarter",
+					"indices": [
+						77,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MadalynSklar",
+					"name": "Madalyn Sklar 🚀 Digital Marketing Coach",
+					"id": 14164297,
+					"id_str": "14164297",
+					"indices": [
+						47,
+						60
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qSx21t2j2Y",
+					"expanded_url": "https://twitter.com/MadalynSklar/status/1025174195611811840",
+					"display_url": "twitter.com/MadalynSklar/s…",
+					"indices": [
+						93,
+						116
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Where 💨 ✳️ 💡 🛌😴 furiously",
+			"description": "💄Entrepreneur, 🤖 Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55975,
+			"friends_count": 26142,
+			"listed_count": 1204,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 16407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15131,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1025174195611811800,
+		"quoted_status_id_str": "1025174195611811840",
+		"quoted_status": {
+			"created_at": "Fri Aug 03 00:19:09 +0000 2018",
+			"id": 1025174195611811800,
+			"id_str": "1025174195611811840",
+			"text": "Check out these Twitter keyboard shortcuts. It will make you #TwitterSmarter! https://t.co/rR0tb9EH7W",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "TwitterSmarter",
+						"indices": [
+							61,
+							76
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1025174193917358100,
+						"id_str": "1025174193917358080",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/media/Djomf21XcAApd8_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Djomf21XcAApd8_.jpg",
+						"url": "https://t.co/rR0tb9EH7W",
+						"display_url": "pic.twitter.com/rR0tb9EH7W",
+						"expanded_url": "https://twitter.com/MadalynSklar/status/1025174195611811840/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1025174193917358100,
+						"id_str": "1025174193917358080",
+						"indices": [
+							78,
+							101
+						],
+						"media_url": "http://pbs.twimg.com/media/Djomf21XcAApd8_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Djomf21XcAApd8_.jpg",
+						"url": "https://t.co/rR0tb9EH7W",
+						"display_url": "pic.twitter.com/rR0tb9EH7W",
+						"expanded_url": "https://twitter.com/MadalynSklar/status/1025174195611811840/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 645,
+								"h": 402,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.socialjukebox.com\" rel=\"nofollow\">The Social Jukebox</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14164297,
+				"id_str": "14164297",
+				"name": "Madalyn Sklar 🚀 Digital Marketing Coach",
+				"screen_name": "MadalynSklar",
+				"location": "Houston, TX",
+				"description": "Twitter Marketing Pro. Host #TwitterSmarter chat Thurs 1pm ET and podcast 🎧 https://t.co/nBDQrJYmZT. #SocialROI chat Tues 5pm ET. #CTCpodcast co-host.",
+				"url": "https://t.co/Oaeqp32FDf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Oaeqp32FDf",
+								"expanded_url": "http://www.madalynsklar.com",
+								"display_url": "madalynsklar.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/nBDQrJYmZT",
+								"expanded_url": "http://bit.ly/twittersmarter",
+								"display_url": "bit.ly/twittersmarter",
+								"indices": [
+									76,
+									99
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 69155,
+				"friends_count": 51078,
+				"listed_count": 4669,
+				"created_at": "Mon Mar 17 17:00:38 +0000 2008",
+				"favourites_count": 118527,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 179238,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/971518376076984320/eQdX_nIQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/971518376076984320/eQdX_nIQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14164297/1485550174",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Fri Aug 03 00:12:20 +0000 2018",
+		"id": 1025172478371352600,
+		"id_str": "1025172478371352578",
+		"text": "@Jenniemore I know right!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jenniemore",
+					"name": "Jenniemore",
+					"id": 856086734181945300,
+					"id_str": "856086734181945344",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1025030065078382600,
+		"in_reply_to_status_id_str": "1025030065078382592",
+		"in_reply_to_user_id": 856086734181945300,
+		"in_reply_to_user_id_str": "856086734181945344",
+		"in_reply_to_screen_name": "Jenniemore",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Where 💨 ✳️ 💡 🛌😴 furiously",
+			"description": "💄Entrepreneur, 🤖 Chatbot Evangelist, #GlobalEducation advocate, love Twitter. @DigitalFuturist @LittleCodersSG @GiftedSGP @0rchestras are my #AI bots",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55975,
+			"friends_count": 26142,
+			"listed_count": 1204,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 16407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15131,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902110924470796289/JYTMIC83_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 02 20:33:47 +0000 2018",
 		"id": 1025117479582949400,
 		"id_str": "1025117479582949376",
