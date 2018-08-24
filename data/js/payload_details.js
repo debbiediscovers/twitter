30a9eff6851a49f6c3702c015936b2b713bae56b
debbiediscovers <debbiediscovers@gmail.com>
2018-08-24 19:12:09 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16612,
-	"created_at": "Fri Aug 24 10:12:06 +0000 2018",
+	"tweets": 16613,
+	"created_at": "Fri Aug 24 11:12:07 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Fri Aug 24 11:05:49 +0000 2018",
+		"id": 1032947079755182100,
+		"id_str": "1032947079755182080",
+		"text": "@shake24x7 @STcom @Samsung @SamsungMobile https://t.co/a9QClagZzK",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "shake24x7",
+					"name": "Shake",
+					"id": 230798580,
+					"id_str": "230798580",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "STcom",
+					"name": "The Straits Times",
+					"id": 37874853,
+					"id_str": "37874853",
+					"indices": [
+						11,
+						17
+					]
+				},
+				{
+					"screen_name": "Samsung",
+					"name": "Samsung Electronics",
+					"id": 2381578122,
+					"id_str": "2381578122",
+					"indices": [
+						18,
+						26
+					]
+				},
+				{
+					"screen_name": "SamsungMobile",
+					"name": "Samsung Mobile",
+					"id": 297169759,
+					"id_str": "297169759",
+					"indices": [
+						27,
+						41
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1032947071043522600,
+					"id_str": "1032947071043522561",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DlXD42lUUAEd4J-.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DlXD42lUUAEd4J-.jpg",
+					"url": "https://t.co/a9QClagZzK",
+					"display_url": "pic.twitter.com/a9QClagZzK",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1032947079755182080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1032947071043522600,
+					"id_str": "1032947071043522561",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DlXD42lUUAEd4J-.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DlXD42lUUAEd4J-.jpg",
+					"url": "https://t.co/a9QClagZzK",
+					"display_url": "pic.twitter.com/a9QClagZzK",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1032947079755182080/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 356,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							120,
+							89
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DlXD42lUUAEd4J-.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1032926943241416700,
+		"in_reply_to_status_id_str": "1032926943241416704",
+		"in_reply_to_user_id": 230798580,
+		"in_reply_to_user_id_str": "230798580",
+		"in_reply_to_screen_name": "shake24x7",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | Global Education Advocate | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58273,
+			"friends_count": 26196,
+			"listed_count": 1228,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 16922,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15526,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "58a4c3a0d54e1400",
+			"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+			"place_type": "admin",
+			"name": "Central Region",
+			"full_name": "Central Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.749959507073,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.36874499902569
+						],
+						[
+							103.749959507073,
+							1.36874499902569
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Fri Aug 24 09:40:55 +0000 2018",
 		"id": 1032925713278459900,
 		"id_str": "1032925713278459905",
