48e7ed3cb13cbc6c0e5059587a2565bbf14dffd5
debbiediscovers <debbiediscovers@gmail.com>
2018-10-14 04:12:07 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17351,
-	"created_at": "Fri Oct 12 22:12:05 +0000 2018",
+	"tweets": 17352,
+	"created_at": "Sat Oct 13 20:12:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Fri Oct 12 20:59:54 +0000 2018",
+		"id": 1050853591450144800,
+		"id_str": "1050853591450144768",
+		"full_text": "Simple tutorial on how to train your bot to deliver knock knock jokes (using context in @Dialogflow) 😆 by  @debbiediscovers by https://t.co/ZSkzSaj22u #Chatbot #Tutorial \n\nFREE Dialogflow example files available for download! Link in article https://t.co/jUoTC5U1Vh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Chatbot",
+					"indices": [
+						151,
+						159
+					]
+				},
+				{
+					"text": "Tutorial",
+					"indices": [
+						160,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Dialogflow",
+					"name": "Dialogflow",
+					"id": 2774598458,
+					"id_str": "2774598458",
+					"indices": [
+						88,
+						99
+					]
+				},
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						107,
+						123
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ZSkzSaj22u",
+					"expanded_url": "https://chatbotslife.com/how-to-handle-context-with-dialogflow-part-1-knock-knock-jokes-4659b346d83b",
+					"display_url": "chatbotslife.com/how-to-handle-…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1050853464031424500,
+					"id_str": "1050853464031424512",
+					"indices": [
+						242,
+						265
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DpVhqGqU8AASEIO.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DpVhqGqU8AASEIO.jpg",
+					"url": "https://t.co/jUoTC5U1Vh",
+					"display_url": "pic.twitter.com/jUoTC5U1Vh",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1050853591450144768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1050853464031424500,
+					"id_str": "1050853464031424512",
+					"indices": [
+						242,
+						265
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DpVhqGqU8AASEIO.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DpVhqGqU8AASEIO.jpg",
+					"url": "https://t.co/jUoTC5U1Vh",
+					"display_url": "pic.twitter.com/jUoTC5U1Vh",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1050853591450144768/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 528,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							11,
+							10
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DpVhqGqU8AASEIO.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | Global Education Advocate | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64801,
+			"friends_count": 37711,
+			"listed_count": 1301,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 17539,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16105,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Simple tutorial on how to train your bot to deliver knock knock jokes (using context in @Dialogflow) 😆 by  @debbiediscovers by https://t.co/ZSkzSaj22u #Chatbot #Tutorial \n\nFREE Dialogflow example files available for download! Link in article https://t.co/jUoTC5U1Vh"
+	},
+	{
 		"created_at": "Fri Oct 12 21:14:26 +0000 2018",
 		"id": 1050857248879046700,
 		"id_str": "1050857248879046656",
