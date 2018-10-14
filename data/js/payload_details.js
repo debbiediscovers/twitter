32ff574fd338ad7c42f2ed554a9a806ca1730cdf
debbiediscovers <debbiediscovers@gmail.com>
2018-10-14 16:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17365,
-	"created_at": "Sun Oct 14 06:12:05 +0000 2018",
+	"tweets": 17366,
+	"created_at": "Sun Oct 14 08:12:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Sun Oct 14 07:45:07 +0000 2018",
+		"id": 1051378352768970800,
+		"id_str": "1051378352768970753",
+		"full_text": "@famlawyer @jorgebarba Yes haha - some of them are. But once you get them to love your chatbot, they stop being mean!",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			117
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "famlawyer",
+					"name": "Tom Daley",
+					"id": 34154055,
+					"id_str": "34154055",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "jorgebarba",
+					"name": "Jorge Barba",
+					"id": 8883702,
+					"id_str": "8883702",
+					"indices": [
+						11,
+						22
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1051348293266133000,
+		"in_reply_to_status_id_str": "1051348293266132992",
+		"in_reply_to_user_id": 34154055,
+		"in_reply_to_user_id_str": "34154055",
+		"in_reply_to_screen_name": "famlawyer",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | Global Education Advocate | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64871,
+			"friends_count": 38234,
+			"listed_count": 1289,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 17546,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16117,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@famlawyer @jorgebarba Yes haha - some of them are. But once you get them to love your chatbot, they stop being mean!"
+	},
+	{
 		"created_at": "Sun Oct 14 05:39:13 +0000 2018",
 		"id": 1051346666706501600,
 		"id_str": "1051346666706501633",
