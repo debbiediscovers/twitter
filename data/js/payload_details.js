75de0d29e19899073d6d843d056f05e14c0e7259
debbiediscovers <debbiediscovers@gmail.com>
2018-11-07 09:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22419,
-	"created_at": "Wed Nov 07 00:12:02 +0000 2018",
+	"tweets": 22420,
+	"created_at": "Wed Nov 07 01:12:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Wed Nov 07 00:08:00 +0000 2018",
+		"id": 1059960622346092500,
+		"id_str": "1059960622346092544",
+		"full_text": "Startup like a girl: why investors should pay more attention to female entrepreneurs https://t.co/LuirSTQpwl #entrepreneurs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						109,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LuirSTQpwl",
+					"expanded_url": "https://www.theguardian.com/sustainable-business/2017/apr/03/startup-like-a-girl-why-investors-should-pay-more-attention-to-female-entrepreneurs",
+					"display_url": "theguardian.com/sustainable-bu…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 721241974880120800,
+			"id_str": "721241974880120832",
+			"name": "Steph L Lee",
+			"screen_name": "hipsterkidster",
+			"location": "",
+			"description": "Professional and Commercial Model. Social Media Concierge.",
+			"url": "https://t.co/oZVDvIEirc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oZVDvIEirc",
+							"expanded_url": "http://hipsterkidster.com",
+							"display_url": "hipsterkidster.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3385,
+			"friends_count": 2695,
+			"listed_count": 35,
+			"created_at": "Sat Apr 16 07:41:01 +0000 2016",
+			"favourites_count": 494,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2882,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/958178636313477120/5cfr6OYe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/958178636313477120/5cfr6OYe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/721241974880120832/1517282599",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Startup like a girl: why investors should pay more attention to female entrepreneurs https://t.co/LuirSTQpwl #entrepreneurs"
+	},
+	{
 		"created_at": "Tue Nov 06 22:28:47 +0000 2018",
 		"id": 1059935653813776400,
 		"id_str": "1059935653813776384",
