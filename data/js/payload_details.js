e4d173a30eb18b80eb59539848adf589d20ccff1
debbiediscovers <debbiediscovers@gmail.com>
2018-11-26 07:12:16 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23677,
-	"created_at": "Sun Nov 25 03:12:20 +0000 2018",
+	"tweets": 23679,
+	"created_at": "Sun Nov 25 23:12:15 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_11.js",
 		"year": 2018,
 		"var_name": "tweets_2018_11",
-		"tweet_count": 1350,
+		"tweet_count": 1352,
 		"month": 11
 	},
 	{
