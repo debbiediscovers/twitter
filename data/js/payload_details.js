29dd3b66b3000178ed88334bbd8129cd5edd6a52
debbiediscovers <debbiediscovers@gmail.com>
2018-12-14 21:12:09 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27508,
-	"created_at": "Fri Dec 14 11:12:06 +0000 2018",
+	"tweets": 27509,
+	"created_at": "Fri Dec 14 13:12:07 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,10 +1,116 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Fri Dec 14 12:17:20 +0000 2018",
+		"id": 1073552518406262800,
+		"id_str": "1073552518406262785",
+		"full_text": "@QuassarianViper 😆",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "QuassarianViper",
+					"name": "🐍Viper🐍",
+					"id": 3739389615,
+					"id_str": "3739389615",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1073552410327613400,
+		"in_reply_to_status_id_str": "1073552410327613440",
+		"in_reply_to_user_id": 3739389615,
+		"in_reply_to_user_id_str": "3739389615",
+		"in_reply_to_screen_name": "QuassarianViper",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57654,
+			"friends_count": 26161,
+			"listed_count": 1298,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18492,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16587,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@QuassarianViper 😆"
+	},
+	{
 		"created_at": "Fri Dec 14 10:26:08 +0000 2018",
 		"id": 1073524532915269600,
 		"id_str": "1073524532915269632",
-		"full_text": "Jason Momoa! Who agrees? #aquaman⁠ ⁠ 😂😂😂 https://t.co/P5oMlNeucq",
+		"full_text": "Jason Momoa! Who agrees? #aquaman  😂😂😂 https://t.co/P5oMlNeucq",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -180,7 +286,7 @@ Grailbird.data.tweets_2018_12 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Jason Momoa! Who agrees? #aquaman⁠ ⁠ 😂😂😂 https://t.co/P5oMlNeucq"
+		"text": "Jason Momoa! Who agrees? #aquaman  😂😂😂 https://t.co/P5oMlNeucq"
 	},
 	{
 		"created_at": "Wed Dec 12 21:38:14 +0000 2018",
