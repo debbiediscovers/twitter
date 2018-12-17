ca33a8fbf0b98a749d649060fac9af90951df269
debbiediscovers <debbiediscovers@gmail.com>
2018-12-18 07:14:27 +0800

    Added 9 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27917,
-	"created_at": "Mon Dec 17 22:17:11 +0000 2018",
+	"tweets": 27926,
+	"created_at": "Mon Dec 17 23:14:25 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 902,
+		"tweet_count": 911,
 		"month": 12
 	},
 	{
