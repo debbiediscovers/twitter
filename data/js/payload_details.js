422b52c22f8f98e013ace1bc17c1ba6944e87b54
debbiediscovers <debbiediscovers@gmail.com>
2018-12-25 14:12:06 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30221,
-	"created_at": "Tue Dec 25 04:12:07 +0000 2018",
+	"tweets": 30222,
+	"created_at": "Tue Dec 25 06:12:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Tue Dec 25 05:37:18 +0000 2018",
+		"id": 1077438111003492400,
+		"id_str": "1077438111003492352",
+		"full_text": "@Art_EdgeDotCom @Debbie Thanks! It’s a great bot template. Try it for yourself!  https://t.co/XHkCtQjeuU",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Art_EdgeDotCom",
+					"name": "artedge",
+					"id": 281675813,
+					"id_str": "281675813",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "Debbie",
+					"name": "Debbie",
+					"id": 9134,
+					"id_str": "9134",
+					"indices": [
+						16,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XHkCtQjeuU",
+					"expanded_url": "http://m.me/imagemanipulationbot",
+					"display_url": "m.me/imagemanipulat…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1077421614969512000,
+		"in_reply_to_status_id_str": "1077421614969511936",
+		"in_reply_to_user_id": 281675813,
+		"in_reply_to_user_id_str": "281675813",
+		"in_reply_to_screen_name": "Art_EdgeDotCom",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58531,
+			"friends_count": 31018,
+			"listed_count": 1317,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18587,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16655,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Art_EdgeDotCom @Debbie Thanks! It’s a great bot template. Try it for yourself!  https://t.co/XHkCtQjeuU"
+	},
+	{
 		"created_at": "Tue Dec 25 03:52:28 +0000 2018",
 		"id": 1077411729112350700,
 		"id_str": "1077411729112350720",
