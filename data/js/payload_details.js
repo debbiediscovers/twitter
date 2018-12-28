6c5cdafb5f0b43610bcb4be5b17d7a31168a727b
debbiediscovers <debbiediscovers@gmail.com>
2018-12-28 19:12:10 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30490,
-	"created_at": "Fri Dec 28 10:12:10 +0000 2018",
+	"tweets": 30491,
+	"created_at": "Fri Dec 28 11:12:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -47,6 +47,99 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
+			"followers_count": 1,
+			"friends_count": 11,
+			"listed_count": 0,
+			"created_at": "Fri Dec 28 02:17:14 +0000 2018",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1078564183652745217/rLYpwZp6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1078564183652745217/rLYpwZp6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1078474927429234688/1545984700",
+			"profile_link_color": "26E2DA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "\"It was Bruce Lee who had coined the famous quote stating you should take what’s useful, dismiss what’s not, and add your own secret sauce to the mix—but that’s what I did.\" - The Leangains Method: The Art of Getting Ripped. Researched, Practiced, Perfected by @Martinberkhan"
+	},
+	{
+		"created_at": "Fri Dec 28 08:31:26 +0000 2018",
+		"id": 1078569096067706900,
+		"id_str": "1078569096067706881",
+		"full_text": "\"It was Bruce Lee who had coined the famous quote stating you should take what’s useful, dismiss what’s not, and add your own secret sauce to the mix—but that’s what I did.\" - The Leangains Method: The Art of Getting Ripped. Researched, Practiced, Perfected by @Martinberkhan",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Martinberkhan",
+					"name": "Martin Berkhan",
+					"id": 105285304,
+					"id_str": "105285304",
+					"indices": [
+						261,
+						275
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1078474927429234700,
+			"id_str": "1078474927429234688",
+			"name": "Improve your Healthspan",
+			"screen_name": "HHealthspan",
+			"location": "Global",
+			"description": "#AI news on health and wellness, for a better healthspan",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 0,
 			"friends_count": 11,
 			"listed_count": 0,
