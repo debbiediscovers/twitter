47cd2fb41c986bfbd8c8416b56fdd5949179a05c
debbiediscovers <debbiediscovers@gmail.com>
2018-12-30 12:12:10 +0800

    Added 1 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sun Dec 30 04:01:13 +0000 2018",
+		"id": 1079225872295415800,
+		"id_str": "1079225872295415808",
+		"full_text": "@Devin_Pickell @cartooninperson Doesn’t say how many drawings but it shows which artists it collaborated with https://t.co/ERBaxyKIZO",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Devin_Pickell",
+					"name": "Devin",
+					"id": 1030503065898639400,
+					"id_str": "1030503065898639364",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "cartooninperson",
+					"name": "Ashley Ashbee",
+					"id": 109728248,
+					"id_str": "109728248",
+					"indices": [
+						15,
+						31
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1079225859662172200,
+					"id_str": "1079225859662172160",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/DvouM42UUAASo2G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvouM42UUAASo2G.jpg",
+					"url": "https://t.co/ERBaxyKIZO",
+					"display_url": "pic.twitter.com/ERBaxyKIZO",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1079225872295415808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 405,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 610,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 610,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1079225859662172200,
+					"id_str": "1079225859662172160",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/DvouM42UUAASo2G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvouM42UUAASo2G.jpg",
+					"url": "https://t.co/ERBaxyKIZO",
+					"display_url": "pic.twitter.com/ERBaxyKIZO",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1079225872295415808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 405,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 610,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 610,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1079221684455071700,
+		"in_reply_to_status_id_str": "1079221684455071745",
+		"in_reply_to_user_id": 1030503065898639400,
+		"in_reply_to_user_id_str": "1030503065898639364",
+		"in_reply_to_screen_name": "Devin_Pickell",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58601,
+			"friends_count": 26714,
+			"listed_count": 1310,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18680,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16687,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Devin_Pickell @cartooninperson Doesn’t say how many drawings but it shows which artists it collaborated with https://t.co/ERBaxyKIZO"
+	},
+	{
 		"created_at": "Sun Dec 30 03:10:18 +0000 2018",
 		"id": 1079213056339501000,
 		"id_str": "1079213056339501056",
