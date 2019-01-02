0da6073f8edb9f94d652b44473a50f949451aee2
debbiediscovers <debbiediscovers@gmail.com>
2019-01-02 22:12:03 +0800

    Added 1 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 02 13:22:36 +0000 2019",
+		"id": 1080454312860385300,
+		"id_str": "1080454312860385280",
+		"full_text": "Banuary! Who’s with me?! \n\n58% said they lost weight.\n57% reported better concentration.\n88% said they saved money.\n80% felt more in control of their drinking.\n71% realized they don’t need a drink to enjoy themselves.\n71% reported sleeping better.\n67% said they had more energy. https://t.co/oG6mZCVe89",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oG6mZCVe89",
+					"expanded_url": "https://twitter.com/loftus/status/1080232376343674881",
+					"display_url": "twitter.com/loftus/status/…",
+					"indices": [
+						279,
+						302
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58654,
+			"friends_count": 26291,
+			"listed_count": 1321,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16714,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "58a4c3a0d54e1400",
+			"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+			"place_type": "admin",
+			"name": "Central Region",
+			"full_name": "Central Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.749959507073,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.36874499902569
+						],
+						[
+							103.749959507073,
+							1.36874499902569
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1080232376343674900,
+		"quoted_status_id_str": "1080232376343674881",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oG6mZCVe89",
+			"expanded": "https://twitter.com/loftus/status/1080232376343674881",
+			"display": "twitter.com/loftus/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 01 22:40:42 +0000 2019",
+			"id": 1080232376343674900,
+			"id_str": "1080232376343674881",
+			"full_text": "Thinking of dropping alcohol for a month? New data shows several ways it can help, even six months later https://t.co/Ac8gbbqRhg via @WSJ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				137
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "WSJ",
+						"name": "The Wall Street Journal",
+						"id": 3108351,
+						"id_str": "3108351",
+						"indices": [
+							133,
+							137
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Ac8gbbqRhg",
+						"expanded_url": "https://www.wsj.com/articles/the-dry-january-effect-11546351200",
+						"display_url": "wsj.com/articles/the-d…",
+						"indices": [
+							105,
+							128
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15563814,
+				"id_str": "15563814",
+				"name": "Peter Loftus",
+				"screen_name": "Loftus",
+				"location": "Philadelphia",
+				"description": "Wall Street Journal @WSJ reporter - drugs, medical devices, healthcare. 215-881-7127, peter.loftus@wsj.com",
+				"url": "http://t.co/CmS03XE5b6",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/CmS03XE5b6",
+								"expanded_url": "http://online.wsj.com/home-page",
+								"display_url": "online.wsj.com/home-page",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16691,
+				"friends_count": 2184,
+				"listed_count": 665,
+				"created_at": "Wed Jul 23 14:50:58 +0000 2008",
+				"favourites_count": 705,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 4121,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFCC4D",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/683316892661100546/SBToYnPX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/683316892661100546/SBToYnPX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15563814/1537047409",
+				"profile_link_color": "FF691F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 20,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Banuary! Who’s with me?! \n\n58% said they lost weight.\n57% reported better concentration.\n88% said they saved money.\n80% felt more in control of their drinking.\n71% realized they don’t need a drink to enjoy themselves.\n71% reported sleeping better.\n67% said they had more energy. https://t.co/oG6mZCVe89"
+	},
+	{
 		"created_at": "Wed Jan 02 13:08:02 +0000 2019",
 		"id": 1080450644266299400,
 		"id_str": "1080450644266299392",
