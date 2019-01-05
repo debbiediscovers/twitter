946a7a581cbda9e77c22ebc6200554158bd40821
debbiediscovers <debbiediscovers@gmail.com>
2019-01-05 21:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31048,
-	"created_at": "Sat Jan 05 07:12:03 +0000 2019",
+	"tweets": 31049,
+	"created_at": "Sat Jan 05 13:12:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 05 12:37:01 +0000 2019",
+		"id": 1081530005358702600,
+		"id_str": "1081530005358702592",
+		"full_text": "@Leo_J_F Used to it for sure. Still drives me bananas tho 🤦🏻‍♀️",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Leo_J_F",
+					"name": "Leo J Freisch",
+					"id": 1935626641,
+					"id_str": "1935626641",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1081528365717340200,
+		"in_reply_to_status_id_str": "1081528365717340166",
+		"in_reply_to_user_id": 1935626641,
+		"in_reply_to_user_id_str": "1935626641",
+		"in_reply_to_screen_name": "Leo_J_F",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58733,
+			"friends_count": 27141,
+			"listed_count": 1320,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18765,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16734,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Leo_J_F Used to it for sure. Still drives me bananas tho 🤦🏻‍♀️"
+	},
+	{
 		"created_at": "Sat Jan 05 07:00:55 +0000 2019",
 		"id": 1081445423317839900,
 		"id_str": "1081445423317839875",
@@ -110,7 +216,7 @@ Grailbird.data.tweets_2019_01 =
 		"created_at": "Sat Jan 05 06:48:32 +0000 2019",
 		"id": 1081442306878627800,
 		"id_str": "1081442306878627841",
-		"full_text": "Can someone explain to this psychopath that the wall collapses in Game of Thrones??? \n\nGood lord 🤦🏻‍♀️🤦🏻‍♀️🤦🏻‍♀️ \n\n#DumpTrump https://t.co/4xk2hyp9OU",
+		"full_text": "Can someone explain to this psychopath that the wall collapses in Game of Thrones??? \n\nGood lord 🤦🏻♀️🤦🏻♀️🤦🏻♀️ \n\n#DumpTrump https://t.co/4xk2hyp9OU",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -319,7 +425,7 @@ Grailbird.data.tweets_2019_01 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Can someone explain to this psychopath that the wall collapses in Game of Thrones??? \n\nGood lord 🤦🏻‍♀️🤦🏻‍♀️🤦🏻‍♀️ \n\n#DumpTrump https://t.co/4xk2hyp9OU"
+		"text": "Can someone explain to this psychopath that the wall collapses in Game of Thrones??? \n\nGood lord 🤦🏻♀️🤦🏻♀️🤦🏻♀️ \n\n#DumpTrump https://t.co/4xk2hyp9OU"
 	},
 	{
 		"created_at": "Sat Jan 05 05:36:51 +0000 2019",
