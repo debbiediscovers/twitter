e63d5359f423c1b3ba7eba97246abe3cc637f185
debbiediscovers <debbiediscovers@gmail.com>
2019-01-07 12:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31103,
-	"created_at": "Mon Jan 07 02:12:03 +0000 2019",
+	"tweets": 31104,
+	"created_at": "Mon Jan 07 04:12:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,373 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Mon Jan 07 03:21:22 +0000 2019",
+		"id": 1082114944504520700,
+		"id_str": "1082114944504520705",
+		"full_text": "This Fiji Water girl has photobombed every red carpet photo and she has a twitter account and a Now This feature video already?! Crazy! This is next level #newsjacking #hollywoodjacking #photobombing https://t.co/5cykxXxt7L",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "newsjacking",
+					"indices": [
+						155,
+						167
+					]
+				},
+				{
+					"text": "hollywoodjacking",
+					"indices": [
+						168,
+						185
+					]
+				},
+				{
+					"text": "photobombing",
+					"indices": [
+						186,
+						199
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5cykxXxt7L",
+					"expanded_url": "https://twitter.com/watergirlgg/status/1082109897226051589",
+					"display_url": "twitter.com/watergirlgg/st…",
+					"indices": [
+						200,
+						223
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58938,
+			"friends_count": 28196,
+			"listed_count": 1322,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18813,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16745,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "58a4c3a0d54e1400",
+			"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+			"place_type": "admin",
+			"name": "Central Region",
+			"full_name": "Central Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.749959507073,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.36874499902569
+						],
+						[
+							103.749959507073,
+							1.36874499902569
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1082109897226051600,
+		"quoted_status_id_str": "1082109897226051589",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5cykxXxt7L",
+			"expanded": "https://twitter.com/watergirlgg/status/1082109897226051589",
+			"display": "twitter.com/watergirlgg/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 07 03:01:18 +0000 2019",
+			"id": 1082109897226051600,
+			"id_str": "1082109897226051589",
+			"full_text": "You've met an hour ago, but you are still too thristy for more of me. Would u like some Fiji? 😏 #GoldenGlobes https://t.co/4h5KMAbG0d",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				109
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GoldenGlobes",
+						"indices": [
+							96,
+							109
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1082109807258005500,
+						"id_str": "1082109807258005504",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1082109807258005504/pu/img/4R7FPHP4pzeidT1l.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1082109807258005504/pu/img/4R7FPHP4pzeidT1l.jpg",
+						"url": "https://t.co/4h5KMAbG0d",
+						"display_url": "pic.twitter.com/4h5KMAbG0d",
+						"expanded_url": "https://twitter.com/watergirlGG/status/1082109897226051589/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1082109807258005500,
+						"id_str": "1082109807258005504",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1082109807258005504/pu/img/4R7FPHP4pzeidT1l.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1082109807258005504/pu/img/4R7FPHP4pzeidT1l.jpg",
+						"url": "https://t.co/4h5KMAbG0d",
+						"display_url": "pic.twitter.com/4h5KMAbG0d",
+						"expanded_url": "https://twitter.com/watergirlGG/status/1082109897226051589/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 35636,
+							"variants": [
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1082109807258005504/pu/vid/720x720/2NSnF9NguhXB3rCJ.mp4?tag=6"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1082109807258005504/pu/vid/480x480/aVSCnMCqRlLSrJD_.mp4?tag=6"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1082109807258005504/pu/vid/320x320/ZamJE-z3bWJzsYy7.mp4?tag=6"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1082109807258005504/pu/pl/mdHcJDs8kSiIKiiw.m3u8?tag=6"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1082098861496242200,
+				"id_str": "1082098861496242176",
+				"name": "FIJI Water Girl",
+				"screen_name": "watergirlGG",
+				"location": "Los Angeles, CA",
+				"description": "Golden Globes' Breakout Water Girl. Drink Fiji!",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 238,
+				"friends_count": 6,
+				"listed_count": 0,
+				"created_at": "Mon Jan 07 02:17:27 +0000 2019",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 70,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1082123507092410369/ZAyVJfVP_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1082123507092410369/ZAyVJfVP_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1082098861496242176/1546829860",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 83,
+			"favorite_count": 259,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This Fiji Water girl has photobombed every red carpet photo and she has a twitter account and a Now This feature video already?! Crazy! This is next level #newsjacking #hollywoodjacking #photobombing https://t.co/5cykxXxt7L"
+	},
+	{
 		"created_at": "Mon Jan 07 01:51:13 +0000 2019",
 		"id": 1082092260471201800,
 		"id_str": "1082092260471201792",
