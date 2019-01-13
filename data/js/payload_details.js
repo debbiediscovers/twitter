a88b944a0464920b2e634510ecb9680c6c4128bf
debbiediscovers <debbiediscovers@gmail.com>
2019-01-13 23:12:08 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31596,
-	"created_at": "Sun Jan 13 14:12:07 +0000 2019",
+	"tweets": 31597,
+	"created_at": "Sun Jan 13 15:12:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Jan 13 14:26:32 +0000 2019",
+		"id": 1084456669105410000,
+		"id_str": "1084456669105410048",
+		"full_text": "@triotres Try it! Scan with Snapchat camera. Hold the QR code until it unlocks. https://t.co/G44BYlEYLK",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "triotres",
+					"name": "mc",
+					"id": 93893516,
+					"id_str": "93893516",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1084456656140824600,
+					"id_str": "1084456656140824576",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/DwzDlUOU8AAWBqB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwzDlUOU8AAWBqB.jpg",
+					"url": "https://t.co/G44BYlEYLK",
+					"display_url": "pic.twitter.com/G44BYlEYLK",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1084456669105410048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 672,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1012,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084456656140824600,
+					"id_str": "1084456656140824576",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/DwzDlUOU8AAWBqB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwzDlUOU8AAWBqB.jpg",
+					"url": "https://t.co/G44BYlEYLK",
+					"display_url": "pic.twitter.com/G44BYlEYLK",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1084456669105410048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 672,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1012,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1084456275646263300,
+		"in_reply_to_status_id_str": "1084456275646263297",
+		"in_reply_to_user_id": 93893516,
+		"in_reply_to_user_id_str": "93893516",
+		"in_reply_to_screen_name": "triotres",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60103,
+			"friends_count": 32561,
+			"listed_count": 1321,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 18996,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16830,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@triotres Try it! Scan with Snapchat camera. Hold the QR code until it unlocks. https://t.co/G44BYlEYLK"
+	},
+	{
 		"created_at": "Sun Jan 13 14:00:28 +0000 2019",
 		"id": 1084450106537996300,
 		"id_str": "1084450106537996289",
