e0749fa89ceb30356b06d23b9744c7de7f01a01d
debbiediscovers <debbiediscovers@gmail.com>
2019-01-15 04:12:10 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31600,
-	"created_at": "Mon Jan 14 12:12:06 +0000 2019",
+	"tweets": 31601,
+	"created_at": "Mon Jan 14 20:12:08 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Mon Jan 14 19:50:45 +0000 2019",
+		"id": 1084900645981675500,
+		"id_str": "1084900645981675520",
+		"full_text": "@phyrebrat @IntenseCityUk 😊😊😊 https://t.co/GeZc8hWj54",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "phyrebrat",
+					"name": "Christopher Bean",
+					"id": 142279510,
+					"id_str": "142279510",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "IntenseCityUk",
+					"name": "IntenseCity UK",
+					"id": 2255603670,
+					"id_str": "2255603670",
+					"indices": [
+						11,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1084900633252003800,
+					"id_str": "1084900633252003840",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dw5XYL2VAAA4tLO.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dw5XYL2VAAA4tLO.jpg",
+					"url": "https://t.co/GeZc8hWj54",
+					"display_url": "pic.twitter.com/GeZc8hWj54",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1084900645981675520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084900633252003800,
+					"id_str": "1084900633252003840",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dw5XYL2VAAA4tLO.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dw5XYL2VAAA4tLO.jpg",
+					"url": "https://t.co/GeZc8hWj54",
+					"display_url": "pic.twitter.com/GeZc8hWj54",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1084900645981675520/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 476,
+							"h": 322,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							34,
+							23
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Dw5XYL2VAAA4tLO.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1084775228952195100,
+		"in_reply_to_status_id_str": "1084775228952195072",
+		"in_reply_to_user_id": 142279510,
+		"in_reply_to_user_id_str": "142279510",
+		"in_reply_to_screen_name": "phyrebrat",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | @Medium Top writer in #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60332,
+			"friends_count": 33439,
+			"listed_count": 1323,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19002,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16834,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@phyrebrat @IntenseCityUk 😊😊😊 https://t.co/GeZc8hWj54"
+	},
+	{
 		"created_at": "Mon Jan 14 11:58:51 +0000 2019",
 		"id": 1084781889401978900,
 		"id_str": "1084781889401978880",
