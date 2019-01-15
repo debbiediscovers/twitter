cd944924698f57efe8527b12b2eeddad4f6cd9f1
debbiediscovers <debbiediscovers@gmail.com>
2019-01-15 15:12:08 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31613,
-	"created_at": "Tue Jan 15 06:12:07 +0000 2019",
+	"tweets": 31614,
+	"created_at": "Tue Jan 15 07:12:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Jan 06 19:09:12 +0000 2019",
+		"id": 1081991089932955600,
+		"id_str": "1085064331463544832",
+		"full_text": "Trump airlines? Shut down.\nTrump casinos? Shut down.\nTrump steaks? Shut down.\nTrump university? Shut down.\nTrump magazine? Shut down.\n\n@realDonaldTrump kept his promise to run our government like he ran his companies: shut down, w/ unpaid workers &amp; blaming everyone but himself.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "realDonaldTrump",
+					"name": "Donald J. Trump",
+					"id": 25073877,
+					"id_str": "25073877",
+					"indices": [
+						135,
+						151
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 242426145,
+			"id_str": "242426145",
+			"name": "Rep. Jim McGovern",
+			"screen_name": "RepMcGovern",
+			"location": "Massachusetts, USA",
+			"description": "Dad. Husband. Chairman of @RulesDemocrats. Born & raised in Worcester. Proudly working #ForThePeople of Massachusetts’ 2nd District.",
+			"url": "https://t.co/f2XcW4hsKs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/f2XcW4hsKs",
+							"expanded_url": "http://mcgovern.house.gov",
+							"display_url": "mcgovern.house.gov",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44122,
+			"friends_count": 655,
+			"listed_count": 1396,
+			"created_at": "Mon Jan 24 19:18:36 +0000 2011",
+			"favourites_count": 219,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8944,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1075476348666372096/AQaATVA3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1075476348666372096/AQaATVA3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/242426145/1505311858",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 68498,
+		"favorite_count": 214816,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Trump airlines? Shut down.\nTrump casinos? Shut down.\nTrump steaks? Shut down.\nTrump university? Shut down.\nTrump magazine? Shut down.\n\n@realDonaldTrump kept his promise to run our government like he ran his companies: shut down, w/ unpaid workers &amp; blaming everyone but himself."
+	},
+	{
 		"created_at": "Tue Jan 15 05:52:39 +0000 2019",
 		"id": 1085052119420350500,
 		"id_str": "1085052119420350465",
