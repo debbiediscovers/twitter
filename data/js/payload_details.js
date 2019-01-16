0fe934d28e246979fb44285c093a7ceb600a8061
debbiediscovers <debbiediscovers@gmail.com>
2019-01-17 07:12:10 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31623,
-	"created_at": "Wed Jan 16 22:12:10 +0000 2019",
+	"tweets": 31624,
+	"created_at": "Wed Jan 16 23:12:09 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 16 22:16:17 +0000 2019",
+		"id": 1085662047507865600,
+		"id_str": "1085662047507865601",
+		"full_text": "@randyclarktko I build schools with @PencilsOfPromis !\nhttps://t.co/wJ8XKQpOnB\nhttps://t.co/dgiktKdVPc\nhttps://t.co/KfBdvavSqW",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "randyclarktko",
+					"name": "randy clark",
+					"id": 167508932,
+					"id_str": "167508932",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "PencilsOfPromis",
+					"name": "Pencils of Promise",
+					"id": 18369852,
+					"id_str": "18369852",
+					"indices": [
+						36,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wJ8XKQpOnB",
+					"expanded_url": "https://pencilsofpromise.org/som-sanouk-laos/",
+					"display_url": "pencilsofpromise.org/som-sanouk-lao…",
+					"indices": [
+						55,
+						78
+					]
+				},
+				{
+					"url": "https://t.co/dgiktKdVPc",
+					"expanded_url": "https://pencilsofpromise.org/mak-phouk-laos/",
+					"display_url": "pencilsofpromise.org/mak-phouk-laos/",
+					"indices": [
+						79,
+						102
+					]
+				},
+				{
+					"url": "https://t.co/KfBdvavSqW",
+					"expanded_url": "https://pencilsofpromise.org/houay-yen-laos/",
+					"display_url": "pencilsofpromise.org/houay-yen-laos/",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1085507637251358700,
+		"in_reply_to_status_id_str": "1085507637251358720",
+		"in_reply_to_user_id": 167508932,
+		"in_reply_to_user_id_str": "167508932",
+		"in_reply_to_screen_name": "randyclarktko",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60424,
+			"friends_count": 29926,
+			"listed_count": 1323,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19051,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16856,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@randyclarktko I build schools with @PencilsOfPromis !\nhttps://t.co/wJ8XKQpOnB\nhttps://t.co/dgiktKdVPc\nhttps://t.co/KfBdvavSqW"
+	},
+	{
 		"created_at": "Thu Aug 16 12:14:03 +0000 2018",
 		"id": 1030065148864561200,
 		"id_str": "1085655043552862208",
