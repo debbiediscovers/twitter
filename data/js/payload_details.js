a15636a1969d2731b74a86ee1a7006cd1694cb45
debbiediscovers <debbiediscovers@gmail.com>
2019-02-28 15:12:05 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31859,
-	"created_at": "Thu Feb 28 03:12:03 +0000 2019",
+	"tweets": 31860,
+	"created_at": "Thu Feb 28 07:12:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Thu Feb 28 06:17:49 +0000 2019",
+		"id": 1101003520222801900,
+		"id_str": "1101003520222801921",
+		"full_text": "\"To have a creative life, we must lose our fear of being wrong.\"\n\n#Quote from the newsletter of @brianclark @furtherHQ https://t.co/qPDSS3IWmx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Quote",
+					"indices": [
+						66,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "brianclark",
+					"name": "Brian Clark",
+					"id": 10367,
+					"id_str": "10367",
+					"indices": [
+						96,
+						107
+					]
+				},
+				{
+					"screen_name": "furtherHQ",
+					"name": "further",
+					"id": 2517004658,
+					"id_str": "2517004658",
+					"indices": [
+						108,
+						118
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101003209617813500,
+					"id_str": "1101003209617813504",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/D0eMlfXVsAAj-Hk.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0eMlfXVsAAj-Hk.png",
+					"url": "https://t.co/qPDSS3IWmx",
+					"display_url": "pic.twitter.com/qPDSS3IWmx",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101003520222801921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 760,
+							"h": 350,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 313,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 760,
+							"h": 350,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101003209617813500,
+					"id_str": "1101003209617813504",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/D0eMlfXVsAAj-Hk.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0eMlfXVsAAj-Hk.png",
+					"url": "https://t.co/qPDSS3IWmx",
+					"display_url": "pic.twitter.com/qPDSS3IWmx",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101003520222801921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 760,
+							"h": 350,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 313,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 760,
+							"h": 350,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62585,
+			"friends_count": 29996,
+			"listed_count": 1330,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19469,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17036,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"To have a creative life, we must lose our fear of being wrong.\"\n\n#Quote from the newsletter of @brianclark @furtherHQ https://t.co/qPDSS3IWmx"
+	},
+	{
 		"created_at": "Wed Feb 27 01:19:24 +0000 2019",
 		"id": 1100566031628566500,
 		"id_str": "1100949804211888129",
