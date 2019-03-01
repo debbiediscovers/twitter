e656caedd167f0722ef5cd1d821b699a96ba18cd
debbiediscovers <debbiediscovers@gmail.com>
2019-03-01 20:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31877,
-	"created_at": "Fri Mar 01 11:12:00 +0000 2019",
+	"tweets": 31878,
+	"created_at": "Fri Mar 01 12:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 01 11:53:02 +0000 2019",
+		"id": 1101450269408124900,
+		"id_str": "1101450269408124928",
+		"full_text": "@SternbergReed R u sure 🙃 https://t.co/ajtJfEGz1T",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SternbergReed",
+					"name": "Sternberg Reed",
+					"id": 2264064666,
+					"id_str": "2264064666",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101450260449132500,
+					"id_str": "1101450260449132545",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D0kjLRfVAAE_TzL.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D0kjLRfVAAE_TzL.jpg",
+					"url": "https://t.co/ajtJfEGz1T",
+					"display_url": "pic.twitter.com/ajtJfEGz1T",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101450269408124928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101450260449132500,
+					"id_str": "1101450260449132545",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D0kjLRfVAAE_TzL.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D0kjLRfVAAE_TzL.jpg",
+					"url": "https://t.co/ajtJfEGz1T",
+					"display_url": "pic.twitter.com/ajtJfEGz1T",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101450269408124928/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 286,
+							"h": 400,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							143,
+							200
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D0kjLRfVAAE_TzL.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1101444596477976600,
+		"in_reply_to_status_id_str": "1101444596477976576",
+		"in_reply_to_user_id": 2264064666,
+		"in_reply_to_user_id_str": "2264064666",
+		"in_reply_to_screen_name": "SternbergReed",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62614,
+			"friends_count": 30134,
+			"listed_count": 1331,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19500,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17051,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "6635b2fcebd13c64",
+			"url": "https://api.twitter.com/1.1/geo/id/6635b2fcebd13c64.json",
+			"place_type": "admin",
+			"name": "East Region",
+			"full_name": "East Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.89216999988,
+							1.30157724092846
+						],
+						[
+							104.085566789885,
+							1.30157724092846
+						],
+						[
+							104.085566789885,
+							1.44093662101956
+						],
+						[
+							103.89216999988,
+							1.44093662101956
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@SternbergReed R u sure 🙃 https://t.co/ajtJfEGz1T"
+	},
+	{
 		"created_at": "Fri Mar 01 11:05:53 +0000 2019",
 		"id": 1101438403734691800,
 		"id_str": "1101438403734691840",
