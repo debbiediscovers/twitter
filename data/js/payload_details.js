ddeb9b3532ef3ddb2d58ea8f35cf155f56115ca1
debbiediscovers <debbiediscovers@gmail.com>
2019-03-01 15:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31872,
-	"created_at": "Fri Mar 01 04:12:02 +0000 2019",
+	"tweets": 31873,
+	"created_at": "Fri Mar 01 07:12:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,354 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 01 06:39:16 +0000 2019",
+		"id": 1101371307999023100,
+		"id_str": "1101371307999023104",
+		"full_text": "@christianrenard @jbonnel @fadouce @SpirosMargaris @jblefevre60 @jhagel @stratandbiz @kalydeoo @ahniati @sandrinea @EskoKilpi @FGraillot @philippejouan @EricScherer @rossdawson @dhinchcliffe Happy Friday! https://t.co/oV2scQoMZL",
+		"truncated": false,
+		"display_text_range": [
+			191,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "christianrenard",
+					"name": "Christian Renard",
+					"id": 20417089,
+					"id_str": "20417089",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "jbonnel",
+					"name": "Julien Bonnel",
+					"id": 14171812,
+					"id_str": "14171812",
+					"indices": [
+						17,
+						25
+					]
+				},
+				{
+					"screen_name": "fadouce",
+					"name": "fabienne billat",
+					"id": 123298601,
+					"id_str": "123298601",
+					"indices": [
+						26,
+						34
+					]
+				},
+				{
+					"screen_name": "SpirosMargaris",
+					"name": "Spiros Margaris",
+					"id": 2711212681,
+					"id_str": "2711212681",
+					"indices": [
+						35,
+						50
+					]
+				},
+				{
+					"screen_name": "jblefevre60",
+					"name": "Jean-Baptiste Lefevre",
+					"id": 4374719908,
+					"id_str": "4374719908",
+					"indices": [
+						51,
+						63
+					]
+				},
+				{
+					"screen_name": "jhagel",
+					"name": "John Hagel",
+					"id": 14076943,
+					"id_str": "14076943",
+					"indices": [
+						64,
+						71
+					]
+				},
+				{
+					"screen_name": "stratandbiz",
+					"name": "strategy+business",
+					"id": 365495044,
+					"id_str": "365495044",
+					"indices": [
+						72,
+						84
+					]
+				},
+				{
+					"screen_name": "kalydeoo",
+					"name": "Eric Gaubert",
+					"id": 380487157,
+					"id_str": "380487157",
+					"indices": [
+						85,
+						94
+					]
+				},
+				{
+					"screen_name": "ahniati",
+					"name": "AHNIATI",
+					"id": 413812972,
+					"id_str": "413812972",
+					"indices": [
+						95,
+						103
+					]
+				},
+				{
+					"screen_name": "sandrinea",
+					"name": "Sandrine",
+					"id": 26217954,
+					"id_str": "26217954",
+					"indices": [
+						104,
+						114
+					]
+				},
+				{
+					"screen_name": "EskoKilpi",
+					"name": "Esko Kilpi",
+					"id": 18836209,
+					"id_str": "18836209",
+					"indices": [
+						115,
+						125
+					]
+				},
+				{
+					"screen_name": "FGraillot",
+					"name": "Florian Graillot",
+					"id": 386169875,
+					"id_str": "386169875",
+					"indices": [
+						126,
+						136
+					]
+				},
+				{
+					"screen_name": "philippejouan",
+					"name": "philippe j",
+					"id": 80486154,
+					"id_str": "80486154",
+					"indices": [
+						137,
+						151
+					]
+				},
+				{
+					"screen_name": "EricScherer",
+					"name": "Eric Scherer",
+					"id": 14813149,
+					"id_str": "14813149",
+					"indices": [
+						152,
+						164
+					]
+				},
+				{
+					"screen_name": "rossdawson",
+					"name": "Ross Dawson",
+					"id": 15184929,
+					"id_str": "15184929",
+					"indices": [
+						165,
+						176
+					]
+				},
+				{
+					"screen_name": "dhinchcliffe",
+					"name": "Dion Hinchcliffe #MWC19",
+					"id": 1585,
+					"id_str": "1585",
+					"indices": [
+						177,
+						190
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101371297412599800,
+					"id_str": "1101371297412599809",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D0jbXBQW0AEdeq1.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D0jbXBQW0AEdeq1.jpg",
+					"url": "https://t.co/oV2scQoMZL",
+					"display_url": "pic.twitter.com/oV2scQoMZL",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101371307999023104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101371297412599800,
+					"id_str": "1101371297412599809",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D0jbXBQW0AEdeq1.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D0jbXBQW0AEdeq1.jpg",
+					"url": "https://t.co/oV2scQoMZL",
+					"display_url": "pic.twitter.com/oV2scQoMZL",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1101371307999023104/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 250,
+							"h": 200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							5,
+							4
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D0jbXBQW0AEdeq1.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1101365072776040400,
+		"in_reply_to_status_id_str": "1101365072776040449",
+		"in_reply_to_user_id": 20417089,
+		"in_reply_to_user_id_str": "20417089",
+		"in_reply_to_screen_name": "christianrenard",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62586,
+			"friends_count": 30022,
+			"listed_count": 1328,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17045,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@christianrenard @jbonnel @fadouce @SpirosMargaris @jblefevre60 @jhagel @stratandbiz @kalydeoo @ahniati @sandrinea @EskoKilpi @FGraillot @philippejouan @EricScherer @rossdawson @dhinchcliffe Happy Friday! https://t.co/oV2scQoMZL"
+	},
+	{
 		"created_at": "Thu Feb 28 01:50:38 +0000 2019",
 		"id": 1100936280341401600,
 		"id_str": "1101325323377885185",
