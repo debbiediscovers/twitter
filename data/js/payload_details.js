e4348af77c02960311be1d7e63dae57b3ac4ffe0
debbiediscovers <debbiediscovers@gmail.com>
2019-03-06 16:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31907,
-	"created_at": "Wed Mar 06 01:12:01 +0000 2019",
+	"tweets": 31908,
+	"created_at": "Wed Mar 06 08:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 07:37:50 +0000 2019",
+		"id": 1103197985364738000,
+		"id_str": "1103197985364738048",
+		"full_text": "I love my daily walks in the park. 🌲🌳🌴\n⠀⠀\nResearch tells us that when people are exposed to the natural environment and natural features, they tend to have a reduced stress response. \nWhen you are out in nature you have lower blood pressure, bett… https://t.co/m7KoyrbFUK https://t.co/GiTYMGx4Gv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/m7KoyrbFUK",
+					"expanded_url": "https://www.instagram.com/p/BuqDL4gBEfE/",
+					"display_url": "instagram.com/p/BuqDL4gBEfE/",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1103197984144134100,
+					"id_str": "1103197984144134144",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D09YuLnWkAA7538.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D09YuLnWkAA7538.jpg",
+					"url": "https://t.co/GiTYMGx4Gv",
+					"display_url": "pic.twitter.com/GiTYMGx4Gv",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1103197985364738048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 759,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 759,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 573,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103197984144134100,
+					"id_str": "1103197984144134144",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D09YuLnWkAA7538.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D09YuLnWkAA7538.jpg",
+					"url": "https://t.co/GiTYMGx4Gv",
+					"display_url": "pic.twitter.com/GiTYMGx4Gv",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1103197985364738048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 759,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 759,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 573,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62759,
+			"friends_count": 30137,
+			"listed_count": 1337,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19558,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17074,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I love my daily walks in the park. 🌲🌳🌴\n⠀⠀\nResearch tells us that when people are exposed to the natural environment and natural features, they tend to have a reduced stress response. \nWhen you are out in nature you have lower blood pressure, bett… https://t.co/m7KoyrbFUK https://t.co/GiTYMGx4Gv"
+	},
+	{
 		"created_at": "Wed Mar 06 00:53:54 +0000 2019",
 		"id": 1103055397659697200,
 		"id_str": "1103096330103140353",
