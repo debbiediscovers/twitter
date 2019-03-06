c69a08e5565f6d19af42458562baa402dbfccfb2
debbiediscovers <debbiediscovers@gmail.com>
2019-03-07 06:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31910,
-	"created_at": "Wed Mar 06 10:12:03 +0000 2019",
+	"tweets": 31911,
+	"created_at": "Wed Mar 06 22:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 21:50:19 +0000 2019",
+		"id": 1103228797183123500,
+		"id_str": "1103412518448320512",
+		"full_text": "80% of viewing hours  on Netflix and 35% of sales on Amazon originate from automated recommendations. Same with majority of matches on dating apps. \n\nGiven these numbers, many of us clearly do not have quite the freedom of choice we believe we do.\n\nhttps://t.co/abtMozfSWs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/abtMozfSWs",
+					"expanded_url": "https://onezero.medium.com/free-will-in-an-algorithmic-world-8d5acb550cb7?fbclid=IwAR0jt7bH6Xf1PFzvpQWR9j_3qCNOUU2n6WqNp0bUNetLtWwp9wUcr82sGnA",
+					"display_url": "onezero.medium.com/free-will-in-a…",
+					"indices": [
+						249,
+						272
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/1ZPsVnM33e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1ZPsVnM33e",
+							"expanded_url": "https://linktr.ee/debbiediscovers",
+							"display_url": "linktr.ee/debbiediscovers",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62743,
+			"friends_count": 30037,
+			"listed_count": 1337,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19556,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17076,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 29,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "80% of viewing hours  on Netflix and 35% of sales on Amazon originate from automated recommendations. Same with majority of matches on dating apps. \n\nGiven these numbers, many of us clearly do not have quite the freedom of choice we believe we do.\n\nhttps://t.co/abtMozfSWs"
+	},
+	{
 		"created_at": "Wed Mar 06 09:40:16 +0000 2019",
 		"id": 1103228797183123500,
 		"id_str": "1103228797183123456",
