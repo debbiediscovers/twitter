c88097d7bd4c9db47adcd77e8e8365e1e393bc98
debbiediscovers <debbiediscovers@gmail.com>
2019-04-04 17:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32066,
-	"created_at": "Wed Apr 03 10:12:01 +0000 2019",
+	"tweets": 32067,
+	"created_at": "Thu Apr 04 09:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 04 08:22:16 +0000 2019",
+		"id": 1113201836498911200,
+		"id_str": "1113718412264361984",
+		"full_text": "Humility leads to greater compassion. A deeper awareness of our own faults and weaknesses makes it easier to be understanding of those who, like us, have stumbled along the way.\n\nNever be proud but show #humility towards others, true #wisdom lies in being humble\n- #Shri_Radhe_Maa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "humility",
+					"indices": [
+						203,
+						212
+					]
+				},
+				{
+					"text": "wisdom",
+					"indices": [
+						234,
+						241
+					]
+				},
+				{
+					"text": "Shri_Radhe_Maa",
+					"indices": [
+						265,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 609940096,
+			"id_str": "609940096",
+			"name": "Peter Kay",
+			"screen_name": "peterkay47",
+			"location": "Singapore",
+			"description": "Mindfulness allows you not to miss out on your life. By focusing our minds in the present moment, we open ourselves to awareness, adaptation and transformation.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301,
+			"friends_count": 475,
+			"listed_count": 1,
+			"created_at": "Sat Jun 16 12:05:23 +0000 2012",
+			"favourites_count": 273,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 365,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/993033070386204673/BJDpO0FG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/993033070386204673/BJDpO0FG_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Humility leads to greater compassion. A deeper awareness of our own faults and weaknesses makes it easier to be understanding of those who, like us, have stumbled along the way.\n\nNever be proud but show #humility towards others, true #wisdom lies in being humble\n- #Shri_Radhe_Maa"
+	},
+	{
 		"created_at": "Wed Apr 03 09:26:04 +0000 2019",
 		"id": 1113372083705176000,
 		"id_str": "1113372083705176065",
