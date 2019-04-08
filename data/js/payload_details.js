7eaae94d1500f332ca5ca7941addd32ec8e2123a
debbiediscovers <debbiediscovers@gmail.com>
2019-04-08 13:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32084,
-	"created_at": "Mon Apr 08 01:12:01 +0000 2019",
+	"tweets": 32085,
+	"created_at": "Mon Apr 08 05:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 08 05:01:03 +0000 2019",
+		"id": 1115117329958850600,
+		"id_str": "1115117329958850560",
+		"full_text": "@DanielJWeeks1 @MaillieDavid @shareparade_com Lol 😆",
+		"truncated": false,
+		"display_text_range": [
+			46,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DanielJWeeks1",
+					"name": "Daniel J. Weeks",
+					"id": 887507234,
+					"id_str": "887507234",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "MaillieDavid",
+					"name": "David F Maillie",
+					"id": 1061620725193363500,
+					"id_str": "1061620725193363456",
+					"indices": [
+						15,
+						28
+					]
+				},
+				{
+					"screen_name": "shareparade_com",
+					"name": "Share Parade",
+					"id": 2973644913,
+					"id_str": "2973644913",
+					"indices": [
+						29,
+						45
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1115052817478180900,
+		"in_reply_to_status_id_str": "1115052817478180864",
+		"in_reply_to_user_id": 887507234,
+		"in_reply_to_user_id_str": "887507234",
+		"in_reply_to_screen_name": "DanielJWeeks1",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Chatbot Evangelist | Speaker | 21 All Star Twitter | School Builder | 2018 @Medium Top writer #AI | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64342,
+			"friends_count": 29620,
+			"listed_count": 1343,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19815,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17180,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@DanielJWeeks1 @MaillieDavid @shareparade_com Lol 😆"
+	},
+	{
 		"created_at": "Mon Apr 08 00:39:51 +0000 2019",
 		"id": 1114844888879779800,
 		"id_str": "1115051595866169346",
