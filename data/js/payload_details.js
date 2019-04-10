38d46d3372bbf1a981134025c59e4c9011c94442
debbiediscovers <debbiediscovers@gmail.com>
2019-04-11 05:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32097,
-	"created_at": "Wed Apr 10 16:12:01 +0000 2019",
+	"tweets": 32098,
+	"created_at": "Wed Apr 10 21:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 10 21:02:20 +0000 2019",
+		"id": 1115892490131923000,
+		"id_str": "1116084019962060800",
+		"full_text": "#Wednesdaywisdom ❤️ https://t.co/44jLnBXuFA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Wednesdaywisdom",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1115892477976809500,
+					"id_str": "1115892477976809473",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/D3xyS-VWkAEwbKd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3xyS-VWkAEwbKd.jpg",
+					"url": "https://t.co/44jLnBXuFA",
+					"display_url": "pic.twitter.com/44jLnBXuFA",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1115892490131922944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1115892477976809500,
+					"id_str": "1115892477976809473",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/D3xyS-VWkAEwbKd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3xyS-VWkAEwbKd.jpg",
+					"url": "https://t.co/44jLnBXuFA",
+					"display_url": "pic.twitter.com/44jLnBXuFA",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1115892490131922944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 595,
+							"h": 595,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64417,
+			"friends_count": 29431,
+			"listed_count": 1348,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17188,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#Wednesdaywisdom ❤️ https://t.co/44jLnBXuFA"
+	},
+	{
 		"created_at": "Wed Apr 10 15:26:58 +0000 2019",
 		"id": 1115892490131923000,
 		"id_str": "1115999619282903042",
