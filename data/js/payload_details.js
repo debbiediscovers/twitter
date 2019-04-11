0630fd0e2e1283a73b088f0c9a3da64f17924c14
debbiediscovers <debbiediscovers@gmail.com>
2019-04-11 20:12:07 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32098,
-	"created_at": "Wed Apr 10 21:12:01 +0000 2019",
+	"tweets": 32099,
+	"created_at": "Thu Apr 11 12:12:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 11 11:24:06 +0000 2019",
+		"id": 1114292888689463300,
+		"id_str": "1116300887347982337",
+		"full_text": "“Humility and knowledge in poor clothes excel pride and ignorance in costly attire.”\n- #William_Penn\n\nIf you are #humble nothing will touch you, neither praise nor disgrace, because you know what you are. \n- #Mother_Teresa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "William_Penn",
+					"indices": [
+						87,
+						100
+					]
+				},
+				{
+					"text": "humble",
+					"indices": [
+						113,
+						120
+					]
+				},
+				{
+					"text": "Mother_Teresa",
+					"indices": [
+						208,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 609940096,
+			"id_str": "609940096",
+			"name": "Peter Kay",
+			"screen_name": "peterkay47",
+			"location": "Singapore",
+			"description": "Mindfulness allows you not to miss out on your life. By focusing our minds in the present moment, we open ourselves to awareness, adaptation and transformation.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 482,
+			"listed_count": 2,
+			"created_at": "Sat Jun 16 12:05:23 +0000 2012",
+			"favourites_count": 274,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 372,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/993033070386204673/BJDpO0FG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/993033070386204673/BJDpO0FG_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "“Humility and knowledge in poor clothes excel pride and ignorance in costly attire.”\n- #William_Penn\n\nIf you are #humble nothing will touch you, neither praise nor disgrace, because you know what you are. \n- #Mother_Teresa"
+	},
+	{
 		"created_at": "Wed Apr 10 21:02:20 +0000 2019",
 		"id": 1115892490131923000,
 		"id_str": "1116084019962060800",
