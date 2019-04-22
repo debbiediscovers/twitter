ce6b886ef301938f1ff773ba3ca60335f9828d48
debbiediscovers <debbiediscovers@gmail.com>
2019-04-22 18:12:03 +0800

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,282 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 22 09:37:49 +0000 2019",
+		"id": 1120260409703960600,
+		"id_str": "1120260409703960576",
+		"full_text": "Don’t leaf me this way \n#puns #punny #purple #rainbowfeed #agameoftones #coloraddict #colorhunters #ig_color #howihue #ihavethisthingwithcolor #livecolorfully #botanicgardens #botanico https://t.co/PXOhaJXWW7 https://t.co/1i8sfgVaEs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "puns",
+					"indices": [
+						24,
+						29
+					]
+				},
+				{
+					"text": "punny",
+					"indices": [
+						30,
+						36
+					]
+				},
+				{
+					"text": "purple",
+					"indices": [
+						37,
+						44
+					]
+				},
+				{
+					"text": "rainbowfeed",
+					"indices": [
+						45,
+						57
+					]
+				},
+				{
+					"text": "agameoftones",
+					"indices": [
+						58,
+						71
+					]
+				},
+				{
+					"text": "coloraddict",
+					"indices": [
+						72,
+						84
+					]
+				},
+				{
+					"text": "colorhunters",
+					"indices": [
+						85,
+						98
+					]
+				},
+				{
+					"text": "ig_color",
+					"indices": [
+						99,
+						108
+					]
+				},
+				{
+					"text": "howihue",
+					"indices": [
+						109,
+						117
+					]
+				},
+				{
+					"text": "ihavethisthingwithcolor",
+					"indices": [
+						118,
+						142
+					]
+				},
+				{
+					"text": "livecolorfully",
+					"indices": [
+						143,
+						158
+					]
+				},
+				{
+					"text": "botanicgardens",
+					"indices": [
+						159,
+						174
+					]
+				},
+				{
+					"text": "botanico",
+					"indices": [
+						175,
+						184
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PXOhaJXWW7",
+					"expanded_url": "https://www.instagram.com/p/BwjXNEnHYa9/",
+					"display_url": "instagram.com/p/BwjXNEnHYa9/",
+					"indices": [
+						185,
+						208
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1120260408751927300,
+					"id_str": "1120260408751927298",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/D4v26AeXsAIA8Tn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4v26AeXsAIA8Tn.jpg",
+					"url": "https://t.co/1i8sfgVaEs",
+					"display_url": "pic.twitter.com/1i8sfgVaEs",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1120260409703960576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1120260408751927300,
+					"id_str": "1120260408751927298",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/D4v26AeXsAIA8Tn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4v26AeXsAIA8Tn.jpg",
+					"url": "https://t.co/1i8sfgVaEs",
+					"display_url": "pic.twitter.com/1i8sfgVaEs",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1120260409703960576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64766,
+			"friends_count": 29428,
+			"listed_count": 1350,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 19926,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17205,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Don’t leaf me this way \n#puns #punny #purple #rainbowfeed #agameoftones #coloraddict #colorhunters #ig_color #howihue #ihavethisthingwithcolor #livecolorfully #botanicgardens #botanico https://t.co/PXOhaJXWW7 https://t.co/1i8sfgVaEs"
+	},
+	{
 		"created_at": "Sat Apr 20 02:26:54 +0000 2019",
 		"id": 1119427190943178800,
 		"id_str": "1119427190943178752",
