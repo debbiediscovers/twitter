138eced83d030cea6cae86bd25acd69c3779a66b
debbiediscovers <debbiediscovers@gmail.com>
2019-04-30 13:12:05 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32196,
-	"created_at": "Tue Apr 30 04:12:03 +0000 2019",
+	"tweets": 32198,
+	"created_at": "Tue Apr 30 05:12:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,396 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 30 04:31:51 +0000 2019",
+		"id": 1123082072430481400,
+		"id_str": "1123082513457369091",
+		"full_text": "@over50optimized @jockowillink https://t.co/9KWYe8bcGC",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "over50optimized",
+					"name": "Over 50 Ben",
+					"id": 32897323,
+					"id_str": "32897323",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "jockowillink",
+					"name": "Jocko Willink",
+					"id": 3320010078,
+					"id_str": "3320010078",
+					"indices": [
+						17,
+						30
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123082062691328000,
+					"id_str": "1123082062691328000",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"url": "https://t.co/9KWYe8bcGC",
+					"display_url": "pic.twitter.com/9KWYe8bcGC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1123082072430481408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1820,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123082062691328000,
+					"id_str": "1123082062691328000",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"url": "https://t.co/9KWYe8bcGC",
+					"display_url": "pic.twitter.com/9KWYe8bcGC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1123082072430481408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1820,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1122834435383136300,
+		"in_reply_to_status_id_str": "1122834435383136257",
+		"in_reply_to_user_id": 32897323,
+		"in_reply_to_user_id_str": "32897323",
+		"in_reply_to_screen_name": "over50optimized",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64750,
+			"friends_count": 30131,
+			"listed_count": 1354,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20019,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17252,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@over50optimized @jockowillink https://t.co/9KWYe8bcGC"
+	},
+	{
+		"created_at": "Tue Apr 30 04:30:06 +0000 2019",
+		"id": 1123082072430481400,
+		"id_str": "1123082072430481408",
+		"full_text": "@over50optimized @jockowillink https://t.co/9KWYe8bcGC",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "over50optimized",
+					"name": "Over 50 Ben",
+					"id": 32897323,
+					"id_str": "32897323",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "jockowillink",
+					"name": "Jocko Willink",
+					"id": 3320010078,
+					"id_str": "3320010078",
+					"indices": [
+						17,
+						30
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123082062691328000,
+					"id_str": "1123082062691328000",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"url": "https://t.co/9KWYe8bcGC",
+					"display_url": "pic.twitter.com/9KWYe8bcGC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1123082072430481408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1820,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123082062691328000,
+					"id_str": "1123082062691328000",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5X9L4hUYAA08Ip.jpg",
+					"url": "https://t.co/9KWYe8bcGC",
+					"display_url": "pic.twitter.com/9KWYe8bcGC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1123082072430481408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1820,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1122834435383136300,
+		"in_reply_to_status_id_str": "1122834435383136257",
+		"in_reply_to_user_id": 32897323,
+		"in_reply_to_user_id_str": "32897323",
+		"in_reply_to_screen_name": "over50optimized",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64750,
+			"friends_count": 30131,
+			"listed_count": 1354,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20019,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17252,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@over50optimized @jockowillink https://t.co/9KWYe8bcGC"
+	},
+	{
 		"created_at": "Tue Apr 30 03:39:08 +0000 2019",
 		"id": 1123069246215376900,
 		"id_str": "1123069246215376896",
