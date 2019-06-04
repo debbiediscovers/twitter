ca542b3209d734f1b399d7600df7178934b19199
debbiediscovers <debbiediscovers@gmail.com>
2019-06-04 18:12:12 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32399,
-	"created_at": "Tue Jun 04 00:12:03 +0000 2019",
+	"tweets": 32400,
+	"created_at": "Tue Jun 04 10:12:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 04 10:02:10 +0000 2019",
+		"id": 1135592474833932300,
+		"id_str": "1135849212669644801",
+		"full_text": "Uh-oh... https://t.co/cdRXUg5mqn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cdRXUg5mqn",
+					"expanded_url": "https://twitter.com/zackwhittaker/status/1135586461535940609",
+					"display_url": "twitter.com/zackwhittaker/…",
+					"indices": [
+						9,
+						32
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65431,
+			"friends_count": 30102,
+			"listed_count": 1361,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20250,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17374,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1135586461535940600,
+		"quoted_status_id_str": "1135586461535940609",
+		"quoted_status_permalink": {
+			"url": "https://t.co/cdRXUg5mqn",
+			"expanded": "https://twitter.com/zackwhittaker/status/1135586461535940609",
+			"display": "twitter.com/zackwhittaker/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 03 16:38:05 +0000 2019",
+			"id": 1135586461535940600,
+			"id_str": "1135586461535940609",
+			"full_text": "Russia's internet and communications regulator has told Tinder's parent company to turn over user records to the state security agency. https://t.co/l9OXCkQPJQ https://t.co/lguVPOwmUm",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				159
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/l9OXCkQPJQ",
+						"expanded_url": "https://www.zdnet.com/article/russia-says-tinder-must-share-user-data-private-messages/",
+						"display_url": "zdnet.com/article/russia…",
+						"indices": [
+							136,
+							159
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1135586455005388800,
+						"id_str": "1135586455005388800",
+						"indices": [
+							160,
+							183
+						],
+						"media_url": "http://pbs.twimg.com/media/D8Jp3T5XYAA_PZr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D8Jp3T5XYAA_PZr.jpg",
+						"url": "https://t.co/lguVPOwmUm",
+						"display_url": "pic.twitter.com/lguVPOwmUm",
+						"expanded_url": "https://twitter.com/zackwhittaker/status/1135586461535940609/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1898,
+								"h": 1266,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1135586455005388800,
+						"id_str": "1135586455005388800",
+						"indices": [
+							160,
+							183
+						],
+						"media_url": "http://pbs.twimg.com/media/D8Jp3T5XYAA_PZr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D8Jp3T5XYAA_PZr.jpg",
+						"url": "https://t.co/lguVPOwmUm",
+						"display_url": "pic.twitter.com/lguVPOwmUm",
+						"expanded_url": "https://twitter.com/zackwhittaker/status/1135586461535940609/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1898,
+								"h": 1266,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15752235,
+				"id_str": "15752235",
+				"name": "Zack Whittaker",
+				"screen_name": "zackwhittaker",
+				"location": "New York, NY",
+				"description": "Security editor, @TechCrunch. CBS alum. Email: zack.whittaker@gmail.com. WhatsApp, Signal: +1.646.755.8849. Newsletter: https://t.co/0aSHFyliQz",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/0aSHFyliQz",
+								"expanded_url": "http://eepurl.com/dBQdJ5",
+								"display_url": "eepurl.com/dBQdJ5",
+								"indices": [
+									120,
+									143
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 43098,
+				"friends_count": 992,
+				"listed_count": 1508,
+				"created_at": "Wed Aug 06 17:00:05 +0000 2008",
+				"favourites_count": 50734,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 3128,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1124108724103196673/Gjd3Gpld_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1124108724103196673/Gjd3Gpld_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15752235/1453743768",
+				"profile_link_color": "36345C",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 62,
+			"favorite_count": 38,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 11,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Uh-oh... https://t.co/cdRXUg5mqn"
+	},
+	{
 		"created_at": "Tue Jun 04 00:10:47 +0000 2019",
 		"id": 1135592474833932300,
 		"id_str": "1135700388751929345",
