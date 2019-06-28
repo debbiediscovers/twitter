6570912338ba114331be84c7a035885b7b73c51a
debbiediscovers <debbiediscovers@gmail.com>
2019-06-28 14:12:03 +0800

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 28 06:02:33 +0000 2019",
+		"id": 1144486220958847000,
+		"id_str": "1144486220958846976",
+		"full_text": "Sounds good in theory but one wrong nudge and this could go soooooooo wrong . \"Inside The Race to Build the Netflix of Food\" - @zandercutt https://t.co/K4izdQawkh https://t.co/J208aNa3R3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "zandercutt",
+					"name": "Zander",
+					"id": 607692446,
+					"id_str": "607692446",
+					"indices": [
+						127,
+						138
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/K4izdQawkh",
+					"expanded_url": "https://onezero.medium.com/inside-the-race-to-build-the-netflix-of-food-1e8e6abd007e#---0-291",
+					"display_url": "onezero.medium.com/inside-the-rac…",
+					"indices": [
+						139,
+						162
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1144486217330778100,
+					"id_str": "1144486217330778112",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/D-IIJqJU4AAz5ml.png",
+					"media_url_https": "https://pbs.twimg.com/media/D-IIJqJU4AAz5ml.png",
+					"url": "https://t.co/J208aNa3R3",
+					"display_url": "pic.twitter.com/J208aNa3R3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1144486220958846976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 781,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 653,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 781,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1144486217330778100,
+					"id_str": "1144486217330778112",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/D-IIJqJU4AAz5ml.png",
+					"media_url_https": "https://pbs.twimg.com/media/D-IIJqJU4AAz5ml.png",
+					"url": "https://t.co/J208aNa3R3",
+					"display_url": "pic.twitter.com/J208aNa3R3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1144486220958846976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 781,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 653,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 781,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://medium.com\" rel=\"nofollow\">Medium</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65426,
+			"friends_count": 30466,
+			"listed_count": 1365,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20391,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17421,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sounds good in theory but one wrong nudge and this could go soooooooo wrong . \"Inside The Race to Build the Netflix of Food\" - @zandercutt https://t.co/K4izdQawkh https://t.co/J208aNa3R3"
+	},
+	{
 		"created_at": "Fri Jun 28 04:41:17 +0000 2019",
 		"id": 1144465770300112900,
 		"id_str": "1144465770300112896",
