acef1e14d9afe57af443964668781819fbd1f0ae
debbiediscovers <debbiediscovers@gmail.com>
2019-07-04 10:12:02 +0800

    Added 1 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 01:51:15 +0000 2019",
+		"id": 1146597308496142300,
+		"id_str": "1146597308496142337",
+		"full_text": "Most of our suffering, of not all of it, is the product of us being lost in a story we’re telling ourselves” - @SamHarrisOrg \n#ThursdayThoughts https://t.co/1sKUyJTBE3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ThursdayThoughts",
+					"indices": [
+						126,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SamHarrisOrg",
+					"name": "Sam Harris",
+					"id": 116994659,
+					"id_str": "116994659",
+					"indices": [
+						111,
+						124
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146597256520323100,
+					"id_str": "1146597256520323072",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/D-mIIT9U0AAKd54.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-mIIT9U0AAKd54.jpg",
+					"url": "https://t.co/1sKUyJTBE3",
+					"display_url": "pic.twitter.com/1sKUyJTBE3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1146597308496142337/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 521,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 784,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 784,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146597256520323100,
+					"id_str": "1146597256520323072",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/D-mIIT9U0AAKd54.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-mIIT9U0AAKd54.jpg",
+					"url": "https://t.co/1sKUyJTBE3",
+					"display_url": "pic.twitter.com/1sKUyJTBE3",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1146597308496142337/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 521,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 784,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 784,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65422,
+			"friends_count": 28898,
+			"listed_count": 1365,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20416,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17430,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9d22e02884003",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9d22e02884003.json",
+			"place_type": "poi",
+			"name": "Singapore Botanic Gardens",
+			"full_name": "Singapore Botanic Gardens",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.81557234846358,
+							1.3158689657800091
+						],
+						[
+							103.81557234846358,
+							1.3158689657800091
+						],
+						[
+							103.81557234846358,
+							1.3158689657800091
+						],
+						[
+							103.81557234846358,
+							1.3158689657800091
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Most of our suffering, of not all of it, is the product of us being lost in a story we’re telling ourselves” - @SamHarrisOrg \n#ThursdayThoughts https://t.co/1sKUyJTBE3"
+	},
+	{
 		"created_at": "Thu Jul 04 00:54:36 +0000 2019",
 		"id": 1146236401807056900,
 		"id_str": "1146583050148446208",
