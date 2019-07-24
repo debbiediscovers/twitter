6573190e36888ece4f47e58b8a253aacfc28f15b
debbiediscovers <debbiediscovers@gmail.com>
2019-07-24 11:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32530,
-	"created_at": "Tue Jul 23 12:12:01 +0000 2019",
+	"tweets": 32531,
+	"created_at": "Wed Jul 24 03:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Wed Jul 24 03:03:24 +0000 2019",
+		"id": 1153636742873751600,
+		"id_str": "1153863220391636992",
+		"full_text": "Whatever tool you want to build, you can do it without code.\n\nMakerpad is the no-code platform you need to create tools for your work. View tutorials, develop skills, browse jobs and build tools.\n\nGreat tutorials on Makerpad. Check it out.\n\nhttps://t.co/mMyW8OOkrX https://t.co/SKZYNSfMIm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mMyW8OOkrX",
+					"expanded_url": "https://www.makerpad.co/secret-signup/?via=deborah",
+					"display_url": "makerpad.co/secret-signup/…",
+					"indices": [
+						241,
+						264
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1153636726692204500,
+					"id_str": "1153636726692204545",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/EAKKfZ5VUAEnumS.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAKKfZ5VUAEnumS.png",
+					"url": "https://t.co/SKZYNSfMIm",
+					"display_url": "pic.twitter.com/SKZYNSfMIm",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1153636742873751552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 534,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 845,
+							"h": 664,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 845,
+							"h": 664,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1153636726692204500,
+					"id_str": "1153636726692204545",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/EAKKfZ5VUAEnumS.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAKKfZ5VUAEnumS.png",
+					"url": "https://t.co/SKZYNSfMIm",
+					"display_url": "pic.twitter.com/SKZYNSfMIm",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1153636742873751552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 534,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 845,
+							"h": 664,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 845,
+							"h": 664,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65224,
+			"friends_count": 25203,
+			"listed_count": 1367,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17449,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Whatever tool you want to build, you can do it without code.\n\nMakerpad is the no-code platform you need to create tools for your work. View tutorials, develop skills, browse jobs and build tools.\n\nGreat tutorials on Makerpad. Check it out.\n\nhttps://t.co/mMyW8OOkrX https://t.co/SKZYNSfMIm"
+	},
+	{
 		"created_at": "Tue Jul 23 12:08:52 +0000 2019",
 		"id": 1153638106219700200,
 		"id_str": "1153638106219700224",
