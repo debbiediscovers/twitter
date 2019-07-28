5cf6a31b51a0c0a2c46f7d9fc1f5149eab7223f0
debbiediscovers <debbiediscovers@gmail.com>
2019-07-28 12:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32541,
-	"created_at": "Sat Jul 27 22:12:02 +0000 2019",
+	"tweets": 32542,
+	"created_at": "Sun Jul 28 04:12:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 28 03:38:46 +0000 2019",
+		"id": 1155236809405882400,
+		"id_str": "1155321673559576577",
+		"full_text": "A new tool uses #AI to spot text written by #AI https://t.co/Tc3VzMgmxJ https://t.co/ssaGZgWgg7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AI",
+					"indices": [
+						16,
+						19
+					]
+				},
+				{
+					"text": "AI",
+					"indices": [
+						44,
+						47
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Tc3VzMgmxJ",
+					"expanded_url": "https://buff.ly/2Mm1Cnw",
+					"display_url": "buff.ly/2Mm1Cnw",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1155236807946264600,
+					"id_str": "1155236807946264577",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/EAg5wZEX4AEwaeF.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAg5wZEX4AEwaeF.png",
+					"url": "https://t.co/ssaGZgWgg7",
+					"display_url": "pic.twitter.com/ssaGZgWgg7",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1155236809405882370/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 775,
+							"h": 375,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 375,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 329,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1155236807946264600,
+					"id_str": "1155236807946264577",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/EAg5wZEX4AEwaeF.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAg5wZEX4AEwaeF.png",
+					"url": "https://t.co/ssaGZgWgg7",
+					"display_url": "pic.twitter.com/ssaGZgWgg7",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1155236809405882370/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 775,
+							"h": 375,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 375,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 329,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65247,
+			"friends_count": 25499,
+			"listed_count": 1367,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20510,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17455,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A new tool uses #AI to spot text written by #AI https://t.co/Tc3VzMgmxJ https://t.co/ssaGZgWgg7"
+	},
+	{
 		"created_at": "Sat Jul 27 22:10:04 +0000 2019",
 		"id": 1155238951659036700,
 		"id_str": "1155238951659036672",
