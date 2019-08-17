4e511b21ad9dd28283ea18431cff404a879e8877
debbiediscovers <debbiediscovers@gmail.com>
2019-08-17 14:12:02 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32570,
-	"created_at": "Wed Aug 14 06:12:02 +0000 2019",
+	"tweets": 32572,
+	"created_at": "Sat Aug 17 06:12:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 17 06:03:35 +0000 2019",
+		"id": 1162415959442772000,
+		"id_str": "1162605875053260800",
+		"full_text": "Paul Writer interviewed @debbiediscovers for our Marketing Maverick series on #APACLeaders. Deborah is the founder of Digital Discovery. She has also been the co-developer of Sammy, the world’s first artificially intelligent Chatbot for sailing. \n\nhttps://t.co/roMjId9Isb\n\n#Sammy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "APACLeaders",
+					"indices": [
+						78,
+						90
+					]
+				},
+				{
+					"text": "Sammy",
+					"indices": [
+						273,
+						279
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						24,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/roMjId9Isb",
+					"expanded_url": "https://paulwriter.com/marketing-maverick-deborah-kay-singapore-sailing-federation-apacleaders/",
+					"display_url": "paulwriter.com/marketing-mave…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 132961301,
+			"id_str": "132961301",
+			"name": "Paul Writer",
+			"screen_name": "paulwriter",
+			"location": "Bangalore, India",
+			"description": "India's only marketing hub. 30,000+ members. Follow for marketing strategy, conference updates, brand news and interviews. Founded by @jessie_paul in 2010",
+			"url": "http://t.co/fHCgBqQjkS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/fHCgBqQjkS",
+							"expanded_url": "http://paulwriter.com",
+							"display_url": "paulwriter.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4674,
+			"friends_count": 4154,
+			"listed_count": 339,
+			"created_at": "Wed Apr 14 16:53:52 +0000 2010",
+			"favourites_count": 1294,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7528,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/433105520254148608/q0eovZVI_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/433105520254148608/q0eovZVI_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/132961301/1551090414",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Paul Writer interviewed @debbiediscovers for our Marketing Maverick series on #APACLeaders. Deborah is the founder of Digital Discovery. She has also been the co-developer of Sammy, the world’s first artificially intelligent Chatbot for sailing. \n\nhttps://t.co/roMjId9Isb\n\n#Sammy"
+	},
+	{
+		"created_at": "Sat Aug 17 06:03:19 +0000 2019",
+		"id": 1162605806505746400,
+		"id_str": "1162605806505746432",
+		"full_text": "@AngelDaddy4 Awesome! 😊",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AngelDaddy4",
+					"name": "Dan and Angel Clarke",
+					"id": 1084195310145060900,
+					"id_str": "1084195310145060865",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1162600657641267200,
+		"in_reply_to_status_id_str": "1162600657641267200",
+		"in_reply_to_user_id": 1084195310145060900,
+		"in_reply_to_user_id_str": "1084195310145060865",
+		"in_reply_to_screen_name": "AngelDaddy4",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65414,
+			"friends_count": 25973,
+			"listed_count": 1373,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20839,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17473,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@AngelDaddy4 Awesome! 😊"
+	},
+	{
 		"created_at": "Wed Aug 14 05:57:47 +0000 2019",
 		"id": 1161517253369770000,
 		"id_str": "1161517253369769987",
