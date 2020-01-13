87bf600ad5dd7704a2c3b0c1fb8a90d9d479ec78
debbiediscovers <debbiediscovers@gmail.com>
2020-01-13 11:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32740,
-	"created_at": "Wed Jan 08 13:12:01 +0000 2020",
+	"tweets": 32741,
+	"created_at": "Mon Jan 13 03:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,150 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 13 02:19:52 +0000 2020",
+		"id": 1216545368445317000,
+		"id_str": "1216545368445317120",
+		"full_text": "GOOD READ\nThe Next Era of Web Marketing: 2019 &amp; Beyond #influencermarketing #internetmarketing https://t.co/w47yZVdjNO via @randfish @sparktoro",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "influencermarketing",
+					"indices": [
+						59,
+						79
+					]
+				},
+				{
+					"text": "internetmarketing",
+					"indices": [
+						80,
+						98
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "randfish",
+					"name": "Rand Fishkin",
+					"id": 6527972,
+					"id_str": "6527972",
+					"indices": [
+						127,
+						136
+					]
+				},
+				{
+					"screen_name": "sparktoro",
+					"name": "Sparktoro",
+					"id": 917075660249563100,
+					"id_str": "917075660249563136",
+					"indices": [
+						137,
+						147
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/w47yZVdjNO",
+					"expanded_url": "https://www.slideshare.net/randfish/the-next-era-of-web-marketing-2019-beyond",
+					"display_url": "slideshare.net/randfish/the-n…",
+					"indices": [
+						99,
+						122
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67371,
+			"friends_count": 29428,
+			"listed_count": 1389,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20838,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17549,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "GOOD READ\nThe Next Era of Web Marketing: 2019 &amp; Beyond #influencermarketing #internetmarketing https://t.co/w47yZVdjNO via @randfish @sparktoro"
+	},
+	{
 		"created_at": "Wed Jan 08 12:41:21 +0000 2020",
 		"id": 1213891691314172000,
 		"id_str": "1214889829734871041",
