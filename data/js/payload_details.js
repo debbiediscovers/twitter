31a0bdcd7e94051ea2669c45b21eadbc5eb0ad2e
debbiediscovers <debbiediscovers@gmail.com>
2020-02-03 11:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32753,
-	"created_at": "Sun Feb 02 22:12:01 +0000 2020",
+	"tweets": 32754,
+	"created_at": "Mon Feb 03 03:12:00 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,680 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 03 03:07:00 +0000 2020",
+		"id": 1224060774273806300,
+		"id_str": "1224167375693500416",
+		"full_text": "Happy #palindrome day! \n\n02.02.2020 https://t.co/gW3Medu6NC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "palindrome",
+					"indices": [
+						6,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gW3Medu6NC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1171415920214007809",
+					"display_url": "twitter.com/debbiediscover…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67487,
+			"friends_count": 31959,
+			"listed_count": 1382,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20836,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17553,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "58a4c3a0d54e1400",
+			"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+			"place_type": "admin",
+			"name": "Central Region",
+			"full_name": "Central Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.749959507073,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.36874499902569
+						],
+						[
+							103.749959507073,
+							1.36874499902569
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1171415920214007800,
+		"quoted_status_id_str": "1171415920214007809",
+		"quoted_status_permalink": {
+			"url": "https://t.co/gW3Medu6NC",
+			"expanded": "https://twitter.com/debbiediscovers/status/1171415920214007809",
+			"display": "twitter.com/debbiediscover…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 10 13:31:33 +0000 2019",
+			"id": 1171415920214007800,
+			"id_str": "1171415920214007809",
+			"full_text": "HAPPY #PALINDROME WEEK! The next 10 days are palindromes, meaning the date reads the same backward as forward.\n\nSource : @Mensa https://t.co/vRDcYi17c7",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				127
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "PALINDROME",
+						"indices": [
+							6,
+							17
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Mensa",
+						"name": "R. L.",
+						"id": 14323801,
+						"id_str": "14323801",
+						"indices": [
+							121,
+							127
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1171415913373065200,
+						"id_str": "1171415913373065217",
+						"indices": [
+							128,
+							151
+						],
+						"media_url": "http://pbs.twimg.com/media/EEG0kRjXYAE5Oum.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EEG0kRjXYAE5Oum.jpg",
+						"url": "https://t.co/vRDcYi17c7",
+						"display_url": "pic.twitter.com/vRDcYi17c7",
+						"expanded_url": "https://twitter.com/debbiediscovers/status/1171415920214007809/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 323,
+										"y": 595,
+										"h": 44,
+										"w": 44
+									},
+									{
+										"x": 219,
+										"y": 589,
+										"h": 52,
+										"w": 52
+									},
+									{
+										"x": 217,
+										"y": 336,
+										"h": 54,
+										"w": 54
+									},
+									{
+										"x": 358,
+										"y": 459,
+										"h": 67,
+										"w": 67
+									},
+									{
+										"x": 217,
+										"y": 524,
+										"h": 59,
+										"w": 59
+									},
+									{
+										"x": 356,
+										"y": 81,
+										"h": 69,
+										"w": 69
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1171415913373065200,
+						"id_str": "1171415913373065217",
+						"indices": [
+							128,
+							151
+						],
+						"media_url": "http://pbs.twimg.com/media/EEG0kRjXYAE5Oum.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EEG0kRjXYAE5Oum.jpg",
+						"url": "https://t.co/vRDcYi17c7",
+						"display_url": "pic.twitter.com/vRDcYi17c7",
+						"expanded_url": "https://twitter.com/debbiediscovers/status/1171415920214007809/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 514,
+										"y": 945,
+										"h": 71,
+										"w": 71
+									},
+									{
+										"x": 348,
+										"y": 936,
+										"h": 84,
+										"w": 84
+									},
+									{
+										"x": 346,
+										"y": 534,
+										"h": 86,
+										"w": 86
+									},
+									{
+										"x": 570,
+										"y": 730,
+										"h": 107,
+										"w": 107
+									},
+									{
+										"x": 345,
+										"y": 833,
+										"h": 95,
+										"w": 95
+									},
+									{
+										"x": 566,
+										"y": 130,
+										"h": 110,
+										"w": 110
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 323,
+										"y": 595,
+										"h": 44,
+										"w": 44
+									},
+									{
+										"x": 219,
+										"y": 589,
+										"h": 52,
+										"w": 52
+									},
+									{
+										"x": 217,
+										"y": 336,
+										"h": 54,
+										"w": 54
+									},
+									{
+										"x": 358,
+										"y": 459,
+										"h": 67,
+										"w": 67
+									},
+									{
+										"x": 217,
+										"y": 524,
+										"h": 59,
+										"w": 59
+									},
+									{
+										"x": 356,
+										"y": 81,
+										"h": 69,
+										"w": 69
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3060836348,
+				"id_str": "3060836348",
+				"name": "Deborah Kay",
+				"screen_name": "debbiediscovers",
+				"location": "Singapore, London, New York",
+				"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+				"url": "https://t.co/wREC5x0iTO",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wREC5x0iTO",
+								"expanded_url": "https://debbiediscovers.me",
+								"display_url": "debbiediscovers.me",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 67487,
+				"friends_count": 31959,
+				"listed_count": 1382,
+				"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+				"favourites_count": 20836,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17553,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+				"profile_link_color": "D50252",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": false,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 61,
+			"favorite_count": 118,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Happy #palindrome day! \n\n02.02.2020 https://t.co/gW3Medu6NC"
+	},
+	{
 		"created_at": "Sun Feb 02 21:51:21 +0000 2020",
 		"id": 1224087937391837200,
 		"id_str": "1224087937391837185",
