1d625046eb9015b7bb66b446396bf1afb1fe9bbe
debbiediscovers <debbiediscovers@gmail.com>
2020-02-09 10:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32768,
-	"created_at": "Sat Feb 08 12:12:01 +0000 2020",
+	"tweets": 32769,
+	"created_at": "Sun Feb 09 02:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,276 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 09 01:26:06 +0000 2020",
+		"id": 1226143328288116700,
+		"id_str": "1226316306661511168",
+		"full_text": "Your welcome @debbiediscovers.\n\nYour #awesome blogs are worth sharing 1000 times. https://t.co/yq0s02pvBq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "awesome",
+					"indices": [
+						37,
+						45
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "debbiediscovers",
+					"name": "Deborah Kay",
+					"id": 3060836348,
+					"id_str": "3060836348",
+					"indices": [
+						13,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yq0s02pvBq",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1226107161727524864",
+					"display_url": "twitter.com/debbiediscover…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1203363413889450000,
+			"id_str": "1203363413889449984",
+			"name": "Botosynthesis",
+			"screen_name": "botosynthesis",
+			"location": "Sector 4, Hiran Magri, Udaipur",
+			"description": "At Botosynthesis, we aim to create automated solutions which can make peoples lives simpler.",
+			"url": "https://t.co/uw3K9VT8Cg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uw3K9VT8Cg",
+							"expanded_url": "http://www.botosynthesis.com",
+							"display_url": "botosynthesis.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57,
+			"friends_count": 152,
+			"listed_count": 1,
+			"created_at": "Sat Dec 07 17:19:50 +0000 2019",
+			"favourites_count": 430,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1212680524600242176/-88PCrRa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1212680524600242176/-88PCrRa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1203363413889449984/1575739816",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1226107161727524900,
+		"quoted_status_id_str": "1226107161727524864",
+		"quoted_status_permalink": {
+			"url": "https://t.co/yq0s02pvBq",
+			"expanded": "https://twitter.com/debbiediscovers/status/1226107161727524864",
+			"display": "twitter.com/debbiediscover…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 08 11:35:01 +0000 2020",
+			"id": 1226107161727524900,
+			"id_str": "1226107161727524864",
+			"full_text": "@botosynthesis Thanks for sharing!! 🙌🙌",
+			"truncated": false,
+			"display_text_range": [
+				15,
+				38
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "botosynthesis",
+						"name": "Botosynthesis",
+						"id": 1203363413889450000,
+						"id_str": "1203363413889449984",
+						"indices": [
+							0,
+							14
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1225372207393591300,
+			"in_reply_to_status_id_str": "1225372207393591299",
+			"in_reply_to_user_id": 1203363413889450000,
+			"in_reply_to_user_id_str": "1203363413889449984",
+			"in_reply_to_screen_name": "botosynthesis",
+			"user": {
+				"id": 3060836348,
+				"id_str": "3060836348",
+				"name": "Deborah Kay",
+				"screen_name": "debbiediscovers",
+				"location": "Singapore, London, New York",
+				"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+				"url": "https://t.co/wREC5x0iTO",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wREC5x0iTO",
+								"expanded_url": "https://debbiediscovers.me",
+								"display_url": "debbiediscovers.me",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 67690,
+				"friends_count": 31445,
+				"listed_count": 1383,
+				"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+				"favourites_count": 20844,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17562,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+				"profile_link_color": "D50252",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "58a4c3a0d54e1400",
+				"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+				"place_type": "admin",
+				"name": "Central Region",
+				"full_name": "Central Region, Singapore",
+				"country_code": "SG",
+				"country": "Singapore",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								103.749959507073,
+								1.2123138339349
+							],
+							[
+								103.918426999964,
+								1.2123138339349
+							],
+							[
+								103.918426999964,
+								1.36874499902569
+							],
+							[
+								103.749959507073,
+								1.36874499902569
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Your welcome @debbiediscovers.\n\nYour #awesome blogs are worth sharing 1000 times. https://t.co/yq0s02pvBq"
+	},
+	{
 		"created_at": "Sat Feb 08 11:35:30 +0000 2020",
 		"id": 1224462831778594800,
 		"id_str": "1226107281747537921",
