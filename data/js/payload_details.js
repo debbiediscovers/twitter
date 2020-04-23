5d79f2e9441107653e319c4a785c52ae65838398
debbiediscovers <debbiediscovers@gmail.com>
2020-04-23 22:12:04 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32833,
-	"created_at": "Thu Apr 23 11:12:01 +0000 2020",
+	"tweets": 32834,
+	"created_at": "Thu Apr 23 14:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 23 13:21:36 +0000 2020",
+		"id": 1253159294859501600,
+		"id_str": "1253313070400561152",
+		"full_text": "How pretty is my cycling route?? ☺️ https://t.co/yNElqG3XsC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1253159258868207600,
+					"id_str": "1253159258868207616",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/EWQdtNfU0AAdVeH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWQdtNfU0AAdVeH.jpg",
+					"url": "https://t.co/yNElqG3XsC",
+					"display_url": "pic.twitter.com/yNElqG3XsC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1253159294859501568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1253159258868207600,
+					"id_str": "1253159258868207616",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/EWQdtNfU0AAdVeH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWQdtNfU0AAdVeH.jpg",
+					"url": "https://t.co/yNElqG3XsC",
+					"display_url": "pic.twitter.com/yNElqG3XsC",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1253159294859501568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67552,
+			"friends_count": 35270,
+			"listed_count": 1366,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20865,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17598,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9cc6e56c80000",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9cc6e56c80000.json",
+			"place_type": "poi",
+			"name": "Bay East (Gardens by the Bay)",
+			"full_name": "Bay East (Gardens by the Bay)",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.86895179748535,
+							1.2895830304297036
+						],
+						[
+							103.86895179748535,
+							1.2895830304297036
+						],
+						[
+							103.86895179748535,
+							1.2895830304297036
+						],
+						[
+							103.86895179748535,
+							1.2895830304297036
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 106,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How pretty is my cycling route?? ☺️ https://t.co/yNElqG3XsC"
+	},
+	{
 		"created_at": "Thu Apr 23 11:09:09 +0000 2020",
 		"id": 1253159294859501600,
 		"id_str": "1253279741794123777",
