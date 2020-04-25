e983d6de02fd2a1cb51f3d4332ad5c578c6e8ec8
debbiediscovers <debbiediscovers@gmail.com>
2020-04-25 16:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32839,
-	"created_at": "Fri Apr 24 10:12:02 +0000 2020",
+	"tweets": 32840,
+	"created_at": "Sat Apr 25 08:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 25 07:21:24 +0000 2020",
+		"id": 1253929242347348000,
+		"id_str": "1253947201375440896",
+		"full_text": "Hubby always here\nNow he knows I drink daily\nLockdown to end please\n.⁠\n.⁠\n.⁠\n⁠\n🥺 Write one, tag us #quarantinehaikus⁠\n⁠\nVisit https://t.co/qu65EWvGlV to 💗 your faves. https://t.co/O9PrQueEUx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "quarantinehaikus",
+					"indices": [
+						99,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qu65EWvGlV",
+					"expanded_url": "https://quarantinehaikus.com",
+					"display_url": "quarantinehaikus.com",
+					"indices": [
+						126,
+						149
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1253929238094327800,
+					"id_str": "1253929238094327809",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EWbZ_5oWsAEpn7R.png",
+					"media_url_https": "https://pbs.twimg.com/media/EWbZ_5oWsAEpn7R.png",
+					"url": "https://t.co/O9PrQueEUx",
+					"display_url": "pic.twitter.com/O9PrQueEUx",
+					"expanded_url": "https://twitter.com/q_haikus/status/1253929242347347969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1253929238094327800,
+					"id_str": "1253929238094327809",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EWbZ_5oWsAEpn7R.png",
+					"media_url_https": "https://pbs.twimg.com/media/EWbZ_5oWsAEpn7R.png",
+					"url": "https://t.co/O9PrQueEUx",
+					"display_url": "pic.twitter.com/O9PrQueEUx",
+					"expanded_url": "https://twitter.com/q_haikus/status/1253929242347347969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1253612859499483100,
+			"id_str": "1253612859499483137",
+			"name": "Quarantine Haikus",
+			"screen_name": "q_haikus",
+			"location": "Singapore, but Global",
+			"description": "To capture the zeitgeist of these times, where millions of people all around the world are under lockdown in their homes. Write one, tag us #quarantinehaikus",
+			"url": "https://t.co/gSxTfEr9EY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gSxTfEr9EY",
+							"expanded_url": "https://quarantinehaikus.com",
+							"display_url": "quarantinehaikus.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3,
+			"friends_count": 0,
+			"listed_count": 0,
+			"created_at": "Fri Apr 24 09:13:23 +0000 2020",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1253618420156751872/56d7sluz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1253618420156751872/56d7sluz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1253612859499483137/1587721014",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hubby always here\nNow he knows I drink daily\nLockdown to end please\n.⁠\n.⁠\n.⁠\n⁠\n🥺 Write one, tag us #quarantinehaikus⁠\n⁠\nVisit https://t.co/qu65EWvGlV to 💗 your faves. https://t.co/O9PrQueEUx"
+	},
+	{
 		"created_at": "Fri Apr 24 10:11:34 +0000 2020",
 		"id": 1253627636506484700,
 		"id_str": "1253627636506484737",
