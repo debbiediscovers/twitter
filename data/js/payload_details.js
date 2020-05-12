5913402ad60f7ab1617aaa851767c67e63cbb411
debbiediscovers <debbiediscovers@gmail.com>
2020-05-12 20:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32857,
-	"created_at": "Mon May 11 14:12:01 +0000 2020",
+	"tweets": 32858,
+	"created_at": "Tue May 12 12:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Tue May 12 11:38:52 +0000 2020",
+		"id": 1260172589239988200,
+		"id_str": "1260172589239988225",
+		"full_text": "Wow @uenli_ Uen-Li Chia so talented! I’ve always wanted bigger eyes, long eyelashes and big fat lips!! \nThanks for making my dreams come true! 🤩 https://t.co/AgAeCYQN52",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "uenli_",
+					"name": "uenlinotes",
+					"id": 1159220129479057400,
+					"id_str": "1159220129479057408",
+					"indices": [
+						4,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1260172583703478300,
+					"id_str": "1260172583703478272",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EX0IScdVAAA0uzC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX0IScdVAAA0uzC.jpg",
+					"url": "https://t.co/AgAeCYQN52",
+					"display_url": "pic.twitter.com/AgAeCYQN52",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1260172589239988225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 575,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1083,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1015,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1260172583703478300,
+					"id_str": "1260172583703478272",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EX0IScdVAAA0uzC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX0IScdVAAA0uzC.jpg",
+					"url": "https://t.co/AgAeCYQN52",
+					"display_url": "pic.twitter.com/AgAeCYQN52",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1260172589239988225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 575,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1083,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1015,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67390,
+			"friends_count": 35159,
+			"listed_count": 1333,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20915,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17617,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027068089618198536/wIl1K2Fm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wow @uenli_ Uen-Li Chia so talented! I’ve always wanted bigger eyes, long eyelashes and big fat lips!! \nThanks for making my dreams come true! 🤩 https://t.co/AgAeCYQN52"
+	},
+	{
 		"created_at": "Mon May 11 13:17:03 +0000 2020",
 		"id": 1257532233398673400,
 		"id_str": "1259834906831548417",
