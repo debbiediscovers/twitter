f8de829da28c015aebff0c0945bab8c49e086981
debbiediscovers <debbiediscovers@gmail.com>
2020-05-23 09:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32876,
-	"created_at": "Fri May 22 23:12:01 +0000 2020",
+	"tweets": 32877,
+	"created_at": "Sat May 23 01:12:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 23 00:24:17 +0000 2020",
+		"id": 1263989088429830100,
+		"id_str": "1263989088429830149",
+		"full_text": "@rigg_alyssa Negronis!",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rigg_alyssa",
+					"name": "Alyssa Rigg",
+					"id": 788000656980410400,
+					"id_str": "788000656980410370",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1263981089040842800,
+		"in_reply_to_status_id_str": "1263981089040842752",
+		"in_reply_to_user_id": 788000656980410400,
+		"in_reply_to_user_id_str": "788000656980410370",
+		"in_reply_to_screen_name": "rigg_alyssa",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67412,
+			"friends_count": 35033,
+			"listed_count": 1327,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20914,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17629,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1260353563605086208/lav5B7UT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1260353563605086208/lav5B7UT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1517291994",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "lt",
+		"text": "@rigg_alyssa Negronis!"
+	},
+	{
 		"created_at": "Fri May 22 22:38:33 +0000 2020",
 		"id": 1262901088673386500,
 		"id_str": "1263962483091361793",
