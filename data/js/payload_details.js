1019384dae4a3ebc986cd09f613efb271a4328cc
debbiediscovers <debbiediscovers@gmail.com>
2020-10-13 10:12:05 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32938,
-	"created_at": "Mon Oct 12 11:12:01 +0000 2020",
+	"tweets": 32939,
+	"created_at": "Tue Oct 13 02:12:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 13 01:38:58 +0000 2020",
+		"id": 1315829350999105500,
+		"id_str": "1315829350999105536",
+		"full_text": "Subscribed to Platformer from Singapore 🇸🇬 🥳! Best $100 spent cuz I can't bear the thought of missing a single issue of this newsletter from @CaseyNewton \n\nhttps://t.co/k78G1vhHSR https://t.co/vzBCHkjB85",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CaseyNewton",
+					"name": "Casey Newton",
+					"id": 69426451,
+					"id_str": "69426451",
+					"indices": [
+						141,
+						153
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/k78G1vhHSR",
+					"expanded_url": "https://www.platformer.news/about",
+					"display_url": "platformer.news/about",
+					"indices": [
+						156,
+						179
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1315828198056194000,
+					"id_str": "1315828198056194055",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EkLCxkqU8AcheNf.png",
+					"media_url_https": "https://pbs.twimg.com/media/EkLCxkqU8AcheNf.png",
+					"url": "https://t.co/vzBCHkjB85",
+					"display_url": "pic.twitter.com/vzBCHkjB85",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1315829350999105536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 762,
+							"h": 301,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 269,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 762,
+							"h": 301,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315828198056194000,
+					"id_str": "1315828198056194055",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EkLCxkqU8AcheNf.png",
+					"media_url_https": "https://pbs.twimg.com/media/EkLCxkqU8AcheNf.png",
+					"url": "https://t.co/vzBCHkjB85",
+					"display_url": "pic.twitter.com/vzBCHkjB85",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1315829350999105536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 762,
+							"h": 301,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 269,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 762,
+							"h": 301,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 66215,
+			"friends_count": 38375,
+			"listed_count": 1334,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20854,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17646,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267444428483948547/m78-Zyes_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267444428483948547/m78-Zyes_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Subscribed to Platformer from Singapore 🇸🇬 🥳! Best $100 spent cuz I can't bear the thought of missing a single issue of this newsletter from @CaseyNewton \n\nhttps://t.co/k78G1vhHSR https://t.co/vzBCHkjB85"
+	},
+	{
 		"created_at": "Mon Oct 12 11:04:22 +0000 2020",
 		"id": 1315609248911380500,
 		"id_str": "1315609248911380481",
