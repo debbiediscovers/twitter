d27b9698030ac2e67706e22c80e30b5d61eac9dc
debbiediscovers <debbiediscovers@gmail.com>
2020-11-25 07:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32954,
-	"created_at": "Wed Nov 18 04:12:01 +0000 2020",
+	"tweets": 32955,
+	"created_at": "Tue Nov 24 23:12:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,364 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Tue Nov 24 22:24:32 +0000 2020",
+		"id": 1331363095759921200,
+		"id_str": "1331363095759921152",
+		"full_text": "@RachelleMHenry @Stanzie_Actress @cckingofficial @LoriMoreno @JulieKaylin @LanceLEdwards @DaveyLissa @TaraAzarian @djg_media @NDC_Martins @jfrankpitt15 @Misty_Lee @RichardLeoHunt @byH_Leitenbauer @ScottTakeda @TomHall @hitRECordJoe GOOD MORNING! https://t.co/4JbOiYbv6w",
+		"truncated": false,
+		"display_text_range": [
+			232,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RachelleMHenry",
+					"name": "Rachelle Henry",
+					"id": 868646066,
+					"id_str": "868646066",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "Stanzie_Actress",
+					"name": "Konstanza Firth-Radford",
+					"id": 1006849914825998300,
+					"id_str": "1006849914825998336",
+					"indices": [
+						16,
+						32
+					]
+				},
+				{
+					"screen_name": "cckingofficial",
+					"name": "CC King",
+					"id": 830892686471106600,
+					"id_str": "830892686471106560",
+					"indices": [
+						33,
+						48
+					]
+				},
+				{
+					"screen_name": "LoriMoreno",
+					"name": "Lori Moreno LoveThatYacht.com",
+					"id": 15039436,
+					"id_str": "15039436",
+					"indices": [
+						49,
+						60
+					]
+				},
+				{
+					"screen_name": "JulieKaylin",
+					"name": "Julie Kaylin",
+					"id": 21932901,
+					"id_str": "21932901",
+					"indices": [
+						61,
+						73
+					]
+				},
+				{
+					"screen_name": "LanceLEdwards",
+					"name": "Lance L Edwards",
+					"id": 232680359,
+					"id_str": "232680359",
+					"indices": [
+						74,
+						88
+					]
+				},
+				{
+					"screen_name": "DaveyLissa",
+					"name": "Melissa Davey",
+					"id": 65112248,
+					"id_str": "65112248",
+					"indices": [
+						89,
+						100
+					]
+				},
+				{
+					"screen_name": "TaraAzarian",
+					"name": "Tara Nicole Azarian",
+					"id": 404733940,
+					"id_str": "404733940",
+					"indices": [
+						101,
+						113
+					]
+				},
+				{
+					"screen_name": "djg_media",
+					"name": "David Grover",
+					"id": 347085558,
+					"id_str": "347085558",
+					"indices": [
+						114,
+						124
+					]
+				},
+				{
+					"screen_name": "NDC_Martins",
+					"name": "Natalie Martins",
+					"id": 431697213,
+					"id_str": "431697213",
+					"indices": [
+						125,
+						137
+					]
+				},
+				{
+					"screen_name": "jfrankpitt15",
+					"name": "Joshua Frankpitt",
+					"id": 292431499,
+					"id_str": "292431499",
+					"indices": [
+						138,
+						151
+					]
+				},
+				{
+					"screen_name": "Misty_Lee",
+					"name": "Misty Lee @🏠",
+					"id": 18009191,
+					"id_str": "18009191",
+					"indices": [
+						152,
+						162
+					]
+				},
+				{
+					"screen_name": "RichardLeoHunt",
+					"name": "Richard Leo Hunt",
+					"id": 1027001701,
+					"id_str": "1027001701",
+					"indices": [
+						163,
+						178
+					]
+				},
+				{
+					"screen_name": "byH_Leitenbauer",
+					"name": "HUBERT LEITENBAUER",
+					"id": 2955404464,
+					"id_str": "2955404464",
+					"indices": [
+						179,
+						195
+					]
+				},
+				{
+					"screen_name": "ScottTakeda",
+					"name": "Scott Takeda",
+					"id": 87009335,
+					"id_str": "87009335",
+					"indices": [
+						196,
+						208
+					]
+				},
+				{
+					"screen_name": "TomHall",
+					"name": "Tom Hall ☘",
+					"id": 14993272,
+					"id_str": "14993272",
+					"indices": [
+						209,
+						217
+					]
+				},
+				{
+					"screen_name": "hitRECordJoe",
+					"name": "Joseph Gordon-Levitt",
+					"id": 24807616,
+					"id_str": "24807616",
+					"indices": [
+						218,
+						231
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1331363087811690500,
+					"id_str": "1331363087811690498",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EnnzrKwVQAIm4pX.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EnnzrKwVQAIm4pX.jpg",
+					"url": "https://t.co/4JbOiYbv6w",
+					"display_url": "pic.twitter.com/4JbOiYbv6w",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1331363095759921152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1331363087811690500,
+					"id_str": "1331363087811690498",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EnnzrKwVQAIm4pX.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EnnzrKwVQAIm4pX.jpg",
+					"url": "https://t.co/4JbOiYbv6w",
+					"display_url": "pic.twitter.com/4JbOiYbv6w",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1331363095759921152/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 370,
+							"h": 300,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							37,
+							30
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EnnzrKwVQAIm4pX.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1331308404749467600,
+		"in_reply_to_status_id_str": "1331308404749467649",
+		"in_reply_to_user_id": 868646066,
+		"in_reply_to_user_id_str": "868646066",
+		"in_reply_to_screen_name": "RachelleMHenry",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65737,
+			"friends_count": 38143,
+			"listed_count": 1333,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20844,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17653,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1318873337158717441/Vzw4RDqx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1318873337158717441/Vzw4RDqx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@RachelleMHenry @Stanzie_Actress @cckingofficial @LoriMoreno @JulieKaylin @LanceLEdwards @DaveyLissa @TaraAzarian @djg_media @NDC_Martins @jfrankpitt15 @Misty_Lee @RichardLeoHunt @byH_Leitenbauer @ScottTakeda @TomHall @hitRECordJoe GOOD MORNING! https://t.co/4JbOiYbv6w"
+	},
+	{
 		"created_at": "Wed Nov 18 03:21:18 +0000 2020",
 		"id": 1328669754979061800,
 		"id_str": "1328901067405332481",
