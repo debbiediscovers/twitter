cbfa01372115f99729f6fffc0f553eeb10708e2e
debbiediscovers <debbiediscovers@gmail.com>
2021-05-01 20:12:03 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32992,
-	"created_at": "Fri Apr 30 00:12:03 +0000 2021",
+	"tweets": 32993,
+	"created_at": "Sat May 01 12:12:01 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_05.js
@@ -0,0 +1,125 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Sat May 01 12:03:53 +0000 2021",
+		"id": 1388464189442695200,
+		"id_str": "1388464189442695173",
+		"full_text": "10 key #AI / #ML research papers from 2020 summarized in an easy-to-read format. These papers will give you a broad overview of research advances in NLP, computer vision, reinforcement learning, and more.\n\nhttps://t.co/wRfIjv0ho9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AI",
+					"indices": [
+						7,
+						10
+					]
+				},
+				{
+					"text": "ML",
+					"indices": [
+						13,
+						16
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wRfIjv0ho9",
+					"expanded_url": "http://www.topbots.com/ai-machine-learning-research-papers-2020/",
+					"display_url": "topbots.com/ai-machine-lea…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder | 2018 @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/wREC5x0iTO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wREC5x0iTO",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63698,
+			"friends_count": 37234,
+			"listed_count": 1308,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20724,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17663,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1387244252267114497/MFT1cQPN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387244252267114497/MFT1cQPN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "10 key #AI / #ML research papers from 2020 summarized in an easy-to-read format. These papers will give you a broad overview of research advances in NLP, computer vision, reinforcement learning, and more.\n\nhttps://t.co/wRfIjv0ho9"
+	}
+]
