45805525c9065f8f3998635708a9a908533b5a0c
debbiediscovers <debbiediscovers@gmail.com>
2021-06-04 21:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33009,
-	"created_at": "Fri Jun 04 12:12:00 +0000 2021",
+	"tweets": 33010,
+	"created_at": "Fri Jun 04 13:12:01 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 04 12:57:49 +0000 2021",
+		"id": 1400646978233532400,
+		"id_str": "1400798946759380996",
+		"full_text": "Funded by the Beijing govt, WuDao 2.0 model is a pre-trained AI model that claims to surpass Google and OpenAI with new language processing model. This model can simulate conversational speech, write poems, understand pictures and even generate recipes. \n\nhttps://t.co/RpD0mv2Xcf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RpD0mv2Xcf",
+					"expanded_url": "https://www.scmp.com/tech/tech-war/article/3135764/us-china-tech-war-beijing-funded-ai-researchers-surpass-google-and?utm_source=Twitter&utm_medium=share_widget&utm_campaign=3135764",
+					"display_url": "scmp.com/tech/tech-war/…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder |  @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/WW6rfePvMp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WW6rfePvMp",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63373,
+			"friends_count": 37170,
+			"listed_count": 1304,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20722,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17670,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1387244252267114497/MFT1cQPN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387244252267114497/MFT1cQPN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Funded by the Beijing govt, WuDao 2.0 model is a pre-trained AI model that claims to surpass Google and OpenAI with new language processing model. This model can simulate conversational speech, write poems, understand pictures and even generate recipes. \n\nhttps://t.co/RpD0mv2Xcf"
+	},
+	{
 		"created_at": "Fri Jun 04 11:47:16 +0000 2021",
 		"id": 1400646978233532400,
 		"id_str": "1400781191960162305",
