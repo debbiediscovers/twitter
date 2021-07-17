12fa774c00d8cfb6ee0e48981827b7e55ac96d9b
debbiediscovers <debbiediscovers@gmail.com>
2021-07-17 22:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33020,
-	"created_at": "Wed Jul 14 13:12:01 +0000 2021",
+	"tweets": 33021,
+	"created_at": "Sat Jul 17 14:12:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sat Jul 17 13:35:45 +0000 2021",
+		"id": 1416391173631930400,
+		"id_str": "1416391173631930370",
+		"full_text": "My view this morning! 😍 #morningrun https://t.co/JLvzbwEF97",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "morningrun",
+					"indices": [
+						24,
+						35
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1416391161124507600,
+					"id_str": "1416391161124507649",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/E6gIQbXUUAEDAVX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6gIQbXUUAEDAVX.jpg",
+					"url": "https://t.co/JLvzbwEF97",
+					"display_url": "pic.twitter.com/JLvzbwEF97",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1416391173631930370/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1416391161124507600,
+					"id_str": "1416391161124507649",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/E6gIQbXUUAEDAVX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6gIQbXUUAEDAVX.jpg",
+					"url": "https://t.co/JLvzbwEF97",
+					"display_url": "pic.twitter.com/JLvzbwEF97",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1416391173631930370/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder |  @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/WW6rfePvMp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WW6rfePvMp",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63077,
+			"friends_count": 37067,
+			"listed_count": 1300,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20719,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1414536637199966210/_hDNPZ5n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1414536637199966210/_hDNPZ5n_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9d22e02884003",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9d22e02884003.json",
+			"place_type": "poi",
+			"name": "Singapore Botanic Gardens",
+			"full_name": "Singapore Botanic Gardens",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.81613271207779,
+							1.3150985515397737
+						],
+						[
+							103.81613271207779,
+							1.3150985515397737
+						],
+						[
+							103.81613271207779,
+							1.3150985515397737
+						],
+						[
+							103.81613271207779,
+							1.3150985515397737
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 35,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My view this morning! 😍 #morningrun https://t.co/JLvzbwEF97"
+	},
+	{
 		"created_at": "Wed Jul 14 12:16:23 +0000 2021",
 		"id": 1414194561102975000,
 		"id_str": "1415284036667265027",
