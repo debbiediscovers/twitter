1ee008e38de8c8a6124a2f7fdfa5ac1897bc27bd
debbiediscovers <debbiediscovers@gmail.com>
2022-01-30 19:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33091,
-	"created_at": "Tue Nov 02 14:12:01 +0000 2021",
+	"tweets": 33092,
+	"created_at": "Sun Jan 30 11:12:00 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_01.js
@@ -0,0 +1,128 @@
+Grailbird.data.tweets_2022_01 = 
+[
+	{
+		"created_at": "Sun Jan 30 11:07:57 +0000 2022",
+		"id": 1487364023003844600,
+		"id_str": "1487744385840136195",
+		"full_text": "If @spotify doesn’t immediately remove @joerogan, I will release new music onto the platform. #youwerebeautiful",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "youwerebeautiful",
+					"indices": [
+						94,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Spotify",
+					"name": "Spotify",
+					"id": 17230018,
+					"id_str": "17230018",
+					"indices": [
+						3,
+						11
+					]
+				},
+				{
+					"screen_name": "joerogan",
+					"name": "Joe Rogan",
+					"id": 18208354,
+					"id_str": "18208354",
+					"indices": [
+						39,
+						48
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 82097756,
+			"id_str": "82097756",
+			"name": "James Blunt",
+			"screen_name": "JamesBlunt",
+			"location": "London",
+			"description": "Cockney rhyming slang for the good stuff; Proof that one song is all you need.",
+			"url": "https://t.co/XjeFGkL5Ly",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XjeFGkL5Ly",
+							"expanded_url": "https://jamesblunt.lnk.to/SBMF",
+							"display_url": "jamesblunt.lnk.to/SBMF",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2152907,
+			"friends_count": 93,
+			"listed_count": 3938,
+			"created_at": "Tue Oct 13 13:46:00 +0000 2009",
+			"favourites_count": 8497,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1336,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "003366",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1437455546572017665/wl9NeJdQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1437455546572017665/wl9NeJdQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/82097756/1637311063",
+			"profile_link_color": "990000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DCE9FA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 27908,
+		"favorite_count": 239883,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "If @spotify doesn’t immediately remove @joerogan, I will release new music onto the platform. #youwerebeautiful"
+	}
+]
