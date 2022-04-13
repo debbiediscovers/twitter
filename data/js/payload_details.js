85acaeda544f824f95edcba45f90de5cd11b8112
debbiediscovers <debbiediscovers@gmail.com>
2022-04-13 22:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33105,
-	"created_at": "Wed Apr 13 12:12:01 +0000 2022",
+	"tweets": 33106,
+	"created_at": "Wed Apr 13 14:12:01 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 13 13:40:06 +0000 2022",
+		"id": 1514111859783508000,
+		"id_str": "1514236989398159362",
+		"full_text": "Sighted a rainbow this morning. A sign of good luck to come…? 🌈 https://t.co/QccyO37pYz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1514111846516625400,
+					"id_str": "1514111846516625412",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/FQM0s8nVkAQtVxZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQM0s8nVkAQtVxZ.jpg",
+					"url": "https://t.co/QccyO37pYz",
+					"display_url": "pic.twitter.com/QccyO37pYz",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1514111859783507968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1514111846516625400,
+					"id_str": "1514111846516625412",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/FQM0s8nVkAQtVxZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQM0s8nVkAQtVxZ.jpg",
+					"url": "https://t.co/QccyO37pYz",
+					"display_url": "pic.twitter.com/QccyO37pYz",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1514111859783507968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder |  @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/WW6rfePvMp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WW6rfePvMp",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61501,
+			"friends_count": 33704,
+			"listed_count": 1275,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20611,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417300441021370368/Y915Frbb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417300441021370368/Y915Frbb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "109bb0adab550005",
+			"url": "https://api.twitter.com/1.1/geo/id/109bb0adab550005.json",
+			"place_type": "poi",
+			"name": "Holland Green Linear Park",
+			"full_name": "Holland Green Linear Park",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.785552,
+							1.32647
+						],
+						[
+							103.785552,
+							1.32647
+						],
+						[
+							103.785552,
+							1.32647
+						],
+						[
+							103.785552,
+							1.32647
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sighted a rainbow this morning. A sign of good luck to come…? 🌈 https://t.co/QccyO37pYz"
+	},
+	{
 		"created_at": "Wed Apr 13 11:43:41 +0000 2022",
 		"id": 1514111859783508000,
 		"id_str": "1514207694193717248",
