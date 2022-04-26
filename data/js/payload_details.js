166b1e4e4259762df66329c09919618ea980a78b
debbiediscovers <debbiediscovers@gmail.com>
2022-04-26 10:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33112,
-	"created_at": "Thu Apr 21 19:12:01 +0000 2022",
+	"tweets": 33113,
+	"created_at": "Tue Apr 26 02:12:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 26 02:10:21 +0000 2022",
+		"id": 1518774453207183400,
+		"id_str": "1518774453207183360",
+		"full_text": "Got my first mystery box!! Woohoo @Stepnofficial $stepn $gmt $gst https://t.co/bzRiZTGVuJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [
+				{
+					"text": "stepn",
+					"indices": [
+						49,
+						55
+					]
+				},
+				{
+					"text": "gmt",
+					"indices": [
+						56,
+						60
+					]
+				},
+				{
+					"text": "gst",
+					"indices": [
+						61,
+						65
+					]
+				}
+			],
+			"user_mentions": [
+				{
+					"screen_name": "Stepnofficial",
+					"name": "STEPN | Public Beta Phase III",
+					"id": 1439068275040653300,
+					"id_str": "1439068275040653313",
+					"indices": [
+						34,
+						48
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1518774447586803700,
+					"id_str": "1518774447586803712",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/FRPFUDJaAAA2H5_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRPFUDJaAAA2H5_.jpg",
+					"url": "https://t.co/bzRiZTGVuJ",
+					"display_url": "pic.twitter.com/bzRiZTGVuJ",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1518774453207183360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1518774447586803700,
+					"id_str": "1518774447586803712",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/FRPFUDJaAAA2H5_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRPFUDJaAAA2H5_.jpg",
+					"url": "https://t.co/bzRiZTGVuJ",
+					"display_url": "pic.twitter.com/bzRiZTGVuJ",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1518774453207183360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1517014455372066800,
+		"in_reply_to_status_id_str": "1517014455372066816",
+		"in_reply_to_user_id": 3060836348,
+		"in_reply_to_user_id_str": "3060836348",
+		"in_reply_to_screen_name": "debbiediscovers",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Deborah Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "Head of Digital Transformation | Speaker | 21 All Star Twitter | 3X School Builder |  @Medium Top writer #AI and chatbots | Allergic to Stupid Tweets",
+			"url": "https://t.co/WW6rfePvMp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WW6rfePvMp",
+							"expanded_url": "https://debbiediscovers.me",
+							"display_url": "debbiediscovers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61406,
+			"friends_count": 33674,
+			"listed_count": 1273,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20610,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17724,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417300441021370368/Y915Frbb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417300441021370368/Y915Frbb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "58a4c3a0d54e1400",
+			"url": "https://api.twitter.com/1.1/geo/id/58a4c3a0d54e1400.json",
+			"place_type": "admin",
+			"name": "Central Region",
+			"full_name": "Central Region, Singapore",
+			"country_code": "SG",
+			"country": "Singapore",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							103.749959507073,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.2123138339349
+						],
+						[
+							103.918426999964,
+							1.36874499902569
+						],
+						[
+							103.749959507073,
+							1.36874499902569
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Got my first mystery box!! Woohoo @Stepnofficial $stepn $gmt $gst https://t.co/bzRiZTGVuJ"
+	},
+	{
 		"created_at": "Thu Apr 21 18:47:32 +0000 2022",
 		"id": 1514748695643754500,
 		"id_str": "1517213460098355200",
