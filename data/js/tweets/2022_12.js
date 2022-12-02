79177303b9209c9294a1e1ceb9132c66ecdd98be
debbiediscovers <debbiediscovers@gmail.com>
2022-12-02 17:12:02 +0800

    Added 1 tweets

---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,99 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Fri Dec 02 08:18:38 +0000 2022",
+		"id": 1597560798255145000,
+		"id_str": "1598592464528736256",
+		"full_text": "AI is taking over the internet.\n\nBut 99% of people aren't utilizing it.\n\nHere are 10 mind-blowing AI websites you should know about:",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://hypefury.com\" rel=\"nofollow\">Hypefury</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1452145642667577300,
+			"id_str": "1452145642667577347",
+			"name": "Colby Kultgen",
+			"screen_name": "thecolbykultgen",
+			"location": "",
+			"description": "Follow me if you'd like to improve 1% every day.  Sign up for my newsletter + receive a FREE Goal-Setting System 👇",
+			"url": "https://t.co/EGdFO4qYYZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EGdFO4qYYZ",
+							"expanded_url": "https://colbynewsletter.ck.page",
+							"display_url": "colbynewsletter.ck.page",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33126,
+			"friends_count": 322,
+			"listed_count": 1037,
+			"created_at": "Sun Oct 24 05:31:29 +0000 2021",
+			"favourites_count": 13373,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2806,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1581191039129333763/63aZwKM1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1581191039129333763/63aZwKM1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1452145642667577347/1656244655",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2282,
+		"favorite_count": 6798,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "AI is taking over the internet.\n\nBut 99% of people aren't utilizing it.\n\nHere are 10 mind-blowing AI websites you should know about:"
+	}
+]
