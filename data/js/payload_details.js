655e907dc96207e2ff1b1b827f3a6e2f0a879cac
debbiediscovers <debbiediscovers@gmail.com>
2023-02-18 10:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33176,
-	"created_at": "Sat Feb 18 01:12:01 +0000 2023",
+	"tweets": 33177,
+	"created_at": "Sat Feb 18 02:12:01 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Sat Feb 18 01:17:25 +0000 2023",
+		"id": 1626752713131442200,
+		"id_str": "1626752713131442176",
+		"full_text": "@oaxa @futurism As long as it's not in a sports car, I think you're safe 🤣",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "oaxa",
+					"name": "juan ugarte 🛩 Pilot🕺 SwingDancer 🕉 Meditator",
+					"id": 15694951,
+					"id_str": "15694951",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "futurism",
+					"name": "Futurism",
+					"id": 2557446343,
+					"id_str": "2557446343",
+					"indices": [
+						6,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1626745579077509000,
+		"in_reply_to_status_id_str": "1626745579077509120",
+		"in_reply_to_user_id": 15694951,
+		"in_reply_to_user_id_str": "15694951",
+		"in_reply_to_screen_name": "oaxa",
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Debbi Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "I love anything tech, geeky and health related. Allergic to stupid tweets",
+			"url": "https://t.co/3Fk7XrPjmf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3Fk7XrPjmf",
+							"expanded_url": "https://deb.bi",
+							"display_url": "deb.bi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 59365,
+			"friends_count": 32898,
+			"listed_count": 1238,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20434,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17733,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1611258667114074112/NT4_mkXO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1611258667114074112/NT4_mkXO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1601181765",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@oaxa @futurism As long as it's not in a sports car, I think you're safe 🤣"
+	},
+	{
 		"created_at": "Sat Feb 18 00:44:01 +0000 2023",
 		"id": 1626744309243605000,
 		"id_str": "1626744309243604993",
