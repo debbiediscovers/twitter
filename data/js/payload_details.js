cb650fa0014ffac35935a21b3840e254da8d753d
debbiediscovers <debbiediscovers@gmail.com>
2023-06-01 15:12:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33260,
-	"created_at": "Thu Jun 01 06:12:01 +0000 2023",
+	"tweets": 33261,
+	"created_at": "Thu Jun 01 07:12:01 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_06.js
+++ data/js/tweets/2023_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Thu Jun 01 06:24:14 +0000 2023",
+		"id": 1664155879548289000,
+		"id_str": "1664155879548289024",
+		"full_text": "Bittersweet. https://t.co/Bm98GyRnZ2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1664155873512681500,
+					"id_str": "1664155873512681472",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FxhE8yuacAAhOGe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxhE8yuacAAhOGe.jpg",
+					"url": "https://t.co/Bm98GyRnZ2",
+					"display_url": "pic.twitter.com/Bm98GyRnZ2",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1664155879548289024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1179,
+							"h": 1179,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1179,
+							"h": 1179,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664155873512681500,
+					"id_str": "1664155873512681472",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FxhE8yuacAAhOGe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxhE8yuacAAhOGe.jpg",
+					"url": "https://t.co/Bm98GyRnZ2",
+					"display_url": "pic.twitter.com/Bm98GyRnZ2",
+					"expanded_url": "https://twitter.com/debbiediscovers/status/1664155879548289024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1179,
+							"h": 1179,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1179,
+							"h": 1179,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060836348,
+			"id_str": "3060836348",
+			"name": "Debbi Kay",
+			"screen_name": "debbiediscovers",
+			"location": "Singapore, London, New York",
+			"description": "I love anything tech, geeky and health related. Allergic to stupid tweets. https://t.co/B9llXCeBMK",
+			"url": "https://t.co/3Fk7XrPjmf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3Fk7XrPjmf",
+							"expanded_url": "https://deb.bi",
+							"display_url": "deb.bi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/B9llXCeBMK",
+							"expanded_url": "https://staging.bsky.app/profile/deb.bi",
+							"display_url": "staging.bsky.app/profile/deb.bi",
+							"indices": [
+								75,
+								98
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 58768,
+			"friends_count": 32728,
+			"listed_count": 1229,
+			"created_at": "Wed Mar 04 05:01:01 +0000 2015",
+			"favourites_count": 20390,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1631824546386034689/g3dC3_bl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1631824546386034689/g3dC3_bl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060836348/1676843509",
+			"profile_link_color": "D50252",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Bittersweet. https://t.co/Bm98GyRnZ2"
+	},
+	{
 		"created_at": "Thu Jun 01 06:09:24 +0000 2023",
 		"id": 1664152145137139700,
 		"id_str": "1664152145137139714",
